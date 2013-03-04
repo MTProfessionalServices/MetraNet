46720768105a7e6a4c746001b8daa6bb57ff2c2f
Scott Swartz <sswartz@metratech.com>
2013-03-03 23:01:57 -0500

    Small changes to MoneyType

---
+++ ExpressionEngine/TypeSystem/Enumerations/CurrencyMode.cs
@@ -0,0 +1,28 @@
+﻿namespace MetraTech.ExpressionEngine.TypeSystem.Enumerations
+{
+    /// <summary>
+    /// Indicates how the Currency is determined.
+    /// </summary>
+    public enum CurrencyMode
+    {
+        /// <summary>
+        /// Either not a numeric or it's unknown
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Implied by the context which is up to the developer.
+        /// </summary>
+        Context,
+
+        /// <summary>
+        /// Always the same (i.e., USD). Specified in the Currency field
+        /// </summary>
+        Fixed,
+
+        /// <summary>
+        /// Determined via a property within the same property collection. Property name specified in the CurrencyProperty property.
+        /// </summary>
+        Property
+    }
+}
--- ExpressionEngine/TypeSystem/MoneyType.cs
+++ ExpressionEngine/TypeSystem/MoneyType.cs
@@ -8,14 +8,24 @@ namespace MetraTech.ExpressionEngine.TypeSystem
     {
         #region Properties
 
+        /// <summary>
+        /// Indicates how the Currency is determine
+        /// </summary>
+        [DataMember]
+        public CurrencyMode CurrencyMode { get; set; }
+
+        /// <summary>
+        /// The currency. Only valid when CurrencyMode=Fixed.
+        /// ???Should this be renamed to FixedCurrency???
+        /// </summary>
         [DataMember]
         public string Currency { get; set; }
-        public UnitOfMeasureMode UnitOfMeasure { get; set; }
 
-        //[DataMember]
-        //public string UnitsProperty { get; set; }
-        //public UnitOfMeasureMode UnitOfMeasureMode { get; set; }
-
+        /// <summary>
+        /// The property that drives the currency. Only valid when CurrencyMode=Property.
+        /// </summary>
+        [DataMember]
+        public string CurrencyProperty { get; set; }
 
         #endregion
       
@@ -26,6 +36,13 @@ namespace MetraTech.ExpressionEngine.TypeSystem
         #endregion
 
         #region Methods
+
+        public override void Validate(string prefix, Validations.ValidationMessageCollection messages, Context context)
+        {
+            //Error if None
+            //Error if Currency not specified
+            //Error if Currency doesn't exist
+        }
 
         public new MoneyType Copy()
         {

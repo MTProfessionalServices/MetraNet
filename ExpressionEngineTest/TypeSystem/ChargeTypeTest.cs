5046cbf993ca5c1239b3f648c7b301bc56217db9
Scott Swartz <sswartz@metratech.com>
2013-03-06 12:02:44 -0500

    Checked in missing file...

---
+++ ExpressionEngineTest/TypeSystem/ChargeTypeTest.cs
@@ -0,0 +1,19 @@
+﻿using MetraTech.ExpressionEngine.TypeSystem;
+using MetraTech.ExpressionEngine.TypeSystem.Enumerations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ExpressionEngineTest
+{
+    [TestClass()]
+    public class ChargeTypeTest
+    {
+        [TestMethod()]
+        public void ChargeTypeFactoryTest()
+        {
+            var chargeType = TypeFactory.CreateCharge();
+            Assert.AreEqual(BaseType.Charge, chargeType.BaseType);
+            Assert.IsTrue(chargeType.IsNumeric);
+        }
+    }
+}

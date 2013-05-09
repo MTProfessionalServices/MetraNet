5825d908ae414ca824bab8a0c1c674b60a93eb60
shahzadzafar <szafar@metratech.com>
2013-05-09 17:41:54 -0400

    Added folders.
    Java, MetraConnect, PrototypeCode, Source and Samples combined into one
    repo.

---
+++ SDK/com/metratech/sdk/base/MTParseException.java
@@ -0,0 +1,24 @@
+package com.metratech.sdk.base;
+
+/**
+ *  Description: *		The MTParseException class handles parsing specific errors.
+ *		This class extends the MTException class.
+ */
+public class MTParseException extends MTException
+{
+	/**
+	 *  Description:
+	 *		Default Constructor
+	 */
+	public MTParseException()
+	{
+	}
+	
+	/**
+	 *  Description:
+	 *		Construct with a message
+	 */
+	public MTParseException(String message)
+	{	super (message);
+	}
+}

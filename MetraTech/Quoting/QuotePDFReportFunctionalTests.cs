4e7fc765e4d7c98adbb667e46425cc413572695b
Shahzad Zafar <szafar@metratech.com>
2013-06-14 11:02:36 -0400

    Updated to rev # 123662.

---
+++ MetraTech/Quoting/QuotePDFReportFunctionalTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using MetraTech.Core.Services.ClientProxies;
+using MetraTech.DataAccess;
+using MetraTech.DomainModel.BaseTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MetraTech.Interop.MTProductCatalog;
+using MetraTech.Shared.Test;
+using Core.Quoting;
+
+namespace MetraTech.Quoting.Test
+{
+  [TestClass]
+  public class QuotePDFReportFunctionalTests
+  {
+    #region Setup/Teardown
+
+    [ClassInitialize]
+    public static void InitTests(TestContext testContext)
+    {
+
+    }
+
+    #endregion
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [TestMethod]
+    public void LoadQuoteReportingConfiguration()
+    {
+      //Quoting Configuration is needed to initialize
+      QuoteReportingConfiguration quoteReportingConfiguration = QuoteReportingConfigurationManager.LoadConfiguration(null);
+
+      //TODO: Determine what values to check as they will be different on smoke test machine
+      Assert.IsTrue(quoteReportingConfiguration.ReportingServerFolder.Contains("MetraNet"), "After loading configuration the ReportingServerFolder was expected to contain the word MetraNet");
+
+      Assert.AreEqual("NetMeter", quoteReportingConfiguration.ReportingDatabaseName);
+    } 
+
+    ///// <summary>
+    ///// 
+    ///// </summary>
+    //[TestMethod]
+    //public void GeneratePDFForQuote()
+    //{
+    //  int idQuote = 20;
+
+    //  QuoteReportingConfiguration quoteReportingConfiguration = QuoteReportingConfigurationManager.LoadConfiguration(null);
+
+    //  QuotePDFReport quotePDFReport = new QuotePDFReport(quoteReportingConfiguration);
+
+    //  quotePDFReport.CreatePDFReport(idQuote, 999, "Quote Report", 840);
+    //} 
+
+  }
+}
---
+++ MetraTech/Quoting/QuotingRepositoryFunctionalTests.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using MetraTech.Core.Services.ClientProxies;
+using MetraTech.DataAccess;
+using MetraTech.DomainModel.BaseTypes;
+using MetraTech.DomainModel.ProductCatalog;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MetraTech.Interop.MTProductCatalog;
+using MetraTech.Shared.Test;
+using Core.Quoting;
+
+namespace MetraTech.Quoting.Test
+{
+  [TestClass]
+  public class QuotingRepositoryFunctionalTests
+  {
+    #region Setup/Teardown
+
+    [ClassInitialize]
+    public static void InitTests(TestContext testContext)
+    {
+      SharedTestCode.MakeSureServiceIsStarted("ActivityServices");
+      //SharedTestCode.MakeSureServiceIsStarted("Pipeline");
+    }
+
+    #endregion
+
+    /// <summary>
+    /// Test check whether QuoteHeader and QuoteContent saved in db based on  QuoteRequest and QuoteResponse
+    /// </summary>
+    [TestMethod]
+    public void QuotingRepositorySaveBMEsFunctionalTest()
+    {
+      #region prepare QuoteRequest
+
+      var request = new QuoteRequest
+          {
+            QuoteIdentifier = "MyQuoteId-1234",
+            QuoteDescription = "Quote for QuotingRepositorySaveFunctionalTest",
+            ReportParameters = new ReportParams() { PDFReport = false },
+            EffectiveDate = new DateTime(2013, 04, 10, 10, 55, 30),
+            EffectiveEndDate = new DateTime(2013, 04, 10, 10, 55, 30)
+          };
+
+      request.Accounts.Add(123);
+      request.Accounts.Add(150);
+      request.ProductOfferings.Add(535);
+      request.ProductOfferings.Add(600);
+
+      #region Create UDRCs
+      
+      List<UDRCInstanceValueBase> udrcValues = new List<UDRCInstanceValueBase>();
+
+      var udrcValue = new UDRCInstanceValue
+        {
+          UDRC_Id = 5351,
+          StartDate = MetraTime.Now,
+          EndDate = MetraTime.Now,
+          Value = 20
+        };
+      udrcValues.Add(udrcValue);
+
+      request.SubscriptionParameters.UDRCValues.Add("535", udrcValues);
+
+      udrcValue = new UDRCInstanceValue
+      {
+        UDRC_Id = 6001,
+        StartDate = MetraTime.Now,
+        EndDate = MetraTime.Now,
+        Value = 20
+      };
+      udrcValues.Add(udrcValue);
+
+      request.SubscriptionParameters.UDRCValues.Add("600", udrcValues); 
+
+      #endregion
+
+      #endregion
+
+      #region Test and Verify how QuoteHeader created and saved into DB before quote generation started (check of QuotingRepository.CreateQuote method)
+
+      var quotingRepository = new QuotingRepository();
+      var requestSaved = false;
+      var quoteHeaderID = -1;
+      try
+      {
+        quoteHeaderID = quotingRepository.CreateQuote(request, null);
+        requestSaved = true;
+      }
+      catch (Exception ex)
+      {
+        Assert.Fail("Exception on request save: " + ex.Message);
+      }
+
+      Assert.IsTrue(requestSaved, "Request not saved");
+
+      Assert.AreNotEqual(quoteHeaderID, -1, "Wrong quoteHeaderID");
+
+      #endregion
+
+      #region prepare QuoteResponse
+
+      var response = new QuoteResponse
+          {
+            idQuote = quoteHeaderID,
+            TotalAmount = 200,
+            ReportLink = "Test Report Link",
+            CreationDate = new DateTime(2013, 05, 10, 10, 55, 30),
+            FailedMessage = "",
+            Status = QuoteStatus.Complete
+          };
+
+      #endregion
+
+      #region Test and Verify how QuoteContent created and saved into DB after quote generated (check of QuotingRepository.UpdateQuoteWithResponse method)
+
+      var responseSaved = false;
+      try
+      {
+        quotingRepository.UpdateQuoteWithResponse(response);
+        responseSaved = true;
+      }
+      catch (Exception ex)
+      {
+        Assert.Fail("Exception on response save: " + ex.Message);
+      }
+
+      Assert.IsTrue(responseSaved, "Response not saved");
+
+      #endregion
+
+      #region Test and Verify how QuoteHeader and related BMEs saved into DB before (check of QuotingRepository.GetQuoteHeader method)
+      //todo add assert to verify QuoteContent
+
+      SharedTestCodeQuoting.VerifyQuoteRequestCorrectInRepository(quoteHeaderID, request, new QuotingRepository());
+      SharedTestCodeQuoting.VerifyQuoteResponseCorrectInRepository(quoteHeaderID, response, new QuotingRepository());
+      #endregion
+
+      #region Cleanup of QuoteHeader, QuoteContent, AccountsForQuote, POsforQuote tables
+
+      //todo Cleanup of QuoteHeader, QuoteContent, AccountsForQuote, POsforQuote tables
+
+      #endregion
+
+    }
+
+    [TestMethod]
+    public void QuotingRepositorySaveLogRecordsFunctionalTests()
+    {
+      var repository = new QuotingRepository();
+
+      int initialRecordsCount = repository.GetQuoteLogRecordsCount();
+
+      var quoteList = new List<QuoteLogRecord>();
+      quoteList.AddRange(Enumerable.Range(0, 2).Select(e => new QuoteLogRecord() { DateAdded = MetraTime.Now })); // Add 2 dummy objects
+
+      repository.SaveQuoteLog(quoteList);
+
+      int currentRecordsCount = repository.GetQuoteLogRecordsCount();
+
+      Assert.AreEqual(initialRecordsCount + 2, currentRecordsCount);
+    }
+  }
+}

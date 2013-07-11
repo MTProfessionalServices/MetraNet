659d08d8c2662d3addc8dcb01a6cc60416fce93f
Scott Swartz <sswartz@metratech.com>
2013-07-11 17:26:30 -0400

    Added two missing files.

---
+++ ExpressionEngineTest/PropertyBags/PropertyBagTest.cs
@@ -0,0 +1,18 @@
+﻿using MetraTech.ExpressionEngine.PropertyBags;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExpressionEngineTest
+{
+    [TestClass()]
+    public class PropertyBagTest
+    {
+        [TestMethod()]
+        public void GetFileNameTest()
+        {
+            var entity = PropertyBagFactory.CreateProductViewEntity("MetraTech", "Foo", null);
+            entity.Extension = "Core";
+            var file = entity.GetFileNameGivenExtensionsDirectory(@"C:\RMP\Extensions");
+            Assert.AreEqual(@"C:\RMP\Extensions\Core\Config\ProductViews\Foo.xml", file);
+        }
+    }
+}

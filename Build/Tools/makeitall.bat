01b096b9f8af5041929f3542da82724ecb5861ca
unknown <Administrator@devtmploraw.metratech.com>
2013-07-13 09:10:04 -0400

    Additional fixes to include MVM build in main scripts

--- Build/Tools/makeitall.bat
+++ Build/Tools/makeitall.bat
@@ -8,4 +8,4 @@ Echo Building %buildTarget%
 Echo.
  
 msbuild %ROOTDIR%\Build\MSBuild\MetraNetBuild.proj /m /ds /t:%buildTarget% /p:config=%VERSION%;MyTargets=Build /fl /flp:ShowTimestamp;Verbosity=DIAG;Summary;LogFile=%temp%\msbuild.log /clp:Verbosity=M;Summary
-msbuild %(MAINWORKDIR)\RMP\Extensions\MVMCore_Internal\SourceCode\Mvm\MVM.sln /m /ds /t:%buildTarget% /p:config=%VERSION%;MyTargets=Build /fl /flp:ShowTimestamp;Verbosity=DIAG;Summary;LogFile=%temp%\MetraNetMVMBuild.log /clp:Verbosity=M;Summary
+msbuild %RMPDIR%\Extensions\MVMCore_Internal\SourceCode\Mvm\MVM.sln /m /ds /p:config=%VERSION%;MyTargets=Build /fl /flp:ShowTimestamp;Verbosity=DIAG;Summary;LogFile=%temp%\MetraNetMVMBuild.log /clp:Verbosity=M;Summary

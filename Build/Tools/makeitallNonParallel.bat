135e3d4a895a00cb53acfb2eabb75a948a6e5477
shahzadzafar <szafar@metratech.com>
2013-07-14 12:10:59 -0400

    new

--- Build/Tools/makeitallNonParallel.bat
+++ Build/Tools/makeitallNonParallel.bat
@@ -8,5 +8,3 @@ Echo Building %buildTarget%
 Echo.
 
 msbuild %ROOTDIR%\Build\MSBuild\MetraNetBuild.proj /t:%buildTarget% /p:config=%VERSION%;MyTargets=Build /fl /flp:ShowTimestamp;Verbosity=N;Summary;LogFile=%temp%\msbuild.log /clp:Verbosity=M;Summary
-
-msbuild %RMPDIR%\Extensions\MVMCore_Internal\SourceCode\Mvm\MVM.sln /m /ds /p:config=%VERSION%;MyTargets=Build /fl /flp:ShowTimestamp;Verbosity=DIAG;Summary;LogFile=%temp%\MetraNetMVMBuild.log /clp:Verbosity=M;Summary

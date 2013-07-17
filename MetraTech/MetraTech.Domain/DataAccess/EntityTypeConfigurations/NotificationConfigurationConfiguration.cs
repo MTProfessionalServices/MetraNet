97d8f0b2a353b2a07dcd050bfa09dc32de462b5b
Sergey Malinovskiy <smalinovskiy@metratech.com>
2013-07-17 04:33:41 -0400

    TA131 DEV: Integrate Mario's project into MetraNet Reviewer: AZhurba

---
+++ MetraTech/MetraTech.Domain/DataAccess/EntityTypeConfigurations/NotificationConfigurationConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using MetraTech.Domain.Notifications;
+using System.Data.Entity.ModelConfiguration;
+
+namespace MetraTech.Domain.DataAccess.EntityTypeConfigurations
+{
+  /// <summary>
+  /// Entity Framework Code-First configuration that maps an entity or complex type to the database
+  /// </summary>
+  public class NotificationConfigurationConfiguration : EntityTypeConfiguration<NotificationConfiguration>
+  {
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public NotificationConfigurationConfiguration()
+    {
+      Map(t =>
+      {
+        t.ToTable("NotificationConfiguration");
+        t.MapInheritedProperties();
+      });
+      Property(t => t.EmailTemplateXml).HasColumnName("EmailTemplate");
+    }
+  }
+}
---
+++ MetraTech/MetraTech.Domain/Events/ThresholdCrossingEvent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MetraTech.Domain.Events
+{
+  [DataContract(Namespace="MetraTech.MetraNet")]
+  public class ThresholdCrossingEvent : Event
+  {
+    [DataMember]
+    public Quantity UsageQuantityForPriorTier { get; set; }
+    [DataMember]
+    public Money PriceForPriorTier { get; set; }
+    [DataMember]
+    public Quantity UsageQuantityForNextTier { get; set; }
+    [DataMember]
+    public Money PriceForNextTier { get; set; }
+    [DataMember]
+    public Quantity CurrentUsageQuantity { get; set; }
+    [DataMember]
+    public DateTime ThresholdPeriodStart { get; set; }
+    [DataMember]
+    public DateTime ThresholdPeriodEnd { get; set; }
+    [DataMember]
+    public Guid SubscriptionId { get; set; }
+  }
+}

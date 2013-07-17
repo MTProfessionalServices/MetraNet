1b2391932b9809a8bef32d5846bed70140ae820a
Sergey Malinovskiy <smalinovskiy@metratech.com>
2013-07-17 04:33:41 -0400

    TA131 DEV: Integrate Mario's project into MetraNet Reviewer: AZhurba

---
+++ MetraTech/MetraTech.Application.Test/NotificationProcessorTest.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using MetraTech.Domain;
+using MetraTech.Domain.Events;
+using MetraTech.Domain.Notifications;
+using MetraTech.Domain.Test;
+using MetraTech.Domain.Test.DataAccess;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace MetraTech.Application.Test
+{
+  [TestClass]
+  public class NotificationProcessorTest
+  {
+    [TestMethod]
+    public void TestNotification()
+    {
+      var notificationEndpoint = CreateTestNotificationEndpoint();
+      var notificationConfiguration = CreateTestNotificationConfiguration(notificationEndpoint);
+
+      var fakeContext = new FakeMetraNetContext
+        {
+          NotificationEndpoints = new List<NotificationEndpoint> {notificationEndpoint}.ToIDbSet(),
+          NotificationConfigurations = new List<NotificationConfiguration> {notificationConfiguration}.ToIDbSet()
+        };
+
+      var triggeredEvent = new ThresholdCrossingEvent
+      {
+        UsageQuantityForPriorTier = new Quantity(1000m, "MIN"),
+        PriceForPriorTier = new Money(0.25m, "USD"),
+        UsageQuantityForNextTier = new Quantity(2000m, "MIN"),
+        PriceForNextTier = new Money(0.20m, "USD"),
+        CurrentUsageQuantity = new Quantity(1025m, "MIN"),
+        ThresholdPeriodStart = new DateTime(2013, 1, 1),
+        ThresholdPeriodEnd = new DateTime(2014, 1, 1),
+        SubscriptionId = Guid.Empty
+      };
+
+      NotificationProcessor.ProcessEvent(fakeContext, triggeredEvent);
+    }
+
+    public static NotificationConfiguration CreateTestNotificationConfiguration(NotificationEndpoint notificationEndpoint)
+    {
+      var localizedEmailTemplate = new LocalizedEmailTemplate
+      {
+        SubjectTemplate = EmailTemplates.ThresholdCrossingTemplateSubject,
+        BodyTemplate = EmailTemplates.ThresholdCrossingTemplateBody
+      };
+
+      var emailTemplate = new EmailTemplate
+      {
+        ToRecipient = "mdesousa@metratech.com",
+        CarbonCopyRecipients = new List<string>(),
+        DeliveryLanguage = "en-us",
+        EmailTemplateDictionary = new EmailTemplateDictionary { { "en-us", localizedEmailTemplate } }
+      };
+
+
+      var notificationConfiguration = new NotificationConfiguration();
+      notificationConfiguration.EntityId = Guid.NewGuid();
+      notificationConfiguration.CreationDate = DateTime.Now;
+      notificationConfiguration.ModifiedDate = DateTime.Now;
+      notificationConfiguration.Name.Add("en-us", "Threshold Notification");
+      notificationConfiguration.EventType = "ThresholdCrossingEvent";
+      notificationConfiguration.NotificationType = NotificationType.Email;
+      notificationConfiguration.NotificationEndpoint = notificationEndpoint;
+      notificationConfiguration.EmailTemplate = emailTemplate;
+      return notificationConfiguration;
+    }
+
+    public static NotificationEndpoint CreateTestNotificationEndpoint()
+    {
+      var authenticationConfiguration = new NetworkAuthenticationConfiguration
+      {
+        UserName = "server3994",
+        Password = "9uY2zevySXPPaoXU2Lbv"
+      };
+
+      var endpointConfiguration = new EmailEndpointConfiguration
+      {
+        EndpointAddress = "smtp.socketlabs.com"
+      };
+
+      var notificationEndpoint = new NotificationEndpoint();
+      notificationEndpoint.EntityId = Guid.NewGuid();
+      notificationEndpoint.CreationDate = DateTime.Now;
+      notificationEndpoint.ModifiedDate = DateTime.Now;
+      notificationEndpoint.Name.Add("en-us", "Corporte Email");
+      notificationEndpoint.Active = true;
+      notificationEndpoint.AuthenticationConfiguration = authenticationConfiguration;
+      notificationEndpoint.EndpointConfiguration = endpointConfiguration;
+      return notificationEndpoint;
+    }
+  }
+}
---
+++ MetraTech/MetraTech.Domain/DataAccess/IMetraNetContext.cs
@@ -0,0 +1,23 @@
+﻿using MetraTech.Domain.Notifications;
+using System.Data.Entity;
+
+namespace MetraTech.Domain.DataAccess
+{
+  /// <summary>
+  /// Interface for Entity Framework database context for MetraNet
+  /// </summary>
+  public interface IMetraNetContext
+  {
+    IDbSet<Entity> Entities { get; set; }
+    IDbSet<NotificationConfiguration> NotificationConfigurations { get; set; }
+    IDbSet<NotificationEndpoint> NotificationEndpoints { get; set; }
+
+    /// <summary>
+    /// Returns a DbSet instance for access to entities of the given type in the context, the ObjectStateManager, and the underlying store.
+    /// </summary>
+    /// <typeparam name="TEntity">The type entity for which a set should be returned.</typeparam>
+    /// <returns>A set for the given entity type.</returns>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1716:IdentifiersShouldNotMatchKeywords", MessageId = "Set", Justification = "Mario: Required to mock DbContext")]
+    IDbSet<TEntity> Set<TEntity>() where TEntity : class;
+  }
+}

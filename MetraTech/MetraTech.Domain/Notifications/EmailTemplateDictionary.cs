18691ab187dd5e52671a8ade85d6ee519909651b
Sergey Malinovskiy <smalinovskiy@metratech.com>
2013-07-17 04:33:41 -0400

    TA131 DEV: Integrate Mario's project into MetraNet Reviewer: AZhurba

---
+++ MetraTech/MetraTech.Domain/Notifications/EmailTemplateDictionary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace MetraTech.Domain.Notifications
+{
+  [Serializable]
+  public class EmailTemplateDictionary : Dictionary<string, LocalizedEmailTemplate>
+  {
+    public EmailTemplateDictionary()
+    {
+    }
+
+    protected EmailTemplateDictionary(SerializationInfo info, StreamingContext context)
+      : base(info, context)
+    {
+    }
+  }
+}

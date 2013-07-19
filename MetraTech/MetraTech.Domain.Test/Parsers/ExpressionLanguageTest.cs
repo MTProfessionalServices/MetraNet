f6d5b63931197756c1f52093f5539dbcfe61db5e
Mario DeSousa <mdesousa@metratech.com>
2013-07-19 09:15:29 -0400

    US15: Initial version of Expression Language (WIP)

--- MetraTech/MetraTech.Domain.Test/Parsers/ExpressionLanguageTest.cs
+++ MetraTech/MetraTech.Domain.Test/Parsers/ExpressionLanguageTest.cs
@@ -1,4 +1,7 @@
-﻿using Antlr4.Runtime;
+﻿using System;
+using Antlr4.Runtime;
+using MetraTech.Domain.Expressions;
+using MetraTech.Domain.Parsers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MetraTech.Domain.Test.Parsers
@@ -9,13 +12,12 @@ namespace MetraTech.Domain.Test.Parsers
         [TestMethod]
         public void ExpressionLanguageBasicTest()
         {
-            var lex = new ExpressionLanguageLexer(new AntlrInputStream("FirstName, LastName\n" +
-                                                               "Mario, DeSousa\n" +
-                                                               "John, Doe\n"));
-            var tokens = new CommonTokenStream(lex);
-            var parser = new ExpressionLanguageParser(tokens);
-            var context = parser.file();
-            Assert.AreEqual(2, context._rows.Count);
+            const string input = "5.3";
+            var expression = ExpressionLanguageHelper.ParseExpression(input);
+            Assert.IsNotNull(expression);
+            Assert.IsInstanceOfType(expression, typeof(ConstantExpression));
+            var value = expression.Evaluate<Decimal>();
+            Assert.AreEqual(5.3m, value);
         }
     }
 }
---
+++ MetraTech/MetraTech.Domain/Parsers/ExpressionBuilder.cs
@@ -0,0 +1,22 @@
+﻿namespace MetraTech.Domain.Parsers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Expressions;
+
+    /// <summary>
+    /// Builds a MetraTech Expression after parsing of Expression Language
+    /// </summary>
+    public class ExpressionBuilder : ExpressionLanguageBaseVisitor<Expression>
+    {
+        public override Expression VisitNumberExpression(ExpressionLanguageParser.NumberExpressionContext context)
+        {
+            var value = Decimal.Parse(context.GetText());
+            var expression = new ConstantExpression { Value = value };
+            return expression;
+        }
+
+    }
+}

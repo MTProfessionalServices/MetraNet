8804f21a3392042cef98b81104289ef40c4f4c5d
MTkachev <mtkachev@metratech.com>
2013-07-19 12:21:50 -0400

    US5 As a Metranet developer I require the entity browser prototype tree control hosted on Metranet

--- MetraTech.ExpressionEngine.Metadata/MetadataHelper.cs
+++ MetraTech.ExpressionEngine.Metadata/MetadataHelper.cs
@@ -1,20 +1,45 @@
-﻿using Newtonsoft.Json;
+﻿using MetraTech.Basic;
+using MetraTech.DataAccess;
+using MetraTech.Interop.QueryAdapter;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
 namespace MetraTech.ExpressionEngine.Metadata
 {
   public static class MetadataHelper
   {
+    private static string _jsonMetadata = string.Empty;
+
     public static string RetrieveJsonMetadata()
     {
-      var metadata = 5;
+      if (!string.IsNullOrEmpty(_jsonMetadata))
+        return _jsonMetadata;
+
+      var metadataSerialized = string.Empty;
+
+      var queryAdapter = new MTQueryAdapter();
+      queryAdapter.Init("Queries\\ExpressionEngine");
+      queryAdapter.SetQueryTag("__GET_METADATA_LATEST__");
+      var txInfo = queryAdapter.GetQuery();
+      using (var conn = ConnectionManager.CreateConnection())
+        using (var stmt = conn.CreatePreparedStatement(txInfo))
+          using (var dataReader = stmt.ExecuteReader())
+            if (dataReader.Read())
+              metadataSerialized = dataReader.GetString("content");
+
+      if (string.IsNullOrEmpty(metadataSerialized))
+        return string.Empty;
+
+      var metadata = SerializationHelper.DeserializeDataContractXml<ContextMetadata>(metadataSerialized);
 
       var jsonSerializerSettings = new JsonSerializerSettings();
       jsonSerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
       jsonSerializerSettings.Converters.Add(new IsoDateTimeConverter());
       jsonSerializerSettings.Converters.Add(new StringEnumConverter());
 
-      return JsonConvert.SerializeObject(metadata, jsonSerializerSettings);      
+      _jsonMetadata = JsonConvert.SerializeObject(metadata, jsonSerializerSettings);
+
+      return _jsonMetadata;
     }
   }
 }

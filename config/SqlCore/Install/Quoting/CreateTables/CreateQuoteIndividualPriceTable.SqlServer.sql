e762e234945f993eed575fd40e4c215912b20e6d
Sergey Letushov <sletushov@metratech.com>
2013-07-22 08:54:59 -0400

    TA86: (Dev) Create ProductOfferingParameters class and implement EntityFramework data context Changed column name from RateSchedule to RateSchedules, because collection. Code Reviewer: ASkrinnik

--- config/SqlCore/Install/Quoting/CreateTables/CreateQuoteIndividualPriceTable.SqlServer.sql
+++ config/SqlCore/Install/Quoting/CreateTables/CreateQuoteIndividualPriceTable.SqlServer.sql
@@ -6,5 +6,5 @@ CREATE TABLE QuoteIndividualPrice
 	[ProductOfferingId] [int] NOT NULL FOREIGN KEY REFERENCES t_po(id_po),
 	[PriceableItemInstanceId] [int] NOT NULL FOREIGN KEY REFERENCES t_pi(id_pi),
 	[ParameterTableId] [int] NOT NULL FOREIGN KEY REFERENCES t_rulesetdefinition(id_paramtable),
-	[RateSchedule] [xml] NOT NULL
+	[RateSchedules] [xml] NOT NULL
 )

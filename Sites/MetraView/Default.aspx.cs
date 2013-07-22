3e575e62950481e8ad2d0ff2ab8f11dc868fd7e9
Andrey Degtyarev <adegtyarev@metratech.com>
2013-07-22 02:44:40 -0400

    DE37

--- Sites/MetraView/Default.aspx.cs
+++ Sites/MetraView/Default.aspx.cs
@@ -4,14 +4,16 @@ using MetraTech.UI.Common;
 public partial class _Default : MTPage
 {
 
-  protected void Page_Load(object sender, EventArgs e)
-  {
-    Session[SiteConstants.ActiveMenu] = "Home";
-
-    if (!string.IsNullOrEmpty(Request.QueryString["URL"]))
+    protected void Page_Load(object sender, EventArgs e)
     {
-      Response.Redirect(Request.QueryString["URL"]);
+        Session[SiteConstants.ActiveMenu] = "Home";
+
+        var crypto = false;
+        if (!string.IsNullOrEmpty(Request.QueryString["Crypto"]))
+            bool.TryParse(Request.QueryString["Crypto"], out crypto);
+        
+        if (!string.IsNullOrEmpty(Request.QueryString["URL"]))
+            Response.Redirect(crypto ? Decrypt(Request.QueryString["URL"]) : Request.QueryString["URL"]);
     }
-  }
 
 }

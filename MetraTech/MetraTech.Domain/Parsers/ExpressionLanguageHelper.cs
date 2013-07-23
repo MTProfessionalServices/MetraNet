c5801cf4443ed5d8848213b74eecbcb710d39d34
unknown <Administrator@devtmploraw.metratech.com>
2013-07-23 14:15:26 -0400

    US15

--- MetraTech/MetraTech.Domain/Parsers/ExpressionLanguageHelper.cs
+++ MetraTech/MetraTech.Domain/Parsers/ExpressionLanguageHelper.cs
@@ -1,4 +1,6 @@
-﻿using Antlr4.Runtime;
+﻿using System;
+using System.Collections.Generic;
+using Antlr4.Runtime;
 using MetraTech.Domain.Expressions;
 
 namespace MetraTech.Domain.Parsers
@@ -10,8 +12,15 @@ namespace MetraTech.Domain.Parsers
             var lex = new ExpressionLanguageLexer(new AntlrInputStream(input));
             var tokens = new CommonTokenStream(lex);
             var parser = new ExpressionLanguageParser(tokens);
-            var visitor = new ExpressionBuilder();
+            parser.RemoveErrorListeners(); // remove default ConsoleErrorListener
+            var syntaxErrors = new List<SyntaxErrorDetail>();
+            parser.AddErrorListener(new ExpressionErrorListener(syntaxErrors));
             var context = parser.parse();
+            if (syntaxErrors.Count > 0)
+            {
+                throw new ExpressionParsingException(syntaxErrors);
+            }
+            var visitor = new ExpressionBuilder();
             var expression = visitor.Visit(context);
             return expression;
         }

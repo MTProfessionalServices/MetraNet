8700efa55d131a5a418e6f86017e31e3dc11f084
Sergey Letushov <sletushov@metratech.com>
2013-07-25 05:14:49 -0400

    Fixed namespaces for QuoteRequest and QuoteResponse classes
    Code Reviewer: ASkrinnik

--- QuotingTestConsole/QuotingConsoleForTesting/Program.cs
+++ QuotingTestConsole/QuotingConsoleForTesting/Program.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Xml.Serialization;
-using MetraTech.DomainModel.BaseTypes;
-using MetraTech.Interop.MTProductCatalog;
+using MetraTech.Domain.Quoting;
 using MetraTech.Core.Services.ClientProxies;
 
 namespace MetraTech.Quoting.Test.ConsoleForTesting

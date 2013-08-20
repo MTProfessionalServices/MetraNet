bff78c121ad03badf8a4af34e9a881b2ce37d2d8
dgaz <dgaz@metratech.com>
2013-08-20 09:41:01 -0400

    Fixing build scripts on Team3 branch

--- Build/Tools/makeitallNonParallel.bat
+++ Build/Tools/makeitallNonParallel.bat
@@ -7,9 +7,7 @@ if not '%1' == '' (set buildTarget=%1)
 Echo Building %buildTarget%
 Echo.
 
-rem Set source path to real source path beacause of submodule "ExpressionEngine_Internal" with source code. And some projects have references to it's source.
-setx ROOTDIR C:\MetraNet\RMP\Extensions\Legacy_Internal\Source /M 
-set ROOTDIR=C:\MetraNet\RMP\Extensions\Legacy_Internal\Source
+
 
 
 msbuild %ROOTDIR%\Build\MSBuild\MetraNetBuild.proj /t:%buildTarget% /p:config=%VERSION%;MyTargets=Build /fl /flp:ShowTimestamp;Verbosity=N;Summary;LogFile=%temp%\msbuild.log /clp:Verbosity=M;Summary

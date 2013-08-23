105a21cd6bc228e7fe7698f21aeb96a6521c47ae
msukhovarov <msukhovarov@metratech.com>
2013-08-23 03:47:35 -0400

    corrected work with pause

--- Build/Tools/Git/GitRevert.bat
+++ Build/Tools/Git/GitRevert.bat
@@ -1,4 +1,6 @@
+rem if you need skip puse after executions call "SetUnchangeConfigFiles.bat skip_pause"
+
 git reset --hard
 git submodule foreach git reset --hard
 
-call SetUnchangeConfigFiles.bat
+call SetUnchangeConfigFiles.bat %1%

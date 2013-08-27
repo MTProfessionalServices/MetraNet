bb2cc9b455a542ef0f42a1e2a701beacebfc408d
msukhovarov <msukhovarov@metratech.com>
2013-08-27 17:06:20 -0400

    added path to root dir

--- Build/Tools/Git/GitRevert.bat
+++ Build/Tools/Git/GitRevert.bat
@@ -1,7 +1,8 @@
 rem if you need skip puse after executions call "SetUnchangeConfigFiles.bat skip_pause"
-
+@pushd %DEVDIR%
 git reset --hard
 git submodule foreach git reset --hard
+@popd
 
 if NOT "%1%"=="skip_set_unchange_config" (
 call SetUnchangeConfigFiles.bat %1%

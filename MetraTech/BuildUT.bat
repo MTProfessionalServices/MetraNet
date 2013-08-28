cc3d1b513ef8931799caaf1e052547346d1fa36f
msukhovarov <msukhovarov@metratech.com>
2013-08-28 16:30:47 -0400

    TA769 : Fix build issue replaced project refs to assemblies refs

--- MetraTech/BuildUT.bat
+++ MetraTech/BuildUT.bat
@@ -1,3 +1,3 @@
 @echo off
 
-msbuild.exe MSBuild\UnitTest.proj /t:all /p:config=%VERSION% /p:platform=x86 /fl /flp:ShowTimestamp;Verbosity=DIAG;Summary;LogFile=%temp%\msbuild_FunctionalTests_MetraNet_UT.log /clp:Verbosity=M;Summary
+msbuild.exe MSBuild\UnitTest.proj /t:all /p:config=%VERSION% /p:platform=x86 /flp:ShowTimestamp;Verbosity=DIAG;Summary;LogFile=%temp%\msbuild_FunctionalTests_MetraNet_UT.log

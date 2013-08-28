ddd608e2488ec4b70cf1134aa3d019c90b69c787
msukhovarov <msukhovarov@metratech.com>
2013-08-28 05:35:03 -0400

    added output log files

--- MetraNet_LegacySmokesTests/BuildLegacyUT.bat
+++ MetraNet_LegacySmokesTests/BuildLegacyUT.bat
@@ -1,5 +1,3 @@
 @echo off
 
-msbuild.exe MSBuild\LegacySmokeTest.proj /t:all /p:config=%VERSION% /p:platform=x86
-
-pause
+msbuild.exe MSBuild\LegacySmokeTest.proj /t:all /p:config=%VERSION% /p:platform=x86 /fl /flp:ShowTimestamp;Verbosity=DIAG;Summary;LogFile=%temp%\msbuild_FunctionalTests_MetraNet_LegacySmokesTests.log /clp:Verbosity=M;Summary

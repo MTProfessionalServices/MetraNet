472d610ba9247c1584f770a04001e73b6a8a4e64
msukhovarov <msukhovarov@metratech.com>
2013-08-28 05:35:03 -0400

    added output log files

--- MetraTech/BuildUT.bat
+++ MetraTech/BuildUT.bat
@@ -1,5 +1,3 @@
 @echo off
 
-msbuild.exe MSBuild\UnitTest.proj /t:all /p:config=%VERSION% /p:platform=x86
-
-pause
+msbuild.exe MSBuild\UnitTest.proj /t:all /p:config=%VERSION% /p:platform=x86 /fl /flp:ShowTimestamp;Verbosity=DIAG;Summary;LogFile=%temp%\msbuild_FunctionalTests_MetraNet_UT.log /clp:Verbosity=M;Summary

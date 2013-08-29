afa54fc65a3ee7a07d549e171922fe82c6c45e5b
msukhovarov <msukhovarov@metratech.com>
2013-08-29 02:48:56 -0400

    TA769 : Fix build issue

--- MetraNet_LegacySmokesTests/BuildLegacyUT.bat
+++ MetraNet_LegacySmokesTests/BuildLegacyUT.bat
@@ -1,3 +1,5 @@
 @echo off
 
-msbuild.exe MSBuild\LegacySmokeTest.proj /t:all /p:config=%VERSION% /p:platform=x86 /fl /flp:ShowTimestamp;Verbosity=DIAG;Summary;LogFile=%temp%\msbuild_FunctionalTests_MetraNet_LegacySmokesTests.log /clp:Verbosity=M;Summary
+msbuild.exe MSBuild\LegacySmokeTest.proj /t:all /p:config=%VERSION% /p:platform=x86 /flp:ShowTimestamp;Verbosity=DIAG;Summary;LogFile=%temp%\msbuild_FunctionalTests_MetraNet_LegacySmokesTests.log
+
+pause

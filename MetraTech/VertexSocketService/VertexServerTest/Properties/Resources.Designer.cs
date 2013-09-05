cff3a0175c698bff3d5e8e98e961b7210b25793b
rpuzikov <rpuzikov@metratech.com>
2013-09-05 08:52:01 -0400

    Fix build test

--- MetraTech/VertexSocketService/VertexServerTest/Properties/Resources.Designer.cs
+++ MetraTech/VertexSocketService/VertexServerTest/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace VertexServerTest.Properties {
+namespace VertexServer.Test.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace VertexServerTest.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("VertexServerTest.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("VertexServer.Test.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;

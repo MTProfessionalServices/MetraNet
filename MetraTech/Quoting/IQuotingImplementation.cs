3e6351acb63a69eb6bd36e4f332a0ea35d4a8c41
msukhovarov <msukhovarov@metratech.com>
2013-09-05 05:01:34 -0400

    TA 847: Refactor QuotingImplementation class to use dependency injection pattern for implementation Unit test by using FakeItEasy Framework.
    using log is re-factored.
    Added using formatter to ILogger
    
    Reviewed bu NKuklich, YZapara

---
+++ MetraTech/Quoting/IQuotingImplementation.cs
@@ -0,0 +1,43 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ILogger.cs" company="MetraTech">
+// **************************************************************************
+// Copyright 2011 by MetraTech
+// All rights reserved.
+//
+// THIS SOFTWARE IS PROVIDED "AS IS", AND MetraTech MAKES NO
+// REPRESENTATIONS OR WARRANTIES, EXPRESS OR IMPLIED. By way of
+// example, but not limitation, MetraTech MAKES NO REPRESENTATIONS OR
+// WARRANTIES OF MERCHANTABILITY OR FITNESS FOR ANY PARTICULAR PURPOSE
+// OR THAT THE USE OF THE LICENCED SOFTWARE OR DOCUMENTATION WILL NOT
+// INFRINGE ANY THIRD PARTY PATENTS, COPYRIGHTS, TRADEMARKS OR OTHER
+// RIGHTS.
+//
+// Title to copyright in this software and any associated
+// documentation shall at all times remain with MetraTech, and USER
+// agrees to preserve the same.
+//
+//
+// $Header$
+// 
+// ***************************************************************************/
+// </copyright>
+// -----------------------------------------------------------------------
+
+
+namespace MetraTech.Quoting
+{
+  using MetraTech.Domain.Quoting;
+
+  /// <summary>
+  /// Interface for quote creation
+  /// </summary>
+  public interface IQuotingImplementation
+  {
+    int StartQuote(QuoteRequest quoteRequest);
+    void AddRecurringChargesToQuote();
+    void AddNonRecurringChargesToQuote();
+    QuoteResponse FinalizeQuote();
+
+    IQuotingRepository QuotingRepository { get; }
+  }
+}

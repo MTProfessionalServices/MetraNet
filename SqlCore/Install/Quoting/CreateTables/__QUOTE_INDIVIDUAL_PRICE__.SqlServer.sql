03caac52fe212cef5cbd86a080e2b2bb1688d7c1
IPisanka <IPisanka@metratech.com>
2013-09-06 09:15:59 -0400

    TA516 (Dev) Write FT for TC: Quote for few different accounts and POs with RC, UDRC and NRC with ICB (TC_QICB_43)
    Reviewed by NKuklich

--- SqlCore/Install/Quoting/CreateTables/__QUOTE_INDIVIDUAL_PRICE__.SqlServer.sql
+++ SqlCore/Install/Quoting/CreateTables/__QUOTE_INDIVIDUAL_PRICE__.SqlServer.sql
@@ -3,7 +3,6 @@ CREATE TABLE QuoteIndividualPrice
 (
 	[Id] [uniqueidentifier] NOT NULL PRIMARY KEY,
 	[QuoteId] [int] NOT NULL,
-	[AccountId] [int] NOT NULL,
 	[ProductOfferingId] [int] NOT NULL FOREIGN KEY REFERENCES t_po(id_po),
 	[PriceableItemInstanceId] [int] NOT NULL FOREIGN KEY REFERENCES t_base_props(id_prop),
 	[ParameterTableId] [int] NOT NULL FOREIGN KEY REFERENCES t_rulesetdefinition(id_paramtable),

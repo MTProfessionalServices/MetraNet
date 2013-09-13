529d6f39515279a138944f50061b1782fb8e68f5
IPisanka <IPisanka@metratech.com>
2013-09-13 03:33:55 -0400

    TA88 (Dev) Update QuotingRepository class to save ICB prices in database
    Reviewed by YZapara

--- SqlCore/Install/Quoting/CreateTables/__QUOTE_INDIVIDUAL_PRICE__.SqlServer.sql
+++ SqlCore/Install/Quoting/CreateTables/__QUOTE_INDIVIDUAL_PRICE__.SqlServer.sql
@@ -3,8 +3,7 @@ CREATE TABLE QuoteIndividualPrice
 (
 	[Id] [uniqueidentifier] NOT NULL PRIMARY KEY,
 	[QuoteId] [int] NOT NULL,
-	[ProductOfferingId] [int] NOT NULL FOREIGN KEY REFERENCES t_po(id_po),
-	[PriceableItemInstanceId] [int] NOT NULL FOREIGN KEY REFERENCES t_base_props(id_prop),
-	[ParameterTableId] [int] NOT NULL FOREIGN KEY REFERENCES t_rulesetdefinition(id_paramtable),
-	[RateSchedules] [xml] NOT NULL
+	[CurrentChargeType] nvarchar(50) NOT NULL,	
+	[ProductOfferingId] [int] NOT NULL,
+	[ChargesRates] [xml] NOT NULL
 )

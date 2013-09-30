330406058cfa268f3d02c555a9e59defb5874623
yzapara <yzapara@metratech.com>
2013-09-30 04:17:04 -0400

    TA1175 [DEV] Implement input for ICB prices Add missed ICBForm files

---
+++ QuotingTestConsole/QuotingConsoleForTesting/ICBForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace QuotingConsoleForTesting
+{
+  partial class ICBForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.gridViewICBs = new System.Windows.Forms.DataGridView();
+      this.button1 = new System.Windows.Forms.Button();
+      this.button2 = new System.Windows.Forms.Button();
+      ((System.ComponentModel.ISupportInitialize)(this.gridViewICBs)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // gridViewICBs
+      // 
+      this.gridViewICBs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+      this.gridViewICBs.Dock = System.Windows.Forms.DockStyle.Top;
+      this.gridViewICBs.Location = new System.Drawing.Point(0, 0);
+      this.gridViewICBs.Name = "gridViewICBs";
+      this.gridViewICBs.Size = new System.Drawing.Size(480, 244);
+      this.gridViewICBs.TabIndex = 0;
+      // 
+      // button1
+      // 
+      this.button1.Location = new System.Drawing.Point(89, 255);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(75, 23);
+      this.button1.TabIndex = 1;
+      this.button1.Text = "OK";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // button2
+      // 
+      this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+      this.button2.Location = new System.Drawing.Point(306, 255);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(75, 23);
+      this.button2.TabIndex = 1;
+      this.button2.Text = "Cancel";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // ICBForm
+      // 
+      this.AcceptButton = this.button1;
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.CancelButton = this.button2;
+      this.ClientSize = new System.Drawing.Size(480, 284);
+      this.Controls.Add(this.button2);
+      this.Controls.Add(this.button1);
+      this.Controls.Add(this.gridViewICBs);
+      this.Name = "ICBForm";
+      this.Text = "ICBForm";
+      ((System.ComponentModel.ISupportInitialize)(this.gridViewICBs)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.DataGridView gridViewICBs;
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Button button2;
+  }
+}
---
+++ QuotingTestConsole/QuotingConsoleForTesting/ICBForm.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MetraTech.Domain.Quoting;
+using MetraTech.DomainModel.BaseTypes;
+
+namespace QuotingConsoleForTesting
+{
+  public partial class ICBForm : Form
+  {
+
+    private const string UnitValueColumn = "UnitValue";
+    private const string UnitAmountColumn = "UnitAmount";
+    private const string BaseAmountColumn = "BaseAmount";
+    private const string PriceColumn = "Price";
+
+    public List<IndividualPrice> icbsLocal;
+
+    private bool isUdrc = false;
+    
+    public ICBForm(BasePriceableItemInstance pi, List<IndividualPrice> icbs)
+    {
+      InitializeComponent();
+
+      icbsLocal = icbs;
+
+      gridViewICBs.Columns.Add(UnitValueColumn, UnitValueColumn);
+      gridViewICBs.Columns.Add(UnitAmountColumn, UnitAmountColumn);
+      gridViewICBs.Columns.Add(BaseAmountColumn, BaseAmountColumn);
+      gridViewICBs.Columns.Add(PriceColumn, PriceColumn);
+
+      switch (pi.PIKind)
+      {
+        case PriceableItemKinds.UnitDependentRecurring:
+          {            
+            isUdrc = true;
+            if(icbs!=null)
+              foreach (var rate in icbs.SelectMany(indPrice => indPrice.ChargesRates))
+              {
+                gridViewICBs.Rows.Add(rate.UnitValue, rate.UnitAmount, rate.BaseAmount, -1);
+              }
+
+          }
+          break;
+        case PriceableItemKinds.NonRecurring:
+          {            
+            if (icbs != null)
+              foreach (var rate in icbs.SelectMany(indPrice => indPrice.ChargesRates))
+              {
+                gridViewICBs.Rows.Add(-1,-1,-1,rate.Price);
+              }
+          }
+          break;
+        case PriceableItemKinds.Recurring:
+          {            
+            if (icbs != null)
+              foreach (var rate in icbs.SelectMany(indPrice => indPrice.ChargesRates))
+              {
+                gridViewICBs.Rows.Add(rate.Price);
+              }
+          }
+          break;
+      }
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      var icbItem = icbsLocal.First();
+      icbItem.ChargesRates.Clear();
+
+      for(var i = 0; i < gridViewICBs.Rows.Count ; i++)
+      {
+        var cr = new ChargesRate();
+        if (isUdrc)
+        {
+          cr.UnitValue = Convert.ToInt32(gridViewICBs.Rows[i].Cells[0].Value);
+          cr.UnitAmount = Convert.ToInt32(gridViewICBs.Rows[i].Cells[1].Value);
+          cr.BaseAmount = Convert.ToInt32(gridViewICBs.Rows[i].Cells[2].Value);
+        }
+        else
+        {
+          cr.Price = Convert.ToInt32(gridViewICBs.Rows[i].Cells[3].Value);
+        }
+        icbItem.ChargesRates.Add(cr);
+      }
+      icbsLocal.Clear();
+      icbsLocal.Add(icbItem);
+
+      Close();
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      Close();
+    }
+  }
+}

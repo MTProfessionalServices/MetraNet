8645f421c4b1ebe526e1934c149f9d5808cd1a57
dgaz <dgaz@metratech.com>
2013-10-01 17:01:04 -0400

    DE259
    MetraView/Activity Servces: ESR-6403 MetraView shows POs available for subscription even though the PO is not configured to be self-subscribable
    
    Fix so all Product Offerings can be subscribed to through MetraCare again and only Product Offerings with t_po.b_user_subscribe <> 'N' can be subscribed to through MetraView

--- SqlCore/Queries/ProductCatalog/__FIND_SUBSCRIBABLE_PO_PARAM__.SqlServer.sql
+++ SqlCore/Queries/ProductCatalog/__FIND_SUBSCRIBABLE_PO_PARAM__.SqlServer.sql
@@ -113,5 +113,5 @@
 					t_base_props.id_prop = t_po.id_po AND
 					t_po.id_po = template_po_map.id_po AND
 					t_po.id_po in (select id_po from vw_acc_po_restrictions where id_acc=@idAcc) AND
-					t_po.b_user_susbcribe!='N'
+					%%CURRENCYFILTER3%%
 				

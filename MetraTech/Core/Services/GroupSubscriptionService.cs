ddc37f45e4a1f0341b233e198940757b442bfd2e
Nikita Kuklich <nkuklich@metratech.com>
2013-10-11 07:39:28 -0400

    DE351:"Fix MetraTech.Core.Services.Test.GroupSubscriptionServiceTests.T04AddGroupSubscription" Add members of group subscription in a separate transaction as it is done in a real-life scenario Revert remove of problematic AddAccountBatch() method Code Reviewer: DStepanov

--- MetraTech/Core/Services/GroupSubscriptionService.cs
+++ MetraTech/Core/Services/GroupSubscriptionService.cs
@@ -1224,18 +1224,17 @@ namespace MetraTech.Core.Services
 
             #endregion
 
-            #region Add the Group Subscription Members
-
-            if (groupSubscription.Members != null && groupSubscription.Members.Items.Count > 0)
-            {
-              AddMembersToGroupSubscription(groupSubscription.GroupId.Value, groupSubscription.Members.Items);
-            }
-
-            #endregion
-
             scope.Complete();
           }
 
+          #region Add the Group Subscription Members
+
+          if (groupSubscription.Members != null && groupSubscription.Members.Items.Count > 0)
+          {
+            AddMembersToGroupSubscription(groupSubscription.GroupId.Value, groupSubscription.Members.Items);
+          }
+
+          #endregion
         }
         catch (MASBasicException masBasic)
         {
@@ -1291,19 +1290,65 @@ namespace MetraTech.Core.Services
             throw new MASBasicException("Error finding group subscription");
           }
 
-          foreach (var groupSubMember in groupSubscriptionMembers)
+          MTGSubMember mtGsubMember = null;
+          MetraTech.Interop.MTProductCatalog.IMTCollection mtCollection =
+            new MTCollection() as MetraTech.Interop.MTProductCatalog.IMTCollection;
+
+          if (groupSubscriptionMembers.Count == 1)
           {
-            var mtGsubMember = new MTGSubMember
-              {
-                AccountID = groupSubMember.AccountId.Value,
-                StartDate = groupSubMember.MembershipSpan.StartDate.Value
-              };
-            if (groupSubMember.MembershipSpan.EndDate.HasValue)
+            mtGsubMember = new MTGSubMember();
+            mtGsubMember.AccountID = groupSubscriptionMembers[0].AccountId.Value;
+            mtGsubMember.StartDate = groupSubscriptionMembers[0].MembershipSpan.StartDate.Value;
+            if (groupSubscriptionMembers[0].MembershipSpan.EndDate.HasValue)
             {
-              mtGsubMember.EndDate = groupSubMember.MembershipSpan.EndDate.Value;
+              mtGsubMember.EndDate = groupSubscriptionMembers[0].MembershipSpan.EndDate.Value;
             }
+
             mtGroupSubscription.AddAccount(mtGsubMember);
+
           }
+          else
+          {
+            foreach (GroupSubscriptionMember groupSubMember in groupSubscriptionMembers)
+            {
+              mtGsubMember = new MTGSubMember();
+              mtGsubMember.AccountID = groupSubMember.AccountId.Value;
+              mtGsubMember.StartDate = groupSubMember.MembershipSpan.StartDate.Value;
+              if (groupSubMember.MembershipSpan.EndDate.HasValue)
+              {
+                mtGsubMember.EndDate = groupSubMember.MembershipSpan.EndDate.Value;
+              }
+              mtCollection.Add(mtGsubMember);
+            }
+
+            bool modified;
+            MetraTech.Interop.MTProductCatalog.IMTRowSet errorRowset =
+                mtGroupSubscription.AddAccountBatch(mtCollection, null, out modified, null);
+
+            if (errorRowset.RecordCount > 0)
+            {
+              StringBuilder errorString = new StringBuilder();
+              string curError = "Error adding group subscription members";
+              m_Logger.LogError(curError);
+              errorString.Append(curError + "<br/>");
+
+              while (!System.Convert.ToBoolean(errorRowset.EOF))
+              {
+                curError = "Account " +
+                    ((int)errorRowset.get_Value("id_acc")).ToString() +
+                    ": " +
+                    (string)errorRowset.get_Value("description");
+
+                m_Logger.LogError(curError);
+                errorString.Append("<li>" + curError + "</li>");
+
+                errorRowset.MoveNext();
+              }
+              errorRowset.MoveFirst();
+              throw new MASBasicException(errorString.ToString());
+            }
+          }
+
         }
         catch (MASBasicException masBasic)
         {

d16c9fba07a666c290aa042a82a4e34bd688cb45
yzapara <yzapara@metratech.com>
2013-10-28 04:29:01 -0400

    CORE-6981 Queries validation in ICE populate DB with additional tables
    Reviewed by Ipisanke

--- SqlCore/Queries/ElectronicPaymentService/__GET_PROVIDER_INFO__.Common.sql
+++ SqlCore/Queries/ElectronicPaymentService/__GET_PROVIDER_INFO__.Common.sql
@@ -1,5 +1,2 @@
-/*GET_PROVIDER_INFO*/
-		begin
-          select nm_provider_info from t_payment_history_details
-					  where id_payment_transaction=@id_payment_transaction;
-		end;
+select nm_provider_info from t_payment_history_details 
+where id_payment_transaction=@id_payment_transaction

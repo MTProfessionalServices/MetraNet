c25c14da1ad6ccadc790d8a9e62ba7ba5afd88d9
Nikita Kuklich <nkuklich@metratech.com>
2013-10-31 07:27:02 -0400

    Test merge from SVN

--- upgrade/v701to702/Oracle/03_runUpgradeScript.bat
+++ upgrade/v701to702/Oracle/03_runUpgradeScript.bat
@@ -8,6 +8,10 @@ SET UserName=%1%
 IF "none%UserName%"=="none" SET /P UserName="Enter User name[ by default "NETMETER"]: "
 IF "none%UserName%"=="none" SET UserName=NETMETER
 
+SET StageDbName=%1%
+IF "none%StageDbName%"=="none" SET /P StageDbName="Enter Stage DB(User) name[ by default "NETMETER_STAGE"]: "
+IF "none%StageDbName%"=="none" SET StageDbName=NETMETER_STAGE
+
 SET Password=%2%
 IF "none%Password%"=="none" SET /P PASSWORD="Enter Password [ by default "MetraTech1" ]: "
 IF "none%Password%"=="none" SET PASSWORD=MetraTech1
@@ -18,13 +22,15 @@ IF "none%OracleServer%"=="none" SET OracleServer=localhost
 
 IF %OracleServer%==localhost (
 SET "SqlPlusStmt=%UserName%/%Password%"
+SET "SqlPlusStageStmt=%StageDbName%/%Password%"
 ) ELSE (
 SET "SqlPlusStmt=%UserName%/%Password%@%OracleServer%:1521/netmeter"
+SET "SqlPlusStageStmt=%StageDbName%/%Password%@%OracleServer%:1521/netmeter"
 )
 
 @echo Starting DB Upgrade...
 
-call runUpgradeScript_internal.bat %SqlPlusStmt% > upgrade_ora_701_702.log
+call runUpgradeScript_internal.bat %SqlPlusStmt% %SqlPlusStageStmt% > upgrade_ora_701_702.log
 
 @echo DB Upgrade finished. See results in log file "upgrade_ora_701_702.log"
 

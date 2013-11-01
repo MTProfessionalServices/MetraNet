d6e5ea721c4fbc33a11a0c2657bbc949bd5bb0d9
Zakhar Martynov <zmartynov@metratech.com>
2013-11-01 08:17:06 -0400

    CORE-6947 CLONE - TELUS: Why password is expired? Port of ESR-5840 to 7.1 Reviewer: SMalinovskiy

--- SqlCore/Queries/Security/__SECURITY_UNLOCK_ACCOUNT__.Common.sql
+++ SqlCore/Queries/Security/__SECURITY_UNLOCK_ACCOUNT__.Common.sql
@@ -2,6 +2,7 @@
           update t_user_credentials 
           set num_failures_since_login = 0,
               b_enabled = 'Y', 
-              dt_last_login = NULL
+              dt_last_login = NULL,
+              dt_expire = case when dt_expire < getdate() then NULL else dt_expire end
           where %%%UPPER%%%(nm_login) = %%%UPPER%%%(N'%%USERNAME%%') and %%%UPPER%%%(nm_space) = %%%UPPER%%%(N'%%NAME_SPACE%%')
         

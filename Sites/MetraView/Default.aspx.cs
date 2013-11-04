984fe6b8a44f894b1ee92d204c188120b26aa256
smalinovskiy <smalinovskiy@metratech.com>
2013-11-04 04:12:35 -0500

    Revert "CORE-6947 CLONE - TELUS: Why password is expired?" This reverts commit 11586931f4b7b057ada18c7a636be24c90c8663b.
    This commit broke build

--- Sites/MetraView/Default.aspx.cs
+++ Sites/MetraView/Default.aspx.cs
@@ -4,21 +4,16 @@ using MetraTech.UI.Common;
 public partial class _Default : MTPage
 {
 
-  protected void Page_Load(object sender, EventArgs e)
-  {
-    Session[SiteConstants.ActiveMenu] = "Home";
-
-       bool crypto=false; 
-    if (!string.IsNullOrEmpty(Request.QueryString["Crypto"])) 
-    { 
-      Boolean.TryParse(Request.QueryString["Crypto"],out crypto); 
-     } 
-
-
-    if (!string.IsNullOrEmpty(Request.QueryString["URL"]))
+    protected void Page_Load(object sender, EventArgs e)
     {
-      Response.Redirect(crypto ? Decrypt(Request.QueryString["URL"]) : Request.QueryString["URL"]);
+        Session[SiteConstants.ActiveMenu] = "Home";
+
+        var crypto = false;
+        if (!string.IsNullOrEmpty(Request.QueryString["Crypto"]))
+            bool.TryParse(Request.QueryString["Crypto"], out crypto);
+        
+        if (!string.IsNullOrEmpty(Request.QueryString["URL"]))
+            Response.Redirect(crypto ? Decrypt(Request.QueryString["URL"]) : Request.QueryString["URL"]);
     }
-  }
 
 }

c653bb2b272acde4859aace31ecc3fc87e338be3
smalinovskiy <smalinovskiy@metratech.com>
2013-11-04 04:12:22 -0500

    Revert "CORE-6947 CLONE - TELUS: Why password is expired?" This reverts commit 53ee29f8826306e570a357afea0e987c84f4fd7b.
    This commit broke build

--- SqlCore/Queries/Security/__SECURITY_UNLOCK_ACCOUNT__.Common.sql
+++ SqlCore/Queries/Security/__SECURITY_UNLOCK_ACCOUNT__.Common.sql
@@ -2,7 +2,6 @@
           update t_user_credentials 
           set num_failures_since_login = 0,
               b_enabled = 'Y', 
-              dt_last_login = NULL,
-              dt_expire = case when dt_expire < getdate() then NULL else dt_expire end
+              dt_last_login = NULL
           where %%%UPPER%%%(nm_login) = %%%UPPER%%%(N'%%USERNAME%%') and %%%UPPER%%%(nm_space) = %%%UPPER%%%(N'%%NAME_SPACE%%')
         

e6674cb1f3af71f11752d99b5c49a4b14f8baa82
Nikita Kuklich <nkuklich@metratech.com>
2013-11-08 07:49:05 -0500

    CORE-6848:"RC: incorrect charges for monthly account for today's subscription" Port for Oracle (Do not forbid RC interval start to be bigger than current time) Code Reviewer: MSukhovarov

--- SqlCore/Install/RecurringCharges/StoredProcedures/__CREATE_METER_INITIAL_FROM_RECUR_WINDOW__.Oracle.sql
+++ SqlCore/Install/RecurringCharges/StoredProcedures/__CREATE_METER_INITIAL_FROM_RECUR_WINDOW__.Oracle.sql
@@ -75,8 +75,7 @@ where 1=1
 /*Also no old unit values*/
     AND NOT EXISTS (SELECT 1 FROM t_recur_value trv WHERE trv.id_sub = rw.c__SubscriptionID AND trv.tt_end < dbo.MTMaxDate())
 /* Don't meter in the current interval for initial*/
-    AND pci.dt_start < currentDate
-	and ui.dt_start <= rw.c_SubscriptionStart 
+    AND ui.dt_start < currentDate
 	AND rw.c__IsAllowGenChargeByTrigger = 1;
 
    insertChargesIntoSvcTables('Initial','Initial');

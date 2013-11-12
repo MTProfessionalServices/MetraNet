2d7b32795b1351e879651890bd3b75ebca6874aa
ipisanka <ipisanka@metratech.com>
2013-11-12 04:59:47 -0500

    Revert "CORE-6966 CLONE - Crystal version of 6.8.1 GA2 is not compatible with Intercall"
    This reverts commit 8e01db01faa23e4056920a61fa5a43ac62528909.
    
    Code Reviewer: RPuzikov

--- MetraTech/Reports/CrystalEnterprise/CEReportManager.cs
+++ MetraTech/Reports/CrystalEnterprise/CEReportManager.cs
@@ -41,17 +41,15 @@ namespace MetraTech.Reports.CrystalEnterprise
 		private InfoStore mIS;
 		private string mUserName = string.Empty;
 		private string mPassword = string.Empty;
-        private string mDataSource = string.Empty;
 
 		private static string mToken = string.Empty;
-		public void LoginToReportingServer(string aServerName, string aUserName, string aPassword, string aDataSource)
+		public void LoginToReportingServer(string aServerName, string aUserName, string aPassword)
 		{
 			Debug.Assert(mLogger != null);
 			mUserName = aUserName;
 			mPassword = aPassword;
-			mDataSource = aDataSource;
 			RecordAndLogInfo
-				(string.Format("Logging into reporting provider server. Provider: {0}, Machine: {1}, User: {2}.", ProviderName, aServerName, aUserName, aDataSource));
+				(string.Format("Logging into reporting provider server. Provider: {0}, Machine: {1}, User: {2}.", ProviderName, aServerName, aUserName));
 
 			try
 			{
@@ -67,7 +65,7 @@ namespace MetraTech.Reports.CrystalEnterprise
 				{
 					RecordAndLogInfo
 						("Creating new logon session.");
-					InternalLogon(aServerName, aUserName, aPassword, aDataSource);
+					InternalLogon(aServerName, aUserName, aPassword);
 					RecordAndLogInfo
 						("Successfully created new logon session.");
 				}
@@ -79,7 +77,7 @@ namespace MetraTech.Reports.CrystalEnterprise
 				GC.Collect();
 				RecordAndLogInfo
 					("Creating new logon session.");
-				InternalLogon(aServerName, aUserName, aPassword, aDataSource);
+				InternalLogon(aServerName, aUserName, aPassword);
 				RecordAndLogInfo
 					("Successfully created new logon session.");
 			}
@@ -989,13 +987,13 @@ namespace MetraTech.Reports.CrystalEnterprise
 				mContext.RecordWarning(message);
 		}
 
-		private void InternalLogon(string aServerName, string aUserName, string aPassword, string aDataSource)
+		private void InternalLogon(string aServerName, string aUserName, string aPassword)
 		{
 			mMgr = new SessionMgr();
 			//APS runs at port 6400
 			if(mToken.Length == 0)
 			{
-				mES = mMgr.Logon(aUserName, aPassword, aServerName, aDataSource);
+				mES = mMgr.Logon(aUserName, aPassword, aServerName, "secWindowsNT");
 				mToken = mES.LogonTokenMgr.CreateLogonTokenEx(Dns.GetHostName(),/*local machine*/
 					60/*valid for 1 hour*/,
 					100/*max logons*/);
@@ -1012,12 +1010,12 @@ namespace MetraTech.Reports.CrystalEnterprise
 						(string.Format("Could not logon to Crystal provider using logon token: '{0}', attempting regular logon.", e.Message));
 					try
 					{
-						mES = mMgr.Logon(aUserName, aPassword, aServerName, aDataSource);
+						mES = mMgr.Logon(aUserName, aPassword, aServerName, "secWindowsNT");
 					}
 					catch(Exception)
 					{
 						GC.Collect();
-						mES = mMgr.Logon(aUserName, aPassword, aServerName, aDataSource);
+						mES = mMgr.Logon(aUserName, aPassword, aServerName, "secWindowsNT");
 					}
 				}
 			}

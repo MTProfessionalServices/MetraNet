f20b8f0105bc5f563c876d0da9a06b2b36ffd290
svishnevskiy <svishnevskiy@metratech.com>
2013-11-26 13:26:59 -0500

    QATO-238 Convert UsageCycle tests into Nunit ones
    Code reviewer: Kkorotkiy

---
+++ MetraNet_LegacySmokesTests/UsageServer/Unit/Cycles/AnnualCycleTypeUnitTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace MetraTech.UsageServer.Test.Unit.Cycles
+{
+  [TestFixture]
+  public class AnnualCycleTypeUnitTest
+  {
+    private DateTime _start;
+    private DateTime _end;
+    private DateTime _refDate;
+    private readonly ICycleType _cycleType = new AnnualCycleType();
+
+    [Test]
+    public void ComputeStartAndEndDateWhenRefDateAfterCycleStartDateTest()
+    {
+      _refDate = new DateTime(2013, 12, 01);
+      var cycle = new Cycle {StartDay = 31, StartMonth = 1};
+      _cycleType.ComputeStartAndEndDate(_refDate, cycle, out _start, out _end);
+
+      Assert.AreEqual(new DateTime(2013, 01, 31), _start);
+      Assert.AreEqual(new DateTime(2014, 01, 30), _end);
+    }
+
+    [Test]
+    public void ComputeStartAndEndDateWhenRefDateBeforeCycleStartDateTest()
+    {
+      _refDate = new DateTime(2013, 01, 31);
+      var cycle = new Cycle { StartDay = 1, StartMonth = 3 };
+      _cycleType.ComputeStartAndEndDate(_refDate, cycle, out _start, out _end);
+
+      Assert.AreEqual(new DateTime(2012, 03, 01), _start);
+      Assert.AreEqual(new DateTime(2013, 02, 28), _end);
+    }
+
+    [Test]
+    public void ComputeStartAndEndDateWhenRefDateBeforeCycleStartDateLeapYearTest()
+    {
+      _refDate = new DateTime(2012, 01, 31);
+      var cycle = new Cycle { StartDay = 1, StartMonth = 3 };
+      _cycleType.ComputeStartAndEndDate(_refDate, cycle, out _start, out _end);
+
+      Assert.AreEqual(new DateTime(2011, 03, 01), _start);
+      Assert.AreEqual(new DateTime(2012, 02, 29), _end);
+    }
+  }
+}
---
+++ MetraNet_LegacySmokesTests/UsageServer/Unit/Cycles/CycleUtilsUnitTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using MetraTech.TestCommon;
+using NUnit.Framework;
+
+namespace MetraTech.UsageServer.Test.Unit.Cycles
+{
+  [TestFixture]
+  public class CycleUtilsUnitTest
+  {
+    [Test]
+    public void LastDayOfMonthTest()
+    {
+      var day = new DateTime(2100, 2, 1);
+      var lastDayOfMonth = CycleUtils.LastDayOfMonth(day);
+      Assert.AreEqual(new DateTime(2100, 2, 28), lastDayOfMonth);
+    }
+
+    [Test]
+    public void MoveToDayWhenDayOfMonthGreaterThanCountOfDaysInMonthTest()
+    {
+      var day = new DateTime(2013, 4, 1);
+      var lastDayOfMonth = CycleUtils.MoveToDay(day, 31);
+      Assert.AreEqual(new DateTime(2013, 4, 30), lastDayOfMonth);
+    }
+
+    [Test]
+    public void MoveToDayWhenDayOfMonthIsLessThanCountOfDaysInMonthTest()
+    {
+      var day = new DateTime(2013, 4, 1);
+      var lastDayOfMonth = CycleUtils.MoveToDay(day, 5);
+      Assert.AreEqual(new DateTime(2013, 4, 5), lastDayOfMonth);
+    }
+
+    [Test]
+    public void ParseCycleType()
+    {
+      Assert.AreEqual(CycleType.Monthly, CycleUtils.ParseCycleType("Monthly"));
+      Assert.AreEqual(CycleType.Daily, CycleUtils.ParseCycleType("Daily"));
+      Assert.AreEqual(CycleType.Weekly, CycleUtils.ParseCycleType("Weekly"));
+      Assert.AreEqual(CycleType.BiWeekly, CycleUtils.ParseCycleType("Bi-weekly"));
+      Assert.AreEqual(CycleType.SemiMonthly, CycleUtils.ParseCycleType("Semi-monthly"));
+      Assert.AreEqual(CycleType.Quarterly, CycleUtils.ParseCycleType("Quarterly"));
+      Assert.AreEqual(CycleType.Annual, CycleUtils.ParseCycleType("Annually"));
+      Assert.AreEqual(CycleType.SemiAnnual, CycleUtils.ParseCycleType("Semi-Annually"));
+      Assert.AreEqual(CycleType.All, CycleUtils.ParseCycleType("All"));
+      ExceptionAssert.Expected<ArgumentException>(() => CycleUtils.ParseCycleType("Monthy"),
+        "Invalid cycle type name Monthy");
+    }
+  }
+}

5528d95c1932fba7db1aad267c98f9f12012fcfa
msukhovarov <msukhovarov@metratech.com>
2014-01-03 13:45:39 -0500

    FEAT-3106 - Improve Miscellaneous Adjustments To Include Tax Amount Task : FEAT-3224 [Issue] JavaScript calculation added + in regex pattern to shoe wrong total Amount for the feature fix
    TODO: Fix issue

--- Adjustments/IssueMiscellaneousAdjustment.aspx.js
+++ Adjustments/IssueMiscellaneousAdjustment.aspx.js
@@ -4,14 +4,14 @@
       $("#adjustmentSummary input:not([id$='adjAmountFldTaxToatl'])").live("keyup", function () {
         var totalSum = 0.00;
          $.each($("#adjustmentSummary input:not([id$='adjAmountFldTaxToatl'])"),function() {
-            totalSum += Number($(this).val()) + 0.00;
+            totalSum += Number($(this).val());
          });
 
       var reg = /\./;
       if (!reg.test(totalSum))
         totalSum += '.00';
 
-      var regOnlyDec = /\d+\.\d\d$/;
+      var regOnlyDec = /\d+\.\d\d+$/;
       if (!regOnlyDec.test(totalSum))
           totalSum = '';
 

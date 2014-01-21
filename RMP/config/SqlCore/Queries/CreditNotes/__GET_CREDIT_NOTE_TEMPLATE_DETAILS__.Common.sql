3b3e191878447618947b84c4d19f728980f3c1eb
damin <damin@metratech.com>
2014-01-21 12:27:00 -0500

    FEAT-3262: Retrieve list of credit note templates. Code review by dgaz

---
+++ RMP/config/SqlCore/Queries/CreditNotes/__GET_CREDIT_NOTE_TEMPLATE_DETAILS__.Common.sql
@@ -0,0 +1,8 @@
+
+              select
+                c_TemplateID TemplateID,
+				c_TemplateName TemplateName
+              from t_be_cor_cre_creditnotetemp 
+              where c_LanguageCode = %%ID_LANG_CODE%%
+
+			

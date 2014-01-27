134d97d8bd19043539dfbd4477e560ed71ea77aa
Nikita Kuklich <nkuklich@metratech.com>
2014-01-27 10:24:54 -0500

    FEAT-3149:"Approval of independent subscriptions" Task: FEAT-3313:"Port SetSubscriptionDate Approvals logic to WF"
    Changes ported to WorkFlow.
    
    Code Reviewer: MSukhovarov

--- Subscriptions/SetSubscriptionDate.aspx.cs
+++ Subscriptions/SetSubscriptionDate.aspx.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.ServiceModel;
+using System.ServiceModel.Security;
+using System.Text;
 using MetraTech;
-using MetraTech.Approvals;
 using MetraTech.Approvals.ChangeTypes;
 using MetraTech.UI.Common;
 using MetraTech.Core.Services.ClientProxies;
@@ -112,7 +115,21 @@ public partial class Subscriptions_SetSubscriptionDate : MTPage
     SpecCharacteristicsBinder.UnbindProperies(charVals, pnlSubscriptionProperties, SpecValues);
     SubscriptionInstance.CharacteristicValues = charVals;
 
-    ProcessTheSubscription(SubscriptionInstance);
+    var isApprovalsEnabled = IsApprovalsEnabled(SubscriptionChangeType.AddSubscriptionChangeTypeName);
+
+    var update = new SubscriptionsEvents_OKSetSubscriptionDate_Client
+      {
+        In_SubscriptionInstance = SubscriptionInstance,
+        In_AccountId = new AccountIdentifier(UI.User.AccountId),
+        In_IsApprovalEnabled = isApprovalsEnabled
+      };
+    
+    PageNav.Execute(update);
+
+    if (isApprovalsEnabled)
+    {
+      Response.Redirect("/MetraNet/ApprovalFrameworkManagement/ChangeSubmittedConfirmation.aspx", false);
+    }
   }
   
   protected void btnCancel_Click(object sender, EventArgs e)
@@ -127,80 +144,7 @@ public partial class Subscriptions_SetSubscriptionDate : MTPage
   #endregion
 
   #region Private Methods
-
-  /// <summary>
-  /// Depending on the conditions does one of the followig:
-  /// 1. Saves subscription;
-  /// 2. Goes to UDRC screen;
-  /// 3. Sending submitted change to approval, if approval is enabled
-  /// </summary>
-  private void ProcessTheSubscription(Subscription sub)
-  {
-    var isNewSubscription = sub.SubscriptionId == null;
-    var changeTypeName = isNewSubscription
-                           ? SubscriptionChangeType.AddSubscriptionChangeTypeName
-                           : SubscriptionChangeType.UpdateSubscriptionChangeTypeName;
-
-    if (IsApprovalsEnabled(changeTypeName) && !HasUdrcValues(sub))
-    {
-      var changeId = SubmitSubscriptionChangeForApproval(sub, changeTypeName, isNewSubscription);
-      Response.Redirect(
-        String.Format("/MetraNet/ApprovalFrameworkManagement/ChangeSubmittedConfirmation.aspx?ChangeId={0}", changeId),
-        false);
-    }
-    else
-    {
-      var update = new SubscriptionsEvents_OKSetSubscriptionDate_Client
-        {
-          In_SubscriptionInstance = sub,
-          In_AccountId = new AccountIdentifier(UI.User.AccountId)
-        };
-      PageNav.Execute(update);
-    }
-  }
-
-  /// <summary>
-  /// Submits a change to pending approvement using Approval Service
-  /// </summary>
-  /// <param name="sub">Subscription</param>
-  /// <param name="changeTypeName">Type of submitted change</param>
-  /// <param name="isNewSub">Is it new subscription or update of existing</param>
-  /// <returns>ID of submitted change</returns>
-  private int SubmitSubscriptionChangeForApproval(Subscription sub, string changeTypeName, bool isNewSub)
-  {
-    var subscriber = UI.Subscriber.SelectedAccount;
-    if (!subscriber._AccountID.HasValue)
-    {
-      throw new NullReferenceException("AccountID property is empty");
-    }
-    var changeNameShort = isNewSub ? "Sub.Add" : "Sub.Update";
-    var changeCommentFormat = isNewSub
-                                ? "Subscribing account '{0}' to product offering '{1}' on '{2}'"
-                                : "Updating subscription of account '{0}' to product offering '{1}' on '{2}'";
-
-    var changeDetails = new ChangeDetailsHelper();
-    changeDetails[SubscriptionChangeType.AccountIdentifierKey] = new AccountIdentifier(subscriber._AccountID.Value);
-    changeDetails[SubscriptionChangeType.SubscriptionKey] = sub;
-      // TODO:  "sub" is a 'ref' parameter of SubscriptionService.AddSubscription(). Ensure it will be tracked appropriately
-
-    var subscriptionChange = new Change
-      {
-        ChangeType = changeTypeName,
-        ChangeDetailsBlob = changeDetails.ToXml(),
-        UniqueItemId = String.Format("{0}_{1}_{2}", subscriber._AccountID.Value, sub.ProductOfferingId, changeNameShort),
-        ItemDisplayName = sub.ProductOffering.Name,
-        Comment = String.Format(changeCommentFormat, subscriber.UserName, sub.ProductOffering.Name, MetraTime.Now)
-      };
-
-    int changeId;
-    using (var client = new ApprovalManagementServiceClient())
-    {
-      SetCredantional(client.ClientCredentials);
-      client.SubmitChange(subscriptionChange, out changeId);
-    }
-    return changeId;
-  }
-
+  
   private bool IsApprovalsEnabled(string changeType)
   {
     bool isEnabled;
@@ -210,17 +154,6 @@ public partial class Subscriptions_SetSubscriptionDate : MTPage
       client.ApprovalEnabledForChangeType(changeType, out isEnabled);
     }
     return isEnabled;
-  }
-
-  private bool HasUdrcValues(Subscription sub)
-  {
-    List<UDRCInstance> listOfUdrcs;
-    using (var client = new SubscriptionServiceClient())
-    {
-      SetCredantional(client.ClientCredentials);
-      client.GetUDRCInstancesForPO(sub.ProductOfferingId, out listOfUdrcs);
-    }
-    return listOfUdrcs.Count > 0;
   }
 
   private void SetCredantional(System.ServiceModel.Description.ClientCredentials clientCredentials)

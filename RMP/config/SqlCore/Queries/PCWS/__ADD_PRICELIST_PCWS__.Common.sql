1c752d154d8cdab2a4dd8a22dac969e329a91d12
yzapara <yzapara@metratech.com>
2014-02-11 16:01:42 -0500

    FEAT-3438 Merge partition-related code changes to Partition Branch. Added missing files

--- RMP/config/SqlCore/Queries/PCWS/__ADD_PRICELIST_PCWS__.Common.sql
+++ RMP/config/SqlCore/Queries/PCWS/__ADD_PRICELIST_PCWS__.Common.sql
@@ -1,3 +1,3 @@
 
-        insert into t_pricelist (id_pricelist, n_type, nm_currency_code) values (%%ID_PL%%,%%TYPE%%,N'%%CURRENCY_CODE%%')
+      insert into t_pricelist (id_pricelist, n_type, nm_currency_code, c_PLPartitionId) values (%%ID_PL%%,%%TYPE%%,N'%%CURRENCY_CODE%%',%%PLPARTITIONID%%)
       

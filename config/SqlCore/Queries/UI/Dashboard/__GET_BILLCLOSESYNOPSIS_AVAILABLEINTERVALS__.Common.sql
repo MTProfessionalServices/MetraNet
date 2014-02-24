94b1a6e576da3a03364a36764b1c94b766c6ac67
gowriraomurthy <gmurthy@metratech.com>
2014-02-24 15:35:58 -0500

    Changes to MetraControl Dashboard queries to just return date part of datetime

--- config/SqlCore/Queries/UI/Dashboard/__GET_BILLCLOSESYNOPSIS_AVAILABLEINTERVALS__.Common.sql
+++ config/SqlCore/Queries/UI/Dashboard/__GET_BILLCLOSESYNOPSIS_AVAILABLEINTERVALS__.Common.sql
@@ -1,7 +1,7 @@
 select
 ui.id_interval,
-dt_start,
-dt_end
+CONVERT(VARCHAR(10), ui.dt_start, 101) as dt_start,
+CONVERT(VARCHAR(10), ui.dt_end, 101) as dt_end
 from t_usage_interval ui
 where ui.tx_interval_status = 'O'
 and exists ( select au.id_usage_interval from t_acc_usage au where au.id_usage_interval = ui.id_interval)

9398d607e6341160124fdea745f2bfa88a206c0e
aknowles <aknowles@metratech.com>
2014-02-24 13:44:32 -0500

    Ops Dashboard Adapter grids link to adapter details.

--- config/SqlCore/Queries/UI/Dashboard/__GET_FAILED_ADAPTERS__.Common.sql
+++ config/SqlCore/Queries/UI/Dashboard/__GET_FAILED_ADAPTERS__.Common.sql
@@ -1,4 +1,8 @@
-SELECT re.tx_display_name as Name,
+SELECT 
+rer.id_instance,
+rei.id_arg_billgroup id_billgroup,
+rei.id_arg_interval id_interval,
+re.tx_display_name as Name,
 rer.dt_end as Date,
 rer.tx_detail as Message 
 FROM t_recevent_run rer
@@ -6,5 +10,5 @@ join t_recevent_inst rei on   rei.id_instance = rer.id_instance
 join t_recevent re on re.id_event = rei.id_event
 where rei.tx_status = 'Failed'
 and rer.tx_status = 'Failed'
-and rer.dt_start > DATEADD(month, -1, getdate())
+and rer.dt_start > DATEADD(month, -1, getutcdate())
 order by Date desc

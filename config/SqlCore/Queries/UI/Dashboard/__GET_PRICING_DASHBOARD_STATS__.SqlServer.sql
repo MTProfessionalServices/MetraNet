2d6b7c78c7c3728591bb6e62f90dc37da2708360
aknowles <aknowles@metratech.com>
2014-02-25 16:55:11 -0500

    Live data for pricing engine.
    Fixing basic report so that it extends to number of rows.
    Add links to rabbitMQ EasyNet client for stats.

---
+++ config/SqlCore/Queries/UI/Dashboard/__GET_PRICING_DASHBOARD_STATS__.SqlServer.sql
@@ -0,0 +1,40 @@
+begin
+declare @StartDate datetime;
+declare @EndDate datetime;
+
+select @EndDate = getutcdate();
+select @StartDate = DATEADD(minute,-1, @EndDate)
+select
+'pipeline_wait_count' as c_category,
+count(ss.session_count) as c_count
+from t_message m with(nolock)
+inner join t_session_set ss with(nolock) on m.id_message = ss.id_message
+where 1=1
+and m.dt_assigned is null
+and m.dt_completed is null
+and m.id_pipeline is null
+union all
+select
+'scheduler_wait_count' as c_category,
+count(1) as c_count
+from mvm_scheduled_tasks tsks with(nolock)
+where 1=1
+and tsks.is_scheduled = 1
+and tsks.mvm_scheduled_dt <= @EndDate
+union all
+select /* we do not treat resubmits special */
+'pipeline_wait_seconds' as c_category,
+isnull(avg(datediff(second,m.dt_crt, isnull(m.dt_assigned, getutcdate()))),0.0) as c_count
+from t_message m with(nolock)
+where 1=1
+and isnull(m.dt_assigned, getutcdate()) between @StartDate and @EndDate
+union all
+select /* we do not treat resubmits special */
+'pipeline_seconds' as c_category,
+isnull(avg(datediff(second,m.dt_assigned, m.dt_completed)),0.0) as c_count
+from t_message m with(nolock)
+where 1=1
+and m.dt_completed between @StartDate and @EndDate
+OPTION(MAXDOP 1, FORCE ORDER)
+;
+end;

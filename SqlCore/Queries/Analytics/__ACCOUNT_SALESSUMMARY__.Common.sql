283d853d45419b0ed15e2829dd9ea5a40a6fa985
rudiperkins <rudi@metratech.com>
2014-02-27 12:37:05 -0500

    Added currency to Account 360 Sales Summary

--- SqlCore/Queries/Analytics/__ACCOUNT_SALESSUMMARY__.Common.sql
+++ SqlCore/Queries/Analytics/__ACCOUNT_SALESSUMMARY__.Common.sql
@@ -1,4 +1,4 @@
 /* For a given account, what are the Analytics we want to show as a summary from the SalesSummary datamart */
 /* TBD: MRR */
-select SUM(invoice_amount) as LTV, 1234.56 as MRR, 1012.34 as MRRPrevious from t_invoice
+select SUM(invoice_amount) as LTV, MAX(invoice_currency) as LTVCurrency, 1234.56 as MRR, 'USD' as 'MRRCurrency', 1012.34 as MRRPrevious from t_invoice
 where id_acc = %%ACCOUNT_ID%%

943286db3f2df6a11988671266cdd1d9c7c7e977
rpuzikov <rpuzikov@metratech.com>
2014-02-27 11:19:45 -0500

    CORE-7487 Partitioning to Core Integration: 'Display Name" field for the PO is not populated automatically Code reviewer: DStepanov

--- MetraOffer/ProductOfferings/CreateProductOffering.aspx.cs
+++ MetraOffer/ProductOfferings/CreateProductOffering.aspx.cs
@@ -1,50 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.IO;
-using System.Text;
-using System.Web;
-using System.Web.UI.WebControls;
-using MetraTech;
-using MetraTech.ActivityServices.Common;
-using MetraTech.ActivityServices.Services.Common;
-using MetraTech.Auth.Capabilities;
+using System.Linq;
 using MetraTech.Core.Services.ClientProxies;
-using MetraTech.DataAccess;
-using MetraTech.DomainModel.AccountTypes;
-using MetraTech.DomainModel.Enums;
-using MetraTech.DomainModel.Billing;
+using MetraTech.DomainModel.Enums.Core.Global;
 using MetraTech.DomainModel.Enums.Core.Global_SystemCurrencies;
 using MetraTech.DomainModel.ProductCatalog;
-using MetraTech.Interop.MTAuth;
 using MetraTech.UI.Common;
-using MetraTech.UI.MetraNet.App_Code;
-using System.ServiceModel;
-using MetraTech.Debug.Diagnostics;
-using System.Xml;
-using MetraTech.Interop.RCD;
 using MetraTech.DomainModel.BaseTypes;
 
-
-public partial class MetraOffer_CreateProductOffering : MTPage
+namespace MetraNet.MetraOffer.ProductOfferings
 {
+  public partial class MetraOfferCreateProductOffering : MTPage
+  {
 
-  public MetraTech.DomainModel.BaseTypes.BaseProductOffering productoffering
-  {
-    get { return ViewState["productoffering"] as BaseProductOffering; } //The ViewState labels are immaterial here..
-    set { ViewState["productoffering"] = value; }
-  }
-  
-  protected void Page_Load(object sender, EventArgs e)
-  {
-    if (!IsPostBack)
+    public BaseProductOffering ProductOffering
     {
+      get { return ViewState["productoffering"] as BaseProductOffering; } //The ViewState labels are immaterial here..
+      set { ViewState["productoffering"] = value; }
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      if (IsPostBack) return;
       try
       {
-        productoffering = new BaseProductOffering();
+        ProductOffering = new BaseProductOffering();
 
         MTGenericForm1.DataBinderInstanceName = "MTDataBinder1";
-        MTGenericForm1.RenderObjectType = productoffering.GetType();
+        MTGenericForm1.RenderObjectType = ProductOffering.GetType();
 
         //This should be same as the public property defined above 
         MTGenericForm1.RenderObjectInstanceName = "productoffering";
@@ -53,16 +35,16 @@ public partial class MetraOffer_CreateProductOffering : MTPage
         MTGenericForm1.TemplateName = "Core.UI.CreateProductOffering";
         MTGenericForm1.ReadOnly = false;
 
-        productoffering.Currency = SystemCurrencies.USD; //Default Currency set to USD on page load
+        ProductOffering.Currency = SystemCurrencies.USD; //Default Currency set to USD on page load
 
         if (PartitionLibrary.PartitionData.isPartitionUser)
         {
-          productoffering.POPartitionId = PartitionLibrary.PartitionData.POPartitionId;
-          productoffering.Name = PartitionLibrary.PartitionData.PartitionUserName+ ":";
+          ProductOffering.POPartitionId = PartitionLibrary.PartitionData.POPartitionId;
+          ProductOffering.Name = PartitionLibrary.PartitionData.PartitionUserName + ":";
         }
         else
         {
-          productoffering.POPartitionId = 1;
+          ProductOffering.POPartitionId = 1;
         }
 
         if (!MTDataBinder1.DataBind())
@@ -75,66 +57,64 @@ public partial class MetraOffer_CreateProductOffering : MTPage
         Logger.LogError(ex.ToString());
       }
     }
-  }
 
-  protected void btnOK_Click(object sender, EventArgs e)
-  {
-    if (!this.MTDataBinder1.Unbind())
+    protected void btnOK_Click(object sender, EventArgs e)
     {
-      this.Logger.LogError(this.MTDataBinder1.BindingErrors.ToHtml());
-    }
+      if (!MTDataBinder1.Unbind())
+      {
+        Logger.LogError(MTDataBinder1.BindingErrors.ToHtml());
+      }
 
-    //Initialize some read only values 
-    PartitionLibrary.RetrievePartitionInformation();
+      //Initialize some read only values 
+      PartitionLibrary.RetrievePartitionInformation();
 
-    ProductOffering newProductOffering = new ProductOffering();
+      var displayName = string.IsNullOrEmpty(ProductOffering.DisplayName)
+                          ? ProductOffering.Name
+                          : ProductOffering.DisplayName;
+      var codes = Enum.GetNames(typeof (LanguageCode));
+      var locolizedDisplayName =
+        codes.Select(code => (LanguageCode) Enum.Parse(typeof (LanguageCode), code))
+             .ToDictionary(languageCode => languageCode, languageCode => string.Format("{0} {1}{2}{3}", displayName, "{", languageCode, "}"));
 
-    //mypo = productoffering;
-    
-    //Initialize some read only values 
-    newProductOffering.Name = productoffering.Name;
-    newProductOffering.DisplayName = productoffering.DisplayName;
-    newProductOffering.Description = productoffering.Description;
-    newProductOffering.Currency = productoffering.Currency;
-    newProductOffering.CanUserSubscribe = false;
-    newProductOffering.CanUserUnsubscribe = false;
-    newProductOffering.EffectiveTimeSpan.StartDate = productoffering.EffectiveTimeSpan.StartDate;
-    newProductOffering.IsHidden = false;
-    newProductOffering.POPartitionId = productoffering.POPartitionId;
-    
-    ProductOfferingServiceClient client = null;
-    
-    try
-    {
-      client = new ProductOfferingServiceClient();
-
-      client.ClientCredentials.UserName.UserName = UI.User.UserName;
-      client.ClientCredentials.UserName.Password = UI.User.SessionPassword;
-
-      client.SaveProductOffering(ref newProductOffering);
+      var newProductOffering = new ProductOffering
+        {
+          Name = ProductOffering.Name,
+          DisplayName = displayName,
+          Description = ProductOffering.Description,
+          Currency = ProductOffering.Currency,
+          CanUserSubscribe = false,
+          CanUserUnsubscribe = false,
+          EffectiveTimeSpan = {StartDate = ProductOffering.EffectiveTimeSpan.StartDate},
+          IsHidden = false,
+          POPartitionId = ProductOffering.POPartitionId,
+          LocalizedDisplayNames = locolizedDisplayName
+        };
       
-      client.Close();
+      using (var client = new ProductOfferingServiceClient())
+        try
+        {
+          if (client.ClientCredentials != null)
+          {
+            client.ClientCredentials.UserName.UserName = UI.User.UserName;
+            client.ClientCredentials.UserName.Password = UI.User.SessionPassword;
+          }
+          client.SaveProductOffering(ref newProductOffering);
+          //From here go to PO Details Screen so that user can be update the newly created Product Offering 
+          var targetUrl = "/MetraNet/TicketToMCM.aspx?Redirect=True&URL=/MCM/default/dialog/ProductOffering.ViewEdit.Frame.asp|ID=" + newProductOffering.ProductOfferingId;
+          Response.Redirect(targetUrl, false);
 
-      //From here go to PO Details Screen so that user can be update the newly created Product Offering 
-      string targetURL = "/MetraNet/TicketToMCM.aspx?Redirect=True&URL=/MCM/default/dialog/ProductOffering.ViewEdit.Frame.asp|ID=" + newProductOffering.ProductOfferingId;
-      Response.Redirect(targetURL, false);
+        }
+        catch (Exception ex)
+        {
+          SetError(ex.Message);
+          Logger.LogError(ex.Message);
+        }
 
     }
 
-    catch (Exception ex)
+    protected void btnCancel_Click(object sender, EventArgs e)
     {
-      SetError(ex.Message);
-      this.Logger.LogError(ex.Message);
-      client.Abort();
+      Response.Redirect("ProductOfferingsList.aspx", false);
     }
-
   }
-
-  protected void btnCancel_Click(object sender, EventArgs e)
-  {
-    Response.Redirect("ProductOfferingsList.aspx", false);
-  }
-
-
-
 }

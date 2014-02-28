cc3a073bac2ddaafd96dfb43e4a41dd1149595cc
yzapara <yzapara@metratech.com>
2014-02-28 10:09:37 -0500

    CORE-7486 [DEV] Fix failed smoke tests on SQL server and Oracle in Partition branch

--- SqlCore/Queries/ProductCatalog/FIND_DEFAULT_PRICE_LIST_BY_ACCOUNT_ID.Common.sql
+++ SqlCore/Queries/ProductCatalog/FIND_DEFAULT_PRICE_LIST_BY_ACCOUNT_ID.Common.sql
@@ -1,6 +1,6 @@
 
         select t_pricelist.id_pricelist, t_pricelist.n_type, t_pricelist.nm_currency_code,
-          t_base_props.nm_name, t_base_props.nm_desc
+          t_base_props.nm_name, t_base_props.nm_desc, t_pricelist.c_PLPartitionId
         from t_av_internal
         join t_pricelist on t_pricelist.id_pricelist = t_av_internal.c_pricelist
         join t_base_props on t_base_props.id_prop = t_pricelist.id_pricelist

9ed63e3b4b6bf34fd4d8b6b3a3840dc76fd8eb58
yzapara <yzapara@metratech.com>
2014-03-04 04:49:59 -0500

    CORE-7486 [DEV] Fix failed smoke tests on SQL server and Oracle in Partition branch CORE-7519 Partitions to Core - Modify AddICBMapping stored procedure to fill c_PLPartitiionId in t_pricelist table

--- SqlCore/Install/ProductCatalog/CreateTables/__CREATE_ADDICBMAPPING__.SqlServer.sql
+++ SqlCore/Install/ProductCatalog/CreateTables/__CREATE_ADDICBMAPPING__.SqlServer.sql
@@ -1,54 +1,68 @@
 
-			create proc AddICBMapping(@id_paramtable as int,
+			create proc AddICBMapping(
+					@id_paramtable as int,
 					@id_pi_instance as int,
 					@id_sub as int,
 					@id_acc as int,
-					@id_po as int,
-          @p_systemdate as datetime)
+					@id_po as int,					
+					@p_systemdate as datetime)
 				as
 					declare @id_pi_type as int
 					declare @id_pricelist as int
 					declare @id_pi_template as int
 					declare @id_pi_instance_parent as int
 					declare @currency as nvarchar(10)
-					select @id_pi_type = id_pi_type,@id_pi_template = id_pi_template,
-					@id_pi_instance_parent = id_pi_instance_parent
-					from
-					t_pl_map where id_pi_instance = @id_pi_instance AND id_paramtable is NULL
+					declare @partitionId as int
+					
+					select 	@id_pi_type = id_pi_type,
+							@id_pi_template = id_pi_template,
+							@id_pi_instance_parent = id_pi_instance_parent
+					from t_pl_map 
+					where id_pi_instance = @id_pi_instance AND id_paramtable is NULL
 
 					--CR 10884 fix: get the price list currency from product catalog, not
 					--corporation. This will take care of the case when gsubs are generated "globally".
 					--Also, this seems to be correct for all other cases as well
 					
-					set @currency = (select pl.nm_currency_code from t_po po
-					inner join t_pricelist pl on po.id_nonshared_pl = pl.id_pricelist
-					where po.id_po = @id_po)
+					set @currency = (select pl.nm_currency_code 
+									from t_po po
+									inner join t_pricelist pl on po.id_nonshared_pl = pl.id_pricelist
+									where po.id_po = @id_po)
+					
+					set @partitionId = (select po.c_POPartitionId 
+										from t_po po
+										where po.id_po = @id_po)
 
 					insert into t_base_props (n_kind,n_name,n_display_name,n_desc) values (150,0,0,0)
+					
 					set @id_pricelist = @@identity
-					insert into t_pricelist(id_pricelist,n_type,nm_currency_code) values (@id_pricelist, 0, @currency)
+					
+					insert into t_pricelist
+						(id_pricelist,n_type,nm_currency_code,c_PLPartitionId)
+						values (@id_pricelist, 0, @currency, @partitionId)
+					
 					insert into t_pl_map(
-              id_paramtable,
-              id_pi_type,
-              id_pi_instance,
-              id_pi_template,
-              id_pi_instance_parent,
-              id_sub,
-              id_po,
-              id_pricelist,
-              b_canICB,
-              dt_modified
-              )
+					  id_paramtable,
+					  id_pi_type,
+					  id_pi_instance,
+					  id_pi_template,
+					  id_pi_instance_parent,
+					  id_sub,
+					  id_po,
+					  id_pricelist,
+					  b_canICB,
+					  dt_modified
+					  )
 					values(
-              @id_paramtable,
-              @id_pi_type,              
-              @id_pi_instance,
-              @id_pi_template,
-              @id_pi_instance_parent,
-              @id_sub,
-              @id_po,
-              @id_pricelist,
-              'N',
-              @p_systemdate
-              )
+					  @id_paramtable,
+					  @id_pi_type,              
+					  @id_pi_instance,
+					  @id_pi_template,
+					  @id_pi_instance_parent,
+					  @id_sub,
+					  @id_po,
+					  @id_pricelist,
+					  'N',
+					  @p_systemdate
+					  )
 		

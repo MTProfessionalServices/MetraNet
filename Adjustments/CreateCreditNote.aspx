279133a1c8e399d290edf8143dd64f16e95fe617
vkurylovich <vkurylovich@metratech.com>
2014-03-12 12:32:39 -0400

    FEAT-3532. Add controls to the "Create Credit Note Document for Adjustments Issued" panel

--- Adjustments/CreateCreditNote.aspx
+++ Adjustments/CreateCreditNote.aspx
@@ -4,32 +4,35 @@ meta:resourcekey="PageResource1" Culture="auto" UICulture="auto"%>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
    <MT:MTPanel ID="MTPanel1" runat="server" Text="Create Credit Note" 
+
     Collapsible="True" Collapsed="False"  meta:resourcekey="MTPanel1Resource1">
      
      &nbsp;&nbsp;&nbsp;
      <MT:MTLabel ID="lblAccount" runat="server" LabelWidth="200"/>
      <br><br/>
      
-     <MT:MTDropDown ID="ddTemplateTypes" runat="server" Label="Credit Note Template To Use" 
-      LabelWidth="200" AllowBlank="False" HideLabel="False" LabelSeparator=":" Listeners="{}"
+    <MT:MTDropDown ID="ddTemplateTypes" runat="server" Label="Credit Note Template To Use" 
+      LabelWidth="170" AllowBlank="False" HideLabel="False" LabelSeparator=":" Listeners="{}"
       meta:resourcekey="ddTemplateTypesResource1" ReadOnly="False" Enabled="True">
-     </MT:MTDropDown>
-    
-     <MT:MTTextArea ID="CommentTextBox" runat="server" Label="Comment"
+    </MT:MTDropDown>
+   
+
+    <MT:MTTextArea ID="CommentTextBox" runat="server" Label="Comment"
       AllowBlank="True" Height="200px" Width="400px" ControlHeight="200" 
       ControlWidth="400" HideLabel="False" LabelSeparator=":" LabelWidth="200" 
       Listeners="{}" MaxLength="255" meta:resourcekey="CommentTextBoxResource1" 
       MinLength="0" ReadOnly="False" Enabled="True" ValidationRegex="null" 
       XType="TextArea" XTypeNameSpace="form"/>
+      
+    <MT:MTDropDown ID="ddTimeIntervals" runat="server" Label="Adjustments issued in past" 
+     LabelWidth="170" AllowBlank="False" HideLabel="False" LabelSeparator=":" Listeners="{}"
+     meta:resourcekey="ddTimeIntervalsResource1" ReadOnly="False" Enabled="True" ControlWidth="100">
+    </MT:MTDropDown>
      
-     <MT:MTDropDown ID="ddTimeIntervals" runat="server" Label="Adjustments Issued In The Past" 
-      LabelWidth="200" AllowBlank="False" HideLabel="False" LabelSeparator=":" Listeners="{}"
-      meta:resourcekey="ddTimeIntervalsResource1" ReadOnly="False" Enabled="True">
-     </MT:MTDropDown>
      
-     <MT:MTFilterGrid ID="MTFilterGrid1" runat="server" ExtensionName="Core" 
-      TemplateFileName="CreateCreditNoteDocument.xml">
-     </MT:MTFilterGrid>
+    <MT:MTFilterGrid ID="MTFilterGrid1" runat="server" ExtensionName="Core" 
+     TemplateFileName="CreateCreditNoteDocument.xml">
+    </MT:MTFilterGrid>
     
      <div  class="x-panel-btns-ct">
      <div style="width:630px" class="x-panel-btns x-panel-btns-center"> 
--- Adjustments/CreateCreditNote.aspx.cs
+++ Adjustments/CreateCreditNote.aspx.cs
@@ -4,13 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using MetraTech.ActivityServices.Common;
+using MetraTech.Core.CreditNotes;
+using MetraTech.Core.Services.ClientProxies;
+using MetraTech.DomainModel.AccountTypes;
+using MetraTech.DomainModel.Enums;
+using MetraTech.DomainModel.Enums.Core.Global;
 using MetraTech.UI.Common;
 
 public partial class Adjustments_CreateCreditNote : MTPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+
       lblAccount.Text = String.Format("{0} ({1})", UI.Subscriber.SelectedAccount.UserName, UI.Subscriber.SelectedAccount._AccountID);
+      populateCreditNotesTemplateTypes();
+      populateDDTimeInterval();
     }
 
   protected void btnIssueCreditNote_Click(object sender, EventArgs e)
@@ -22,4 +31,64 @@ public partial class Adjustments_CreateCreditNote : MTPage
   {
     throw new NotImplementedException();
   }
+
+    private void populateDDTimeInterval()
+    {
+
+      List<string> items = new List<string>(GetLocalResourceObject("ddTimeIntervalsResource1.Text").ToString().Split(','));
+      int i = 0;
+      foreach (var item in items)
+      {
+        ddTimeIntervals.Items.Add(new ListItem { Text = item, Value = i.ToString() });
+        if (item.ToLower().Contains("30"))
+          ddTimeIntervals.SelectedIndex = i;
+
+        i++;
+      }
+    }
+
+    private void populateCreditNotesTemplateTypes()
+    {
+      CreditNoteServiceClient client = null;
+
+      try
+      {
+        client = new CreditNoteServiceClient();
+
+        if (client.ClientCredentials != null)
+        {
+          client.ClientCredentials.UserName.UserName = UI.User.UserName;
+          client.ClientCredentials.UserName.Password = UI.User.SessionPassword;
+        }
+
+        LanguageCode? languageCode = ((InternalView)UI.Subscriber.SelectedAccount.GetInternalView()).Language;
+
+        var items = new MTList<CreditNoteTmpl>();
+        client.GetCreditNoteTemplates(ref items, Convert.ToInt32(EnumHelper.GetValueByEnum(languageCode, 1)));
+        if (items.Items.Count == 0)
+        {
+          items = new MTList<CreditNoteTmpl>();
+          client.GetCreditNoteTemplates(ref items, Convert.ToInt32(EnumHelper.GetValueByEnum(LanguageCode.US, 1)));
+        }
+
+        foreach (var item in items.Items)
+        {
+          ddTemplateTypes.Items.Add(new ListItem(item.TemplateName, item.CreditNoteTemplateID.ToString()));
+        }
+      }
+      catch (Exception ex)
+      {
+        Logger.LogException("An unknown exception occurred.  Please check system logs.", ex);
+        throw;
+      }
+      finally
+      {
+        if (client != null)
+        {
+          client.Abort();
+        }
+      }
+    }
+
+
 }

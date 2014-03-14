bf80b8da80fa87c759489247fb006781d5583276
jgarbacz <jgarbacz@metratech.com>
2014-03-14 11:56:42 -0400

    sales summary - ui + hardcoded query

--- SqlCore/Queries/Analytics/__ACCOUNT_SALESSUMMARY__.Common.sql
+++ SqlCore/Queries/Analytics/__ACCOUNT_SALESSUMMARY__.Common.sql
@@ -1,4 +1,19 @@
 /* For a given account, what are the Analytics we want to show as a summary from the SalesSummary datamart */
 /* TBD: MRR */
-select SUM(invoice_amount) as LTV, MAX(invoice_currency) as LTVCurrency, 1234.56 as MRR, 'USD' as 'MRRCurrency', 1012.34 as MRRPrevious from t_invoice
+/*select id_acc, SUM(invoice_amount) as LTV, MAX(invoice_currency) as LTVCurrency, 1234.56 as MRR, 'USD' as 'MRRCurrency', 1012.34 as MRRPrevious from t_invoice
 where id_acc = %%ACCOUNT_ID%%
+group by id_acc
+
+
+subscription datamart not ready and lack of data thus hardcoding wht is returned.
+SELECT SUM(subSum.SubscriptionRevenue) as LTV ,SUM(subSum.MRR) as MRR,subSum.MRRBasePrimaryCurrency as 'Currency'
+ FROM SubscriptionSummary subSum
+
+*/
+
+
+
+
+select %%ACCOUNT_ID%% as id_acc, 1313.55 as LTV, 1234.56 as MRR, 'USD' as 'Currency'
+--FROM SubscriptionSummary
+

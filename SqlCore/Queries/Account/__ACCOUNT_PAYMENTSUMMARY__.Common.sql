610761956c24f07d3f7d6fe1a814c24227474bd6
rudiperkins <rudi@metratech.com>
2014-03-20 04:31:07 -0400

    Updated invoice and payment grids/queries

--- SqlCore/Queries/Account/__ACCOUNT_PAYMENTSUMMARY__.Common.sql
+++ SqlCore/Queries/Account/__ACCOUNT_PAYMENTSUMMARY__.Common.sql
@@ -1,11 +1,16 @@
 select
-pv.c_eventdate as dt_transaction,
-pv.c_description,
-au.amount,
-au.am_currency
+au.id_sess as PaymentId,
+pv.c_eventdate as PaymentDate,
+pv.c_description as PaymentDescription,
+'Visa Ending:4564' as PaymentTypeDescription, /* TODO: Fix */
+pv.c_Source as PaymentSource,
+pv.c_ReferenceID as PaymentReferenceId,
+au.amount as PaymentAmount,
+au.am_currency as Currency
 from t_usage_interval tui
 inner join t_pv_payment pv on pv.id_usage_interval = tui.id_interval
 inner join t_acc_usage au on au.id_usage_interval = pv.id_usage_interval and au.id_sess = pv.id_sess
 where 1=1
 and au.id_acc = %%ACCOUNT_ID%%
-and pv.c_eventdate >= %%DT_START%%
+/*and pv.c_eventdate >= */
+

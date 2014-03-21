cbf32b3fbaa08ba71771cc39a775253a3ec72979
Jeff Johnson <jjohnson@metratech.com>
2014-03-21 14:04:25 -0400

    FEAT-3667

--- SqlCore/Queries/CreditNotes/__DELETE_STALE_TEMPLATES__.Common.sql
+++ SqlCore/Queries/CreditNotes/__DELETE_STALE_TEMPLATES__.Common.sql
@@ -1,2 +1,3 @@
+/* ***** DELETES Stale Templates ***** */
 DELETE FROM t_be_cor_cre_creditnotetmpl
-WHERE c_TemplateName NOT IN (@ExistingTemplates)
+WHERE c_TemplateName NOT IN (%%ExistingTemplates%%)

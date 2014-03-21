6609275921c205acb46324fcbb7c28f9c64c0960
msukhovarov <msukhovarov@metratech.com>
2014-03-21 07:01:41 -0700

    FEAT-3363 - Make it easier to add account at any level in hierarchy Estimate
    Task:
    FEAT-3668 - Implement RenderAllowedDescAccountTypes() method with get allowed descendants "Account type"
    
    Implemented

---
+++ AjaxServices/GetAllowedDescAccountTypes.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MetraTech.UI.Common;
+
+
+public partial class AjaxServices_GetAllowedDescAccountTypes : MTPage
+{
+  protected void Page_Load(object sender, EventArgs e)
+  {
+    int id = -1;
+    if (!String.IsNullOrEmpty(Request["id"]))
+    {
+      id = int.Parse(Request["id"]);
+      Logger.LogDebug("Recieves accountID={0}", id);
+    }
+    else
+    {
+      Logger.LogDebug("accountID is not set, so all types will be returned for account hierarchy");
+    }
+
+    // account type names
+    List<string> accTypes = new List<string>
+      {
+        "CorporateAccount",
+        "DepartmentAccount"
+      };
+
+      var jss = new JavaScriptSerializer();
+      Response.Write(jss.Serialize(accTypes));
+      Response.End();
+    }
+}

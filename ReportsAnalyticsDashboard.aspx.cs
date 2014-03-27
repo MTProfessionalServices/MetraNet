345c80288b278d3ca06e295501b90d2f81fb805d
Vadim Vasilenko <vvasilenko@metratech.com>
2014-03-27 04:35:38 -0400

    Port reports MRR, New Customers and Revenue from 7.1.1-Demo to 8.0

---
+++ ReportsAnalyticsDashboard.aspx.cs
@@ -0,0 +1,28 @@
+using System;
+using MetraTech.UI.Common;
+
+public partial class ReportsAnalyticsDashboard : MTPage
+{
+  protected long previousMonth, firstMonth;
+  protected long startMonthMRR, endMonthMRR;
+
+  public ReportsAnalyticsDashboard()
+  {
+    var previousMonthAndHalf = DateTime.Now.ToUniversalTime().AddMonths(-1).AddDays(1 - DateTime.Today.Day + 15);
+    var firstMonthAndHalf = DateTime.Now.ToUniversalTime().AddMonths(-13).AddDays(1 - DateTime.Today.Day - 15);
+
+    var endMonthAndHalfForMRR = DateTime.Now.ToUniversalTime().AddMonths(12).AddDays(1 - DateTime.Today.Day + 15);
+    var startMonthAndHalfForMRR = DateTime.Now.ToUniversalTime().AddMonths(-13).AddDays(1 - DateTime.Today.Day - 15);
+
+    previousMonth = GetJSDate(previousMonthAndHalf);
+    firstMonth = GetJSDate(firstMonthAndHalf);
+
+    startMonthMRR = GetJSDate(startMonthAndHalfForMRR);
+    endMonthMRR = GetJSDate(endMonthAndHalfForMRR);
+  }
+
+  private long GetJSDate(DateTime date)
+  {
+    return (date.Ticks - new DateTime(1970, 1, 1).Ticks) / 10000;
+  }
+}

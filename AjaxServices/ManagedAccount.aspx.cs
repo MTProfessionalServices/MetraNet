aec54d5c3ba028590591b745fc45d2406d704eda
rudiperkins <rudi@metratech.com>
2014-03-28 07:39:15 -0400

    Added error handling; looks like may have uncovered issue with Response.End and unhandled exceptions

--- AjaxServices/ManagedAccount.aspx.cs
+++ AjaxServices/ManagedAccount.aspx.cs
@@ -5,6 +5,7 @@ using System.Configuration;
 using System.Data;
 using System.ServiceModel;
 using System.Text;
+using System.Threading;
 using System.Web;
 using System.Web.Script.Serialization;
 using System.Web.Security;
@@ -21,146 +22,165 @@ using MetraTech.UI.Common;
 
 public partial class AjaxServices_ManagedAccount : MTListServicePage
 {
-    string sqlQueriesPath = @"Queries\AccHierarchies";
+  string sqlQueriesPath = @"Queries\AccHierarchies";
 
-    protected void Page_Load(object sender, EventArgs e)
+  protected void Page_Load(object sender, EventArgs e)
+  {
+    //parse query name
+    String operation = Request["operation"];
+    if (string.IsNullOrEmpty(operation))
     {
-        //parse query name
-        String operation = Request["operation"];
-         if (string.IsNullOrEmpty(operation))
+      Logger.LogWarning("No query specified");
+      Response.Write("{\"Items\":[]}");
+      Response.End();
+      return;
+    }
+
+    Logger.LogInfo("operation : " + operation);
+
+    using (new HighResolutionTimer("ManagedAccount", 5000))
+    {
+      try
+      {
+        MTList<SQLRecord> items = new MTList<SQLRecord>();
+        Dictionary<string, object> paramDict = new Dictionary<string, object>();
+
+        if (!String.IsNullOrEmpty(UI.Subscriber["_AccountID"]))
         {
-            Logger.LogWarning("No query specified");
-            Response.Write("{\"Items\":[]}");
-            Response.End();
-            return;
+          paramDict.Add("%%ACCOUNT_ID%%", int.Parse(UI.Subscriber["_AccountID"]));
+        }
+        else
+        {
+          Logger.LogWarning("No account currently managed");
+          Response.Write("{\"Items\":[]}");
+          Response.End();
+          return;
         }
 
-        Logger.LogInfo("operation : " + operation);
-
-        using (new HighResolutionTimer("ManagedAccount", 5000))
+        if (operation.Equals("subscriptionsummary"))
         {
-            MTList<SQLRecord> items = new MTList<SQLRecord>();
-            Dictionary<string, object> paramDict = new Dictionary<string, object>();
-
-            if (!String.IsNullOrEmpty(UI.Subscriber["_AccountID"]))
-            {
-                paramDict.Add("%%ACCOUNT_ID%%", int.Parse(UI.Subscriber["_AccountID"]));
-            }
-            else
-            {
-                Logger.LogWarning("No account currently managed");
-                Response.Write("{\"Items\":[]}");
-                Response.End();
-                return;
-            }
-
-            if (operation.Equals("subscriptionsummary"))
-            {
-                GetData("__ACCOUNT_SUBSCRIPTIONSUMMARY__", paramDict, ref items);
-            }
-
-            if (operation.Equals("paymentsummary"))
-            {
-              GetData("__ACCOUNT_PAYMENTSUMMARY__", paramDict, ref items);
-            }
-
-            if (operation.Equals("invoicesummary"))
-            {
-              GetData("__ACCOUNT_INVOICESUMMARY__", paramDict, ref items);
-            }
-
-            if (operation.Equals("balancesummary"))
-            {
-              GetData("__ACCOUNT_BALANCESUMMARY__", paramDict, ref items);
-            }
-
-            if (operation.Equals("billingsummary"))
-            {
-              GetData("__ACCOUNT_BILLINGSUMMARY__", paramDict, ref items);
-            }
-
-            if (operation.Equals("discountcommitmentsummary"))
-            {
-              GetData("__ACCOUNT_DISCOUNTANDCOMMITMENTSUMMARY__", paramDict, ref items);
-            }
-
-            if (items.Items.Count == 0)
-            {
-                Response.Write("{\"Items\":[]}");
-                Response.End();
-                return;
-            }
-
-            string json = SerializeItems(items);
-            Logger.LogInfo("Returning " + json);
-            Response.Write(json);
-            Response.End();
+          GetData("__ACCOUNT_SUBSCRIPTIONSUMMARY__", paramDict, ref items);
         }
 
+        if (operation.Equals("paymentsummary"))
+        {
+          GetData("__ACCOUNT_PAYMENTSUMMARY__", paramDict, ref items);
+        }
+
+        if (operation.Equals("invoicesummary"))
+        {
+          GetData("__ACCOUNT_INVOICESUMMARY__", paramDict, ref items);
+        }
+
+        if (operation.Equals("balancesummary"))
+        {
+          GetData("__ACCOUNT_BALANCESUMMARY__", paramDict, ref items);
+        }
+
+        if (operation.Equals("billingsummary"))
+        {
+          GetData("__ACCOUNT_BILLINGSUMMARY__", paramDict, ref items);
+        }
+
+        if (operation.Equals("discountcommitmentsummary"))
+        {
+          GetData("__ACCOUNT_DISCOUNTANDCOMMITMENTSUMMARY__", paramDict, ref items);
+        }
+
+        if (items.Items.Count == 0)
+        {
+          Response.Write("{\"Items\":[]}");
+          Response.End();
+          return;
+        }
+        else
+        {
+          string json = SerializeItems(items);
+          Logger.LogInfo("Returning " + json);
+          Response.Write(json);
+          Response.End();
+        }
+
+      }
+      catch (ThreadAbortException ex)
+      {
+        //Looks like Response.End is deprecated/changed
+        //Might have a lot of unhandled exceptions in product from when we call response.end
+        //http://support.microsoft.com/kb/312629
+      }
+      catch (Exception ex)
+      {
+        Logger.LogError("Exception: {0} {1}", ex.Message, ex.ToString());
+        Response.Write("{\"Items\":[]}");
+        Response.End();
+        return;
+      }
+    }
+
+  }
+
+
+  private void GetData(string sqlQueryTag, Dictionary<string, object> paramDict, ref MTList<SQLRecord> items)
+  {
+
+    using (IMTConnection conn = ConnectionManager.CreateConnection())
+    {
+
+      using (IMTAdapterStatement stmt = conn.CreateAdapterStatement(sqlQueriesPath, sqlQueryTag))
+      {
+        if (paramDict != null)
+        {
+          foreach (var pair in paramDict)
+          {
+            stmt.AddParam(pair.Key, pair.Value);
+          }
+        }
+
+        using (IMTDataReader reader = stmt.ExecuteReader())
+        {
+
+          ConstructItems(reader, ref items);
+          // get the total rows that would be returned without paging
+
+        }
+      }
+
+      conn.Close();
     }
 
 
-    private void GetData(string sqlQueryTag, Dictionary<string, object> paramDict, ref MTList<SQLRecord> items)
+
+  }
+
+
+
+  protected void ConstructItems(IMTDataReader rdr, ref MTList<SQLRecord> items)
+  {
+    items.Items.Clear();
+
+    // process the results
+    while (rdr.Read())
     {
+      SQLRecord record = new SQLRecord();
 
-        using (IMTConnection conn = ConnectionManager.CreateConnection())
+
+      for (int i = 0; i < rdr.FieldCount; i++)
+      {
+        SQLField field = new SQLField();
+        field.FieldDataType = rdr.GetType(i);
+        field.FieldName = rdr.GetName(i);
+
+        if (!rdr.IsDBNull(i))
         {
-
-            using (IMTAdapterStatement stmt = conn.CreateAdapterStatement(sqlQueriesPath, sqlQueryTag))
-            {
-                if (paramDict != null)
-                {
-                    foreach (var pair in paramDict)
-                    {
-                        stmt.AddParam(pair.Key, pair.Value);
-                    }
-                }
-
-                using (IMTDataReader reader = stmt.ExecuteReader())
-                {
-
-                    ConstructItems(reader, ref items);
-                    // get the total rows that would be returned without paging
-                    
-                }
-            }
-
-            conn.Close();
+          field.FieldValue = rdr.GetValue(i);
         }
 
-        
+        record.Fields.Add(field);
+      }
 
+      items.Items.Add(record);
     }
-
-
-
-    protected void ConstructItems(IMTDataReader rdr, ref MTList<SQLRecord> items)
-    {
-        items.Items.Clear();
-
-        // process the results
-        while (rdr.Read())
-        {
-            SQLRecord record = new SQLRecord();
-
-           
-            for (int i = 0; i < rdr.FieldCount; i++)
-            {
-                SQLField field = new SQLField();
-                field.FieldDataType = rdr.GetType(i);
-                field.FieldName = rdr.GetName(i);
-
-                if (!rdr.IsDBNull(i))
-                {
-                    field.FieldValue = rdr.GetValue(i);
-                }
-
-                record.Fields.Add(field);
-            }
-
-            items.Items.Add(record);
-        }
-    }
+  }
 
   protected string SerializeItems(MTList<SQLRecord> items)
   {
@@ -168,10 +188,10 @@ public partial class AjaxServices_ManagedAccount : MTListServicePage
 
     //json.Append("{\"TotalRows\":");
     //json.Append(items.TotalRows.ToString());
-    
-      json.Append("{\"Items\":[");
 
-    for (int i = 0; i < items.Items.Count; i++ )
+    json.Append("{\"Items\":[");
+
+    for (int i = 0; i < items.Items.Count; i++)
     {
       SQLRecord record = items.Items[i];
 
@@ -208,16 +228,16 @@ public partial class AjaxServices_ManagedAccount : MTListServicePage
           }
 
 
-            string value = "0";
-            if (typeof(Byte[]) == field.FieldDataType)
-            {
-                System.Text.Encoding enc = System.Text.Encoding.ASCII;
-                value = enc.GetString((Byte[])(field.FieldValue));
-            }
-            else
-            {
-                value = field.FieldValue.ToString();
-            }
+          string value = "0";
+          if (typeof(Byte[]) == field.FieldDataType)
+          {
+            System.Text.Encoding enc = System.Text.Encoding.ASCII;
+            value = enc.GetString((Byte[])(field.FieldValue));
+          }
+          else
+          {
+            value = field.FieldValue.ToString();
+          }
 
 
           // CORE-5487 HtmlEncode the field so XSS tags don't show up in UI.
@@ -246,7 +266,7 @@ public partial class AjaxServices_ManagedAccount : MTListServicePage
     }
 
     json.Append("]");
-    
+
     json.Append("}");
 
     return json.ToString();

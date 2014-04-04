114493496a8f27b4d28a3f82d3adcd08f354eae8
akaboli <Administrator@devtmploraw.metratech.com>
2014-04-04 15:50:26 -0400

    commit 9, FEAT-3643

---
+++ SqlCore/Queries/CreditNotes/__UPSERT_CREDIT_NOTE_TEMPLATE__.SqlServer.sql
@@ -0,0 +1,29 @@
+UPDATE t_be_cor_cre_creditnotetmpl
+ SET c_LanguageCode=@LanguageCode,
+     c_CreditNotePrefix = @CreditNotePrefix,     
+	 c__version = c__version+1
+WHERE c_TemplateName=@TemplateName
+
+IF @@ROWCOUNT = 0  
+INSERT INTO t_be_cor_cre_creditnotetmpl
+           (c_CreditNoteTmpl_Id
+           ,c__version
+           ,c_internal_key
+           ,c_CreationDate
+           ,c_UpdateDate
+           ,c_UID
+           ,c_LanguageCode
+           ,c_CreditNotePrefix
+           ,c_TemplateName
+           ,c_CreditNoteTemplateID)
+     VALUES(
+            NEWID()
+           ,1
+           ,NEWID()
+           ,getdate()
+           ,getdate()
+           ,1
+           ,@LanguageCode
+           ,@CreditNotePrefix
+           ,@TemplateName
+           ,@CreditNoteTemplateID);

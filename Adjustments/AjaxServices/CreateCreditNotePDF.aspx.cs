65ed2a275e2e2ed0d17d5c9caa60a16fd8b4b10f
damin <damin@metratech.com>
2014-04-04 10:56:32 -0400

    FEAT-3567: UI task, new ajax service page that will retry pdf generation

---
+++ Adjustments/AjaxServices/CreateCreditNotePDF.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MetraTech.ActivityServices.Common;
+using MetraTech.Core.Services.ClientProxies;
+using MetraTech.Debug.Diagnostics;
+using MetraTech.UI.Common;
+
+public partial class Adjustments_AjaxServices_CreateCreditNotePDF : MTListServicePage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      var response = new AjaxResponse();
+      int creditNoteId = Convert.ToInt32(Request.Params["CreditNoteID"]);
+      int accountId = Convert.ToInt32(Request.Params["AccountID"]);
+      int languageCode = Convert.ToInt32(Request.Params["LanguageCode"]);
+      string creditNotePrefix = Convert.ToString(Request.Params["CreditNotePrefix"]);
+      string templateName = Convert.ToString(Request.Params["TemplateName"]);
+      
+      using (new HighResolutionTimer("CreateCreditNotePDF", 5000))
+      {
+        CreditNoteServiceClient client = null;
+        try
+        {
+          client = new CreditNoteServiceClient();
+
+          if (client.ClientCredentials != null)
+          {
+            client.ClientCredentials.UserName.UserName = UI.User.UserName;
+            client.ClientCredentials.UserName.Password = UI.User.SessionPassword;
+          }
+          client.CreateCreditNotePDF(creditNoteId, accountId, creditNotePrefix, templateName, languageCode);
+          response.Success = true;
+          response.Message = string.Format("Successfully submitted request to generate Credit Note pdf");
+        }
+        catch (FaultException<MASBasicFaultDetail> ex)
+        {
+          response.Success = false;
+          response.Message = ex.Detail.ErrorMessages[0];
+          Logger.LogError(ex.Detail.ErrorMessages[0]);
+        }
+        catch (Exception ex)
+        {
+          response.Success = false;
+          response.Message = ex.Message;
+          Logger.LogException("An unknown exception occurred.  Please check system logs.", ex);
+          throw;
+        }
+        finally
+        {
+          if (client != null)
+          {
+            client.Abort();
+          }
+          Response.Write(response.ToJson());
+          Response.End();
+        }
+      }
+    }
+}

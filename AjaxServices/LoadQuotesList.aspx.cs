745ad1a3f37bc11f9a14c2c1fbd419de99a22670
yzapara <yzapara@metratech.com>
2014-04-14 10:38:54 -0400

    CORE-7712 Quoting API methods allow a user whose do not have such permissions to work with them CORE-7713 Quoting UI: On the View all quotes page Quote status is shown as code FEAT-3460 [DEV] Fix search by date
    Review by RPuzikov

--- AjaxServices/LoadQuotesList.aspx.cs
+++ AjaxServices/LoadQuotesList.aspx.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.ServiceModel;
+using System.Text;
 using System.Web;
-using System.Web.Script.Serialization;
 using MetraTech.ActivityServices.Common;
-using MetraTech.Core.Services.ClientProxies;
+using MetraTech.BusinessEntity.DataAccess.Metadata;
+using MetraTech.BusinessEntity.Service.ClientProxies;
 using MetraTech.Debug.Diagnostics;
-using MetraTech.Domain.Quoting;
+using MetraTech.DomainModel.Enums;
+using MetraTech.SecurityFramework;
 using MetraTech.UI.Common;
 
 namespace MetraNet.AjaxServices
@@ -14,24 +16,23 @@ namespace MetraNet.AjaxServices
   {
     private const int MaxRecordsPerBatch = 50;
 
-    protected bool ExtractDataInternal(ref MTList<Quote> items, int batchID, int limit)
+    protected bool ExtractDataInternal(ref MTList<EntityInstance> items, int batchID, int limit)
     {
       try
-      {
-        using (var client = new QuotingServiceClient())
-        {
-          if (client.ClientCredentials != null)
-          {
-            client.ClientCredentials.UserName.UserName = UI.User.UserName;
-            client.ClientCredentials.UserName.Password = UI.User.SessionPassword;
-          }
+      {        
+          var client = new EntityInstanceService_LoadEntityInstances_Client {UserName = UI.User.UserName};
+          client.Password = UI.User.SessionPassword;
+
           items.Items.Clear();
 
           items.PageSize = limit;
           items.CurrentPage = batchID;
 
-          client.GetQuotes(ref items);
-        }
+          client.In_entityName = "Core.Quoting.QuoteHeader";
+          client.InOut_entityInstances = items;
+
+          client.Invoke();
+          items = client.InOut_entityInstances;        
       }
       catch (FaultException<MASBasicFaultDetail> ex)
       {
@@ -51,7 +52,7 @@ namespace MetraNet.AjaxServices
       return true;
     }
 
-    protected bool ExtractData(ref MTList<Quote> items)
+    protected bool ExtractData(ref MTList<EntityInstance> items)
     {
       if (Page.Request["mode"] == "csv")
       {
@@ -92,35 +93,35 @@ namespace MetraNet.AjaxServices
       using (new HighResolutionTimer("LoadQuoteList", 5000))
       {
         try
-        {  
-            var items = new MTList<Quote>();
+        {
+          var items = new MTList<EntityInstance>();
 
-            SetPaging(items);
-            SetSorting(items);
-            SetFilters(items);
+          SetPaging(items);
+          SetSorting(items);
+          SetFilters(items);
 
-            //unable to extract data
-            if (!ExtractData(ref items))
-            {
-              return;
-            }
+          //unable to extract data
+          if (!ExtractData(ref items))
+          {
+            return;
+          }
 
-            if (items.Items.Count == 0)
-            {
-              Response.Write("{\"TotalRows\":\"0\",\"Items\":[]}");
-              HttpContext.Current.ApplicationInstance.CompleteRequest();
-              return;
-            }
+          if (items.Items.Count == 0)
+          {
+            Response.Write("{\"TotalRows\":\"0\",\"Items\":[]}");
+            HttpContext.Current.ApplicationInstance.CompleteRequest();
+            return;
+          }
 
-            if (Page.Request["mode"] != "csv")
-            {
-              //convert adjustments into JSON
-              var jss = new JavaScriptSerializer();
-              string json = jss.Serialize(items);
-              json = FixJsonDate(json);
-              json = FixJsonBigInt(json);
-              Response.Write(json);
-            }
+          if (Page.Request["mode"] != "csv")
+          {
+            //convert adjustments into JSON
+            //var jss = new JavaScriptSerializer();
+            string json = SerialiseItemsToJason(items);
+            json = FixJsonDate(json);
+            json = FixJsonBigInt(json);
+            Response.Write(json);
+          }
         }
         catch
           (Exception ex)
@@ -135,5 +136,146 @@ namespace MetraNet.AjaxServices
 
       }
     }
+    //for BME entity
+    private string SerialiseItemsToJason(MTList<EntityInstance> items)
+    {
+      var json = new StringBuilder();
+
+      json.Append("{\"TotalRows\":");
+// ReSharper disable SpecifyACultureInStringConversionExplicitly
+      json.Append(items.TotalRows.ToString());
+      json.Append(", \"Items\":[");
+
+      int j = 0;
+      foreach (var entityInstance in items.Items)
+      {
+        if (!(j == 0 || j == items.Items.Count))
+        {
+          json.Append(",");
+        }
+
+        json.Append("{");
+
+        // add internalId to each row
+        json.Append("\"internalId\":");
+        json.Append("\"");
+        json.Append(entityInstance.Id);
+        json.Append("\",");
+
+        int i = 0;
+        foreach (PropertyInstance propertyInstance in entityInstance.Properties)
+        {
+          if (!(i == 0 || i == entityInstance.Properties.Count))
+          {
+            json.Append(",");
+          }
+
+          json.Append("\"");
+          json.Append(propertyInstance.Name);
+          json.Append("\":");
+
+          object dispalyValue = null;
+
+          if (propertyInstance.Value == null || string.IsNullOrEmpty(propertyInstance.Value.ToString()))
+          {
+            json.Append("null");
+          }
+          else
+          {
+            dispalyValue = (propertyInstance.PropertyType == MetraTech.BusinessEntity.Core.PropertyType.Enum
+                              ? EnumHelper.GetEnumEntryName(propertyInstance.Value)
+                              : propertyInstance.Value.ToString()).EncodeForJavaScript();
+
+            if (propertyInstance.PropertyType == MetraTech.BusinessEntity.Core.PropertyType.String ||
+                propertyInstance.PropertyType == MetraTech.BusinessEntity.Core.PropertyType.Enum ||
+                propertyInstance.PropertyType == MetraTech.BusinessEntity.Core.PropertyType.DateTime ||
+                propertyInstance.PropertyType == MetraTech.BusinessEntity.Core.PropertyType.Decimal ||
+                propertyInstance.PropertyType == MetraTech.BusinessEntity.Core.PropertyType.Guid)
+            {
+              json.Append("\"");
+            }
+
+            if (propertyInstance.PropertyType == MetraTech.BusinessEntity.Core.PropertyType.Boolean)
+            {
+              // boolean is lowercase in javascript
+              json.Append(propertyInstance.Value.ToString().ToLower());
+            }
+            else
+            {
+              json.Append(dispalyValue);
+            }
+
+            if (propertyInstance.PropertyType == MetraTech.BusinessEntity.Core.PropertyType.String ||
+                propertyInstance.PropertyType == MetraTech.BusinessEntity.Core.PropertyType.Enum ||
+                propertyInstance.PropertyType == MetraTech.BusinessEntity.Core.PropertyType.DateTime ||
+                propertyInstance.PropertyType == MetraTech.BusinessEntity.Core.PropertyType.Decimal ||
+                propertyInstance.PropertyType == MetraTech.BusinessEntity.Core.PropertyType.Guid)
+            {
+              json.Append("\"");
+            }
+          }
+
+          // Display Name
+          json.Append(",");
+          json.Append("\"");
+          json.Append(propertyInstance.Name.EncodeForJavaScript());
+          json.Append("DisplayName");
+          json.Append("\":");
+          json.Append("\"");
+          json.Append(propertyInstance.Name.EncodeForJavaScript()); // TODO:  KAB: Localized label
+          json.Append("\"");
+
+          // Value Display Name for enums
+          if (propertyInstance.PropertyType == MetraTech.BusinessEntity.Core.PropertyType.Enum)
+          {
+            json.Append(",");
+            json.Append("\"");
+            json.Append(propertyInstance.Name.EncodeForJavaScript());
+            json.Append("ValueDisplayName");
+            json.Append("\":");
+            json.Append("\"");
+
+            if (dispalyValue != null)
+            {
+              json.Append(dispalyValue); // TODO:  KAB: Localized label
+            }
+
+            json.Append("\"");
+          }
+
+          i++;
+        }
+
+        json.Append("}");
+        j++;
+      }
+
+      json.Append("]");
+      json.Append(", \"CurrentPage\":");
+      json.Append(items.CurrentPage.ToString());
+      json.Append(", \"PageSize\":");
+      json.Append(items.PageSize.ToString());
+      //json.Append(", \"Filters\":");
+      //json.Append(jss.Serialize(items.Filters));
+      json.Append(", \"SortProperty\":");
+      if (items.SortCriteria == null || items.SortCriteria.Count == 0)
+      {
+        json.Append("null");
+        json.Append(", \"SortDirection\":\"");
+        json.Append(SortType.Ascending.ToString());
+      }
+      else
+      {
+        json.Append("\"");
+        json.Append(items.SortCriteria[0].SortProperty);
+        json.Append("\"");
+        json.Append(", \"SortDirection\":\"");
+        json.Append(items.SortCriteria[0].SortDirection.ToString());
+
+      }
+      json.Append("\"}");
+      return json.ToString();
+    }
+
   }
 }

ea5068fcfc35d6c4a638fcddda2996081ff88005
msukhovarov <msukhovarov@metratech.com>
2014-04-30 07:18:34 -0700

    CORE-7749 - "[Bling] Ice validation (MSSQL) delta: 50 errors; 2 warnings; 3 messages"
    Fixed 26 errors on MSSQL
    TODO 61(in bling) - 37 (in develop) = 24 error is remained

--- config/SqlCore/Queries/UI/Dashboard/__SubscriptionSummary_TopMRR__.Common.sql
+++ config/SqlCore/Queries/UI/Dashboard/__SubscriptionSummary_TopMRR__.Common.sql
@@ -1,9 +1,23 @@
-select top 10 po.ProductOfferingName, ss.Month, sum(ss.MRRPrimaryCurrency) as 'MRR', sum(prev.MRRPrimaryCurrency) as 'MRRPrevious', sum(ss.MRRPrimaryCurrency)-sum(prev.MRRPrimaryCurrency) as 'MRRChange',
-sum(ss.TotalParticipants) as 'Subscriptions', sum(prev.TotalParticipants) as 'SubscriptionsPrevious', sum(ss.TotalParticipants)-sum(prev.TotalParticipants) as 'SubscriptionsChange',
-sum(ss.NewParticipants) as 'NewCustomers', sum(prev.NewParticipants) as 'NewCustomersPrevious', sum(ss.NewParticipants)-sum(prev.NewParticipants) as 'NewCustomersChange'
-from SubscriptionSummary ss
-inner join ProductOffering po on po.ProductOfferingId = ss.ProductOfferingId and ss.InstanceId = po.InstanceId
-left join SubscriptionSummary prev on ss.InstanceId = prev.InstanceId AND ss.ProductOfferingId = prev.ProductOfferingId AND prev.Month = DATEADD(m,-1,ss.Month)
-WHERE DATEPART(m, ss.Month) = DATEPART(m, DATEADD(m, -1, getdate())) AND DATEPART(yyyy, ss.Month) = DATEPART(yyyy, DATEADD(m, -1, getdate()))
+SELECT top 10 po.ProductOfferingName, 
+		ss.Month, 
+		sum(ss.MRRPrimaryCurrency) as MRR, 
+		sum(prev.MRRPrimaryCurrency) as MRRPrevious, 
+		sum(ss.MRRPrimaryCurrency)-sum(prev.MRRPrimaryCurrency) as MRRChange,
+		sum(ss.TotalParticipants) as Subscriptions, 
+		sum(prev.TotalParticipants) as SubscriptionsPrevious, 
+		sum(ss.TotalParticipants)-sum(prev.TotalParticipants) as SubscriptionsChange,
+		sum(ss.NewParticipants) as NewCustomers, 
+		sum(prev.NewParticipants) as NewCustomersPrevious, 
+		sum(ss.NewParticipants)-sum(prev.NewParticipants) as NewCustomersChange
+FROM SubscriptionDataMart..SubscriptionSummary ss
+INNER JOIN SubscriptionDataMart..ProductOffering po 
+ON po.ProductOfferingId = ss.ProductOfferingId 
+		AND ss.InstanceId = po.InstanceId
+LEFT JOIN SubscriptionDataMart..SubscriptionSummary prev 
+ON ss.InstanceId = prev.InstanceId 
+	AND ss.ProductOfferingId = prev.ProductOfferingId 
+	AND prev.Month = DATEADD(m,-1,ss.Month)
+WHERE DATEPART(m, ss.Month) = DATEPART(m, DATEADD(m, -1, getdate())) 
+		AND DATEPART(yyyy, ss.Month) = DATEPART(yyyy, DATEADD(m, -1, getdate()))
 GROUP BY ss.InstanceId, po.ProductOfferingName, ss.Month 
 ORDER BY sum(ss.MRRPrimaryCurrency) desc

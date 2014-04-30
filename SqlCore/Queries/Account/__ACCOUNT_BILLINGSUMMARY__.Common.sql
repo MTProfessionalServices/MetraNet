fecb21947fe5ab9b8457649e7610d79bc052e1a2
msukhovarov <msukhovarov@metratech.com>
2014-04-30 07:18:34 -0700

    CORE-7749 - "[Bling] Ice validation (MSSQL) delta: 50 errors; 2 warnings; 3 messages"
    Fixed 26 errors on MSSQL
    TODO 61(in bling) - 37 (in develop) = 24 error is remained

--- SqlCore/Queries/Account/__ACCOUNT_BILLINGSUMMARY__.Common.sql
+++ SqlCore/Queries/Account/__ACCOUNT_BILLINGSUMMARY__.Common.sql
@@ -1,16 +1,44 @@
-select a.*
-from (select dense_rank () OVER (order by invoice_date asc, id_invoice) n_order, 'Invoice' nm_type, id_interval, id_invoice id_transaction, invoice_date dt_transaction, invoice_amount n_amount, invoice_amount n_invoice_amount, current_balance n_balance_amount, ar_adj_ttl_amt+postbill_adj_ttl_amt n_adj_amount, payment_ttl_amt n_payment_amount, isnull(mrr.mrr,0.0) n_mrr_amount
-from t_invoice inv with(nolock)
-left outer join t_gsubmember mbr with(nolock) on mbr.id_acc = inv.id_acc
-left outer join t_sub sub with(nolock) on sub.id_group = mbr.id_group
-left outer join SubscriptionDataMart..SubscriptionsByMonth mrr on mrr.SubscriptionId = sub.id_sub and datepart(year, inv.invoice_date) = mrr.year and datepart(month, inv.invoice_date) = mrr.month
-where 1=1 and inv.id_acc = %%ACCOUNT_ID%%
+SELECT a.*
+FROM (select dense_rank () OVER (order by invoice_date asc, id_invoice) AS n_order, 
+		'Invoice' AS nm_type, 
+		id_interval,
+		id_invoice AS id_transaction, 
+		invoice_date AS dt_transaction, 
+		invoice_amount AS n_amount, 
+		invoice_amount AS n_invoice_amount, 
+		current_balance n_balance_amount, 
+		ar_adj_ttl_amt+postbill_adj_ttl_amt AS n_adj_amount, 
+		payment_ttl_amt AS n_payment_amount, 
+		isnull(mrr.mrr,0.0) AS n_mrr_amount
+FROM t_invoice inv with(nolock)
+LEFT OUTER JOIN t_gsubmember mbr with(nolock)
+ON mbr.id_acc = inv.id_acc
+LEFT OUTER JOIN t_sub sub with(nolock)
+ON sub.id_group = mbr.id_group
+LEFT OUTER JOIN SubscriptionDataMart..SubscriptionsByMonth mrr 
+ON mrr.SubscriptionId = sub.id_sub 
+	AND datepart(year, inv.invoice_date) = mrr.year 
+	AND datepart(month, inv.invoice_date) = mrr.month
+WHERE 1=1 AND inv.id_acc = %%ACCOUNT_ID%%
 ) a where a.n_order <= 10
-union all
-select a.*
-from (select dense_rank () OVER (order by pv.c_eventdate asc, pv.id_sess) n_order, 'Payment' nm_type, pv.id_usage_interval id_interval, pv.id_sess id_transaction, pv.c_eventdate dt_transaction, au.amount n_amount, null n_invoice_amount, null n_balance_amount, null n_adj_amount, au.amount n_payment_amount, null n_mrr_amount
-from t_pv_payment pv with(nolock)
-inner join t_acc_usage au with(nolock) on pv.id_usage_interval = au.id_usage_interval and pv.id_sess = au.id_sess
-where 1=1 and pv.c_source <> 'MT' and au.id_acc = %%ACCOUNT_ID%%
-) a where a.n_order <= 10
-order by 5 asc, 4 asc
+UNION ALL
+SELECT a.*
+FROM (select dense_rank () OVER (order by pv.c_eventdate asc, pv.id_sess) AS n_order, 
+		'Payment' AS nm_type, 
+		pv.id_usage_interval AS id_interval, 
+		pv.id_sess AS id_transaction, 
+		pv.c_eventdate AS dt_transaction, 
+		au.amount AS n_amount, 
+		null AS n_invoice_amount, 
+		null AS n_balance_amount, 
+		null AS n_adj_amount, 
+		au.amount AS n_payment_amount, 
+		null AS n_mrr_amount
+FROM t_pv_payment pv with(nolock)
+INNER JOIN t_acc_usage au with(nolock) 
+ON pv.id_usage_interval = au.id_usage_interval 
+	AND pv.id_sess = au.id_sess
+WHERE 1=1 AND pv.c_source <> 'MT'
+		AND au.id_acc = %%ACCOUNT_ID%%
+) a WHERE a.n_order <= 10
+ORDER BY 5 asc, 4 asc

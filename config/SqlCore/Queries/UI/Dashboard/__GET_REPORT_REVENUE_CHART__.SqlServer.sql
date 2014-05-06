5df050da2fd900164bf0e24602f9bdb9a5a6d493
dgaz <dgaz@metratech.com>
2014-05-06 17:17:42 -0400

    Fix ICE validation errors for Oracle queries

--- config/SqlCore/Queries/UI/Dashboard/__GET_REPORT_REVENUE_CHART__.SqlServer.sql
+++ config/SqlCore/Queries/UI/Dashboard/__GET_REPORT_REVENUE_CHART__.SqlServer.sql
@@ -1,6 +1,6 @@
 declare @dateFrom DATE, @dateTo DATE;
-SET @dateFrom = DATEADD(month, -13, GETDATE());
-SET @dateTo = GETDATE();--DATEADD(day, -1, CONVERT(DATE, CONCAT(DATEPART(month, GETDATE()),'-','01','-',DATEPART(year, GETDATE())), 110));
+SET @dateFrom = DATEADD(month, -13, GETUTCDATE());
+SET @dateTo = GETUTCDATE();--DATEADD(day, -1, CONVERT(DATE, CONCAT(DATEPART(month, GETUTCDATE()),'-','01','-',DATEPART(year, GETUTCDATE())), 110));
 SELECT CONVERT(DATE, CONCAT(DATEPART(month, i.Invoice_date),'-','01','-',DATEPART(year, i.Invoice_date)), 110) as [Date]
       ,i.Invoice_currency as [Currency]
 	  ,SUM(i.Invoice_amount) as [Amount]

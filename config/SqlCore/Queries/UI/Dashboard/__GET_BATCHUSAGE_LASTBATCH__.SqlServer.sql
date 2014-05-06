9b9da2c5f91a78bea8effc3e69996e0d6de1c886
dgaz <dgaz@metratech.com>
2014-05-06 19:37:37 -0400

    Fix ICE Validation Errors for Oracle Queries

--- config/SqlCore/Queries/UI/Dashboard/__GET_BATCHUSAGE_LASTBATCH__.SqlServer.sql
+++ config/SqlCore/Queries/UI/Dashboard/__GET_BATCHUSAGE_LASTBATCH__.SqlServer.sql
@@ -6,4 +6,4 @@ set @currentBatch = (SELECT top 1 dt_crt from t_batch where id_batch=@currentBat
 set @previousBatch = (SELECT top 1 dt_crt from t_batch where dt_crt < @currentBatch and n_expected > 0 order by dt_crt desc)
 SELECT @currentBatchID as BATCHID, DATENAME(MM, @currentBatch) + ' ' + CAST(DAY(@currentBatch) AS VARCHAR(2)) AS [DATE],
 Replace(Replace(RIGHT(CONVERT(VARCHAR, @currentBatch),7), 'AM', ' AM'), 'PM', ' PM') AS [TIME],
-DATEDIFF(MINUTE, @previousBatch, @currentBatch) as [TIME DIFF];
+DATEDIFF(MINUTE, @previousBatch, @currentBatch) as [TIME DIFF]

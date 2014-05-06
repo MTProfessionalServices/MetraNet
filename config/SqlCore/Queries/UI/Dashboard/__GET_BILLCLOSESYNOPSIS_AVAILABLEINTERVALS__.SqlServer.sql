b0d4b55038927225ad9e570ce7890e69e16fe9b2
dgaz <dgaz@metratech.com>
2014-05-06 19:11:30 -0400

    Fix ICE Validation errors for Oracle queries

--- config/SqlCore/Queries/UI/Dashboard/__GET_BILLCLOSESYNOPSIS_AVAILABLEINTERVALS__.SqlServer.sql
+++ config/SqlCore/Queries/UI/Dashboard/__GET_BILLCLOSESYNOPSIS_AVAILABLEINTERVALS__.SqlServer.sql
@@ -5,4 +5,4 @@ CONVERT(VARCHAR(10), ui.dt_end, 101) as dt_end
 from t_usage_interval ui
 where ui.tx_interval_status = 'O'
 and exists ( select au.id_usage_interval from t_acc_usage au where au.id_usage_interval = ui.id_interval)
-order by ui.dt_end;
+order by ui.dt_end

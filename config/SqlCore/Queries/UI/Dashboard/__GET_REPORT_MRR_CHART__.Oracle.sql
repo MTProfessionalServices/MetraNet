2c6968318f9fe1c1c4b32d4991644b147d82d654
msukhovarov <msukhovarov@gmail.com>
2014-05-07 09:54:05 -0400

    CORE-7778 - [Bling] Ice validation (ORACLE) delta: TBD errors; TBD warnings; TBD messages was 22 errors now 19

--- config/SqlCore/Queries/UI/Dashboard/__GET_REPORT_MRR_CHART__.Oracle.sql
+++ config/SqlCore/Queries/UI/Dashboard/__GET_REPORT_MRR_CHART__.Oracle.sql
@@ -1,9 +1,9 @@
 SELECT trunc(sm.Month,'MON'),
        st.FeeCurrency as CurrencyCode,
        SUM(sm.MRRBase + sm.MRRNew + sm.MRRRenewal + sm.MRRPriceChange + sm.MRRChurn + sm.MRRCancellation) as Amount
-FROM AnalyticsDatamart.dbo.SubscriptionsByMonth sm
-		join AnalyticsDatamart.dbo.SubscriptionTable st on sm.SubscriptionId = st.SubscriptionId
-		join AnalyticsDatamart.dbo.Customer c on st.AccountId = c.AccountId
+FROM AnalyticsDatamart.SubscriptionsByMonth sm
+		join AnalyticsDatamart.SubscriptionTable st on sm.SubscriptionId = st.SubscriptionId
+		join AnalyticsDatamart.Customer c on st.AccountId = c.AccountId
 WHERE sm.Month IS NOT NULL
 AND sm.Month >= add_months(GETUTCDATE(), -13)
 AND sm.Month < trunc(add_months(GETUTCDATE(), 12),'MON')

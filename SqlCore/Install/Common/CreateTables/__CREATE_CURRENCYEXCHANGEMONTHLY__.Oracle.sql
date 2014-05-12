33fa94054c59a08baeffba7ccc64f56273225ddc
dgaz <dgaz@metratech.com>
2014-05-12 16:26:20 -0400

    FEAT-3851 Bling: Productize Subscription Datamart (Analytics Datamart)
    FEAT-3987
    Table create script for NetMeter.CurrencyExchangeMonthly

--- SqlCore/Install/Common/CreateTables/__CREATE_CURRENCYEXCHANGEMONTHLY__.Oracle.sql
+++ SqlCore/Install/Common/CreateTables/__CREATE_CURRENCYEXCHANGEMONTHLY__.Oracle.sql
@@ -2,7 +2,7 @@ create table CurrencyExchangeMonthly
 (
 InstanceId nvarchar2(64),
 StartDate datetime NOT NULL,
-EndDate datetime,
+EndDate datetime NOT NULL,
 SourceCurrency nvarchar2(100) NOT NULL,
 TargetCurrency nvarchar2(100) NOT NULL,
 ExchangeRate number(22,10) NOT NULL

1f711a2fd198b98ad1c4d62be15ca66d1716d24b
dgaz <dgaz@metratech.com>
2014-05-12 16:45:22 -0400

    FEAT-3851 Bling: Productize Subscription Datamart (Analytics Datamart)
    FEAT-3985
    Table create script for NetMeter.SubscriptionSummary

---
+++ SqlCore/Install/Common/CreateTables/__CREATE_SUBSCRIPTIONSUMMARY__.Oracle.sql
@@ -0,0 +1,19 @@
+create table SubscriptionSummary
+(
+InstanceId nvarchar2(64),
+ProductOfferingId int NOT NULL,
+Year int NOT NULL,
+Month int NOT NULL,
+TotalParticipants int NOT NULL,
+DistinctHierarchies int NOT NULL,
+NewParticipants int NOT NULL,
+MRRPrimaryCurrency number(22,10) NOT NULL,
+MRRNewPrimaryCurrency number(22,10) NOT NULL,
+MRRBasePrimaryCurrency number(22,10) NOT NULL,
+MRRRenewalPrimaryCurrency number(22,10) NOT NULL,
+MRRPriceChangePrimaryCurrency number(22,10) NOT NULL,
+MRRChurnPrimaryCurrency number(22,10) NOT NULL,
+MRRCancelationPrimaryCurrency number(22,10) NOT NULL,
+SubscriptionRevenuePrimaryCurrency number(22,10) NOT NULL,
+DaysInMonth int NOT NULL
+)

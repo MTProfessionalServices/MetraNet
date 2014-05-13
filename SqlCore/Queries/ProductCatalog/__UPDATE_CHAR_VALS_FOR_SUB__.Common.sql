838c976966b1f652015053fabd59942bd7476cf0
apyrshev <apyrshev@metratech.com>
2014-05-13 11:00:06 -0400

    CORE-7745 CLONE - Encoding problem in subscription properties (QC-3491)

--- SqlCore/Queries/ProductCatalog/__UPDATE_CHAR_VALS_FOR_SUB__.Common.sql
+++ SqlCore/Queries/ProductCatalog/__UPDATE_CHAR_VALS_FOR_SUB__.Common.sql
@@ -1,5 +1,5 @@
 
                 update t_char_values
-                  set nm_value = '%%VALUE%%'
+                  set nm_value = N'%%VALUE%%'
                 where id_scv = %%SPEC_CHAR_VAL_ID%% and id_entity = %%ENTITY_ID%%
             

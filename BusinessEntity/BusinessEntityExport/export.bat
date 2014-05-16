aaac99ba079d169197c3bbd2d6bc2ea60729befd
akaboli <akaboli@metratech.com>
2014-05-16 16:33:56 -0400

    FeaturesFEAT-3853 Bling: Productize 'System User Reports' install and some reportsFEAT-4049
    Move the Reports BME to the SystemConfig extension

---
+++ BusinessEntity/BusinessEntityExport/export.bat
@@ -0,0 +1,2 @@
+SET LOCALPATH=%~dp0
+BMEImportExport.exe exp -E MetraTech.SysConfig.Reports.Report %LOCALPATH%
---
+++ BusinessEntity/BusinessEntityExport/import.bat
@@ -0,0 +1,2 @@
+SET LOCALPATH=%~dp0
+BMEImportExport.exe imp -I -E MetraTech.SystemConfig.Reports.Report %LOCALPATH%

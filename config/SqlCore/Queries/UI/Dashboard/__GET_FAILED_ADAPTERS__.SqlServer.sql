002519d194d8a46c589088159a1b8e94986b5c78
Nikita Kuklich <nkuklich@metratech.com>
2014-05-19 06:43:27 -0400

    FEAT-3848:"Bling: Productize Operations Dashboard" Task FEAT-3906:"Fix MSSQL & Ora Failed Adapters widget"
    1. Correct name binding of sql query and xml template;
    2. Code format/cleanup.
    
    Code Reviewer: MSukhovarov

--- config/SqlCore/Queries/UI/Dashboard/__GET_FAILED_ADAPTERS__.SqlServer.sql
+++ config/SqlCore/Queries/UI/Dashboard/__GET_FAILED_ADAPTERS__.SqlServer.sql
@@ -1,14 +1,16 @@
-SELECT 
-rer.id_instance,
-rei.id_arg_billgroup id_billgroup,
-rei.id_arg_interval id_interval,
-re.tx_display_name as DisplayName,
-rer.dt_end as EndDate,
-rer.tx_detail as Message 
-FROM t_recevent_run rer
-join t_recevent_inst rei on   rei.id_instance = rer.id_instance
-join t_recevent re on re.id_event = rei.id_event
-where rei.tx_status = 'Failed'
-and rer.tx_status = 'Failed'
-and rer.dt_start > DATEADD(month, -1, getutcdate())
-order by EndDate desc
+SELECT rer.id_instance,
+       rei.id_arg_billgroup id_billgroup,
+       rei.id_arg_interval id_interval,
+       re.tx_display_name AS name,
+       rer.dt_end AS EndDate,
+       rer.tx_detail AS message
+FROM   t_recevent_run rer
+       JOIN t_recevent_inst rei
+            ON  rei.id_instance = rer.id_instance
+       JOIN t_recevent re
+            ON  re.id_event = rei.id_event
+WHERE  rei.tx_status = 'Failed'
+       AND rer.tx_status = 'Failed'
+       AND rer.dt_start > DATEADD(MONTH, -1, GETUTCDATE())
+ORDER BY
+       EndDate DESC

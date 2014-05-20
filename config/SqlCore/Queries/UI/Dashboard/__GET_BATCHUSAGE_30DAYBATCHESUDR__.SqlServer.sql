60850b8b2b5670a7de3b517b631a67f6106c52a3
Nikita Kuklich <nkuklich@metratech.com>
2014-05-20 08:42:05 -0400

    FEAT-3848:"Bling: Productize Operations Dashboard" Task FEAT-3901:"Fix Usage Data Records Widget (MSSQL & Ora)"
    1. Don't cut chart's Y axis by Batch max number, as UDR has bigger value;
    2. Correct MSSQL and Oracle queries;
    3. On mouse over "UDRs" line wasn't highlighted - remove default color specification.
    
    Code Reviewer: MSukhovarov

--- config/SqlCore/Queries/UI/Dashboard/__GET_BATCHUSAGE_30DAYBATCHESUDR__.SqlServer.sql
+++ config/SqlCore/Queries/UI/Dashboard/__GET_BATCHUSAGE_30DAYBATCHESUDR__.SqlServer.sql
@@ -1,11 +1,25 @@
-/*create index IDX_TB_DATE ON t_batch (dt_crt desc) include(n_expected);*/
-with cte1 as(
-select datediff(day,tb.dt_crt, getutcdate()) as Day_No, count(1) as Batch_Count, sum(tb.n_expected) as UDR_Count from t_batch tb with(nolock, index(IDX_TB_DATE)) where 1=1 and datediff(day,tb.dt_crt, getutcdate()) <= 30 and tb.n_expected > 0 /*and tx_namespace = 'premconf.com/CDR'*/ group by datediff(day,tb.dt_crt, getutcdate()), cast(tb.dt_crt as date)
-),
-cte2 as(
-SELECT TOP (31) n = CONVERT(INT, ROW_NUMBER() OVER (ORDER BY s1.[object_id])) - 1
-FROM sys.all_objects AS s1 CROSS JOIN sys.all_objects AS s2
-)
-select DATEADD(day, -cte2.n, CAST(GETUTCDATE() AS DATE)) as CalendarDate, cte2.n as Day_No, IsNull(cte1.Batch_Count,0) as Batch_Count, IsNull(cte1.UDR_Count,0) as UDR_Count from cte2 left outer join cte1 on cte2.n = cte1.Day_No
-order by cte2.n asc
+WITH cte1 AS(
+                SELECT DATEDIFF(DAY, tb.dt_crt, GETUTCDATE()) AS Day_No,
+                       COUNT(1) AS Batch_Count,
+                       SUM(tb.n_completed + tb.n_failed + tb.n_dismissed) AS UDR_Count
+                FROM   t_batch tb WITH(NOLOCK, INDEX(IDX_TB_DATE))
+                WHERE  DATEDIFF(DAY, tb.dt_crt, GETUTCDATE()) <= 30
+                GROUP BY
+                       DATEDIFF(DAY, tb.dt_crt, GETUTCDATE())
+            ),
+cte2 AS(
+           SELECT TOP(31) n = CONVERT(INT, ROW_NUMBER() OVER(ORDER BY s1.[object_id])) 
+                  - 1
+           FROM   sys.all_objects AS s1
+                  CROSS JOIN sys.all_objects AS s2
+       )
+SELECT DATEADD(DAY, -cte2.n, CAST(GETUTCDATE() AS DATE)) AS CalendarDate,
+       cte2.n AS Day_No,
+       ISNULL(cte1.Batch_Count, 0) AS Batch_Count,
+       ISNULL(cte1.UDR_Count, 0) AS UDR_Count
+FROM   cte2
+       LEFT OUTER JOIN cte1
+            ON  cte2.n = cte1.Day_No
+ORDER BY
+       cte2.n ASC 
 OPTION (MAXDOP 1)
--- config/SqlCore/Queries/UI/Dashboard/__GET_BATCHUSAGE_LASTBATCH__.SqlServer.sql
+++ config/SqlCore/Queries/UI/Dashboard/__GET_BATCHUSAGE_LASTBATCH__.SqlServer.sql
@@ -1,9 +1,35 @@
-DECLARE @currentBatch DateTime
-DECLARE @currentBatchID Int
-DECLARE @previousBatch DateTime
-set @currentBatchID = (SELECT top 1 id_batch from t_batch where n_expected > 0 order by dt_crt desc)
-set @currentBatch = (SELECT top 1 dt_crt from t_batch where id_batch=@currentBatchID)
-set @previousBatch = (SELECT top 1 dt_crt from t_batch where dt_crt < @currentBatch and n_expected > 0 order by dt_crt desc)
-SELECT @currentBatchID as BATCHID, DATENAME(MM, @currentBatch) + ' ' + CAST(DAY(@currentBatch) AS VARCHAR(2)) AS [DATE],
-Replace(Replace(RIGHT(CONVERT(VARCHAR, @currentBatch),7), 'AM', ' AM'), 'PM', ' PM') AS [TIME],
-DATEDIFF(MINUTE, @previousBatch, @currentBatch) as [TIME DIFF]
+DECLARE @currentBatch DATETIME
+DECLARE @currentBatchID INT
+DECLARE @previousBatch DATETIME
+
+SET @currentBatchID = (
+        SELECT TOP 1 id_batch
+        FROM   t_batch
+        WHERE  (n_completed + n_failed + n_dismissed) > 0
+        ORDER BY
+               dt_crt DESC
+    )
+
+SET @currentBatch = (
+        SELECT TOP 1 dt_crt
+        FROM   t_batch
+        WHERE  id_batch = @currentBatchID
+    )
+
+SET @previousBatch = (
+        SELECT TOP 1 dt_crt
+        FROM   t_batch
+        WHERE  dt_crt < @currentBatch
+               AND (n_completed + n_failed + n_dismissed) > 0
+        ORDER BY
+               dt_crt DESC
+    )
+
+SELECT @currentBatchID AS BATCHID,
+       DATENAME(MM, @currentBatch) + ' ' + CAST(DAY(@currentBatch) AS VARCHAR(2))
+		 AS [DATE],
+       REPLACE(
+           REPLACE(RIGHT(CONVERT(VARCHAR, @currentBatch), 7), 'AM', ' AM'),
+           'PM', ' PM'
+       ) AS [TIME],
+       DATEDIFF(MINUTE, @previousBatch, @currentBatch) AS [TIME DIFF]

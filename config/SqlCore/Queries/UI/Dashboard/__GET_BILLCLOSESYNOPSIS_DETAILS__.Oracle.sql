a6f69a3169cbcba2505ba7d404e33800f920ee3a
msukhovarov <msukhovarov@metratech.com>
2014-05-21 17:47:01 -0400

    FEAT-3848 Bling: Productize Operations Dashboard
    Task: FEAT-3905 Verify Ora Bill Close Synopsis
    corrected Oracle queries

--- config/SqlCore/Queries/UI/Dashboard/__GET_BILLCLOSESYNOPSIS_DETAILS__.Oracle.sql
+++ config/SqlCore/Queries/UI/Dashboard/__GET_BILLCLOSESYNOPSIS_DETAILS__.Oracle.sql
@@ -3,19 +3,9 @@ SELECT
 COUNT(*) as Count
 FROM t_failed_transaction ft
 JOIN t_acc_usage_cycle cycle ON cycle.id_acc = ft.id_PossiblePayeeID
+JOIN t_usage_interval ui ON cycle.id_usage_cycle = ui.id_usage_cycle
 WHERE ft.State = 'N'
-AND cycle.id_usage_cycle = ( 
-SELECT
-      CASE EXTRACT(DAY FROM dt_end)
-         WHEN 5 THEN 7
-         WHEN 12 THEN 14
-         WHEN 19 THEN 21
-         WHEN 26 THEN 28
-         ELSE 30
-      END
-FROM t_usage_interval ui
-WHERE ui.id_interval = %%ID_USAGE_INTERVAL%%
-)
+AND  ui.id_interval = %%ID_USAGE_INTERVAL%%
 AND ft.dt_FailureTime > getutcdate()-30
 
 UNION
@@ -25,19 +15,9 @@ SELECT
 COUNT(*) as Count 
 FROM t_failed_transaction ft
 JOIN t_acc_usage_cycle cycle ON cycle.id_acc = ft.id_PossiblePayeeID
+JOIN t_usage_interval ui ON cycle.id_usage_cycle = ui.id_usage_cycle
 WHERE ft.State = 'I'
-AND cycle.id_usage_cycle = (
-SELECT
-      CASE EXTRACT(DAY FROM dt_end)
-         WHEN 5 THEN 7
-         WHEN 12 THEN 14
-         WHEN 19 THEN 21
-         WHEN 26 THEN 28
-         ELSE 30
-      END
-FROM t_usage_interval ui
-WHERE ui.id_interval = %%ID_USAGE_INTERVAL%%
-)
+AND ui.id_interval = %%ID_USAGE_INTERVAL%%
 AND ft.dt_FailureTime > getutcdate()-30
 
 UNION
@@ -46,19 +26,9 @@ SELECT 'Fixed' AS Status,
 COUNT(*) AS Count
 FROM t_failed_transaction ft
 JOIN t_acc_usage_cycle cycle ON cycle.id_acc = ft.id_PossiblePayeeID
+JOIN t_usage_interval ui ON cycle.id_usage_cycle = ui.id_usage_cycle
 WHERE ft.State = 'R'
-AND cycle.id_usage_cycle = (
-SELECT
-      CASE EXTRACT(DAY FROM dt_end)
-         WHEN 5 THEN 7
-         WHEN 12 THEN 14
-         WHEN 19 THEN 21
-         WHEN 26 THEN 28
-         ELSE 30
-      END
-FROM t_usage_interval ui
-WHERE ui.id_interval = %%ID_USAGE_INTERVAL%%
-)
+AND  ui.id_interval = %%ID_USAGE_INTERVAL%%
 AND ft.dt_FailureTime > getutcdate()-30
 
 UNION

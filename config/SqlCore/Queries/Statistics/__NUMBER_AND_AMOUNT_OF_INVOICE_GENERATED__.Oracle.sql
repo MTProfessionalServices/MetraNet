f682a5b84e679af300dbe03d1abea724e4289d46
akaboli <akaboli@metratech.com>
2014-05-23 14:37:01 -0400

    CoreCORE-7860
    [Bling] Sort Column Failed for Interval Statistics -> Invoice -> Invoice Summary

--- config/SqlCore/Queries/Statistics/__NUMBER_AND_AMOUNT_OF_INVOICE_GENERATED__.Oracle.sql
+++ config/SqlCore/Queries/Statistics/__NUMBER_AND_AMOUNT_OF_INVOICE_GENERATED__.Oracle.sql
@@ -1,8 +1,8 @@
 
-			select count(*)  "# of invoices",
-				  invoice_currency  "Currency" ,
-				  SUM(COALESCE(invoice_amount, 0.0)) - sum(COALESCE(tax_ttl_amt,0.0)) "Amount",
-				  sum(COALESCE(tax_ttl_amt,0.0)) "Tax Amount" 
+			select count(*)  "NUMOFINV",
+				  invoice_currency  "CURRENCY" ,
+				  SUM(COALESCE(invoice_amount, 0.0)) - sum(COALESCE(tax_ttl_amt,0.0)) "TOTALAMT",
+				  sum(COALESCE(tax_ttl_amt,0.0)) "TOTALTAX" 
 			  from t_invoice inv 
 				inner join t_av_internal av on inv.id_payer=av.id_acc
 				left outer join t_description des on av.c_invoicemethod=des.id_desc

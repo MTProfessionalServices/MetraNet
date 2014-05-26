3a2c314a17c9d3fcd3c5243879179dfe54536698
yzapara <yzapara@metratech.com>
2014-05-26 03:27:25 -0400

    FEAT-4091 As a MN User I would like the ability to access my Application Help for any MN screen by clicking on the Help button located on each page.
    Code prototype

---
+++ Help.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Help.aspx.cs" Inherits="Help" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+      <iframe src="<%=URL%>" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>
+</body>
+</html> 
+
---
+++ Help.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MetraTech.UI.Common;
+using MetraTech;
+using MindTouch;
+
+public partial class Help : System.Web.UI.Page
+{
+  /// <summary>
+  /// Get the MetraTech logger object
+  /// </summary>
+  private Logger mLog = new Logger("[MTPage]");
+  public Logger Logger
+  {
+    get { return mLog; }
+    set { mLog = value; }
+  }
+
+
+  //public string URL = @"/MetraNetHelp/en-us/";
+  public string URL = @"https://doc.metratech.com/MetraNet/MetraNet_8.0/Application_Help/MetraCare";
+  protected void Page_Load(object sender, EventArgs e)
+  {
+    //Response.Redirect(URL);
+    //MindTouch.MindTouchSso.GetHelpPageUrl(System.Threading.Thread.CurrentThread.CurrentCulture.ToString(),)
+    //var impersonationToken = MindTouchSso.GetImpersonationToken(MindTouchSso.DefaultUser);
+    //var pageUrl = MindTouchSso.GetHelpPageUrl("en-US", "VersionInfo.asp");
+
+    //var redirectUrl = string.Format(
+    //    CultureInfo.InvariantCulture,
+    //    "http://{0}/@api/deki/users/authenticate?authtoken={1}&redirect={2}",
+    //    MindTouchSso.ServerName, impersonationToken, pageUrl);
+
+    string pageName = Request["PageName"];
+    if (string.IsNullOrEmpty(pageName)) pageName = "VersionInfo.asp";
+    string redirectUrl = MindTouchSso.GetRedirectUrl(MindTouchSso.DefaultUser, "en-US", pageName);
+    Logger.LogDebug("redirect URL for page {0} is {1}", pageName, redirectUrl);
+    URL = redirectUrl;
+  }
+}

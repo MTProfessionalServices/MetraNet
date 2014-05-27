91fd7dc0a1728dce367d5bdce4850b8032770c49
damin <damin@metratech.com>
2014-05-27 15:28:19 -0400

    CORE-7855 [Bling - Account360] Localization and UI issues of Account 360 page - Enabled sorting on Subscriptions grid

---
+++ SqlCore/Queries/Account/__ACCOUNT_SUBSCRIPTIONSUMMARY__.SqlServer.sql
@@ -0,0 +1,58 @@
+select
+'GroupSubscription' AS SubscriptionType,
+tb_po.nm_name AS ProductOfferingName,
+tb_po.nm_desc AS ProductOfferingDescription,
+tmp.vt_start AS SubscriptionStart,
+tmp.vt_end AS SubscriptionEnd,
+tmp.id_po AS ProductOfferingId,
+tmp.id_sub AS SubscriptionId,
+1234.99 AS RecurringCharge,
+plist.nm_currency_code AS RecurringChargeCurrency,
+ null AS PromoCode,
+tg.tx_name AS GroupSubscriptionName,
+tg.tx_desc AS GroupSubscriptionDescription
+from
+(
+select s.id_sub, p.id_po,gsm.id_acc,gsm.vt_start,gsm.vt_end,
+sum(case when (bp.n_kind = 20 or bp.n_kind = 25) and plm.id_paramtable is null then 1 else 0 end) as num_recurring,
+sum(case when bp.n_kind = 40 and plm.id_paramtable is null then 1 else 0 end) as num_discount,
+sum(case when plm.id_sub is not null then 1 else 0 end) as num_icbs
+from
+t_gsubmember gsm
+inner join t_group_sub gs on gs.id_group=gsm.id_group
+inner join t_sub s on s.id_group=gs.id_group
+inner join t_po p on s.id_po=p.id_po
+inner join t_pl_map plm on plm.id_po=p.id_po
+inner join t_base_props bp on bp.id_prop=plm.id_pi_type
+where
+(plm.id_paramtable is null or plm.id_sub=s.id_sub)
+and
+gsm.id_acc=@AccountId
+group by s.id_sub, p.id_po,gsm.id_acc,gsm.vt_start,gsm.vt_end
+) tmp
+inner join t_sub sub on sub.id_sub=tmp.id_sub
+left JOIN t_vw_base_props tb_po on tb_po.id_prop = sub.id_po and tb_po.id_lang_code = 840
+INNER JOIN t_group_sub tg on sub.id_group = tg.id_group
+inner join t_po on t_po.id_po = tmp.id_po
+inner join t_pricelist plist on plist.id_pricelist  = t_po.id_nonshared_pl
+
+UNION
+
+SELECT 
+'Subscription' AS SubscriptionType,
+tb_po.nm_name AS ProductOfferingName,
+tb_po.nm_desc AS ProductOfferingDescription,
+sub.vt_start AS SubscriptionStart,
+sub.vt_end AS SubscriptionEnd,
+sub.id_po AS ProductOfferingId,
+sub.id_sub AS SubscriptionId,
+34.99 AS RecurringCharge,
+plist.nm_currency_code AS RecurringChargeCurrency,
+null AS PromoCode,
+null AS GroupSubscriptionName,
+null AS GroupSubscriptionDescription
+FROM t_sub sub
+LEFT JOIN t_vw_base_props tb_po on tb_po.id_prop = sub.id_po and tb_po.id_lang_code = 840
+inner join t_po on t_po.id_po = sub.id_po
+inner join t_pricelist plist on plist.id_pricelist  = t_po.id_nonshared_pl		
+WHERE sub.id_acc = @AccountId and sub.id_group is null

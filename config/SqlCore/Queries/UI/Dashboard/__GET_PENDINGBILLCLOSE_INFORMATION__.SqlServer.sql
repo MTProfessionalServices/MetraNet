7aeb43cc93a54f4e675a43dc93a2f274d3f24301
vkurylovich <vkurylovich@metratech.com>
2014-05-28 10:41:23 -0400

    CORE-7885: [Bling] Localization for Operations Dashboard: Pending Bill Closes

--- config/SqlCore/Queries/UI/Dashboard/__GET_PENDINGBILLCLOSE_INFORMATION__.SqlServer.sql
+++ config/SqlCore/Queries/UI/Dashboard/__GET_PENDINGBILLCLOSE_INFORMATION__.SqlServer.sql
@@ -1,7 +1,9 @@
-select ui.id_interval, CONVERT(VARCHAR(10), ui.dt_start, 101) as dt_start, 
-CONVERT(VARCHAR(10), ui.dt_end, 101) as dt_end, 
+select 
+ui.id_interval, 
+ui.dt_start as dt_start, 
+ui.dt_end as dt_end, 
 tx_interval_status, 
-DATEDIFF(day, dt_start, dt_end) as cycle
+datediff(day, dt_start, dt_end) as cycle
 from t_usage_interval ui
 where ui.tx_interval_status = 'O'
 and exists ( select au.id_usage_interval from t_acc_usage au where au.id_usage_interval = ui.id_interval)

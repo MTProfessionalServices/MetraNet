9b2cbb085b30c01efba76f136a236f1376ed0452
rpuzikov <rpuzikov@metratech.com>
2014-05-28 05:34:05 -0400

    FEAT-4112 [DEV] Review Master pages that would require modification Code reviewer: YZapara

--- TicketToMOMNoMenu.aspx.cs
+++ TicketToMOMNoMenu.aspx.cs
@@ -16,55 +16,17 @@ public partial class TicketToMOMNoMenu : System.Web.UI.Page
       Title = Server.HtmlEncode(Request.QueryString["Title"]);
     }
 
-    if (Request.QueryString["URL"] != null)
-    {
-      Session["IsMOMActive"] = true;
+    if (Request.QueryString["URL"] == null) return;
 
-      // replace | with ? and ** with &
-      string gotoURL = Request.QueryString["URL"].Replace("|", "?").Replace("**", "&");
+    Session["IsMOMActive"] = true;
 
-      try
-      {
-        ApiInput input = new ApiInput(gotoURL);
-        SecurityKernel.AccessController.Api.ExecuteDefaultByCategory(AccessControllerEngineCategory.UrlController.ToString(), input);
-      }
-      catch (AccessControllerException accessExp)
-      {
-        Session[Constants.ERROR] = accessExp.Message;
-        gotoURL = string.Empty;
-      }
-      catch (Exception exp)
-      {
-        Session[Constants.ERROR] = exp.Message;
-        throw exp;
-      }
+    // replace | with ? and ** with &
+    var gotoURL = Request.QueryString["URL"].Replace("|", "?").Replace("**", "&");
 
-      // Setup help URL
-      string helpName = "welcome.aspx";
-      string filePath = "";
-      try
-      {
-        string[] helpArr = gotoURL.Split('?');
-        helpName = helpArr[0].Substring(helpArr[0].LastIndexOf("/") + 1);
-        helpName = helpName.Substring(0, helpName.LastIndexOf('.'));
-      }
-      catch (Exception exp)
-      {
-        // Could not get help url
-        Logger.LogException("Could not get help URL from:" + gotoURL, exp);
-      }
-      HelpPage = "/MetraNetHelp/" + Thread.CurrentThread.CurrentCulture + "/index.htm?toc.htm?" + helpName + ".hlp.htm";
-      filePath = Server.MapPath(HelpPage.Substring(0, HelpPage.IndexOf('?')));
+    HelpPage = MetraTech.Core.UI.CoreUISiteGateway.GetHelpPageAsp(Server, Session, gotoURL, Logger);
 
-      if (!File.Exists(filePath))
-      {
-        // Set to en-us if you can't find Current Culture Help
-        HelpPage = "/MetraNetHelp/" + "en-us" + "/index.htm?toc.htm?" + helpName + ".hlp.htm";
-      }
-
-      Auth auth = new Auth();
-      auth.Initialize(UI.User.UserName, UI.User.NameSpace);
-      URL = auth.CreateEntryPoint("mom", "system_user", 0, gotoURL, false, true);
-    }
+    Auth auth = new Auth();
+    auth.Initialize(UI.User.UserName, UI.User.NameSpace);
+    URL = auth.CreateEntryPoint("mom", "system_user", 0, gotoURL, false, true);
   }
 }

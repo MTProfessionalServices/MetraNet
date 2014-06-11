b3ae17eb54b3e0bff775e09150c53ea3992e4d5c
Evgeniy Shvecov <yshvetsov@metratech.com>
2014-06-11 11:49:30 -0400

    ported CORE-7326 CLONE - MetraTax Adapter failures - performance/timeout issue (QC-3914)

--- SqlCore/Queries/Tax/__DROP_T_TAX_DETAILS_ROWS__.SqlServer.sql
+++ SqlCore/Queries/Tax/__DROP_T_TAX_DETAILS_ROWS__.SqlServer.sql
@@ -1,9 +1,14 @@
-
-          if object_id('t_tax_details') is not null
+	 DoItAgain:
+         delete top (5000)
+          from t_tax_details
+          where id_tax_run=%%RUN_ID%%
+	IF @@ROWCOUNT>0
+        GOTO DoItAgain
+        /* if object_id('t_tax_details') is not null
           while exists(select 1 from t_tax_details where id_tax_run=%%RUN_ID%%) 
           begin
             delete top (5000)
             from t_tax_details
             where id_tax_run=%%RUN_ID%%
-	  end
+	  end*/
        

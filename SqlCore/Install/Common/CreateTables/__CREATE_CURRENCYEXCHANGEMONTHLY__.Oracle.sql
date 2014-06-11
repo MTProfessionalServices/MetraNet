d5338bd22152331ce97db856cd7f4c2ef48760d2
msukhovarov <msukhovarov@metratech.com>
2014-06-11 05:23:04 -0400

    replaced datetime to date for Oracle version

--- SqlCore/Install/Common/CreateTables/__CREATE_CURRENCYEXCHANGEMONTHLY__.Oracle.sql
+++ SqlCore/Install/Common/CreateTables/__CREATE_CURRENCYEXCHANGEMONTHLY__.Oracle.sql
@@ -1,8 +1,8 @@
 create table CurrencyExchangeMonthly
 (
 InstanceId nvarchar2(64),
-StartDate datetime NOT NULL,
-EndDate datetime NOT NULL,
+StartDate date NOT NULL,
+EndDate date NOT NULL,
 SourceCurrency nvarchar2(100) NOT NULL,
 TargetCurrency nvarchar2(100) NOT NULL,
 ExchangeRate number(22,10) NOT NULL

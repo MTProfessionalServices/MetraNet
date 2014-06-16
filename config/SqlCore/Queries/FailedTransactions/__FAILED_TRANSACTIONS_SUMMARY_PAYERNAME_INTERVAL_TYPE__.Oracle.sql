b176f314849b34c4ed939d04c49cd9a590e4676d
aproskura <aproskura@metratech.com>
2014-06-16 07:15:14 -0400

    CORE-7446 CLONE - ICAL - EU - QA - MT6.8: Failed Transations - Syntax Error on 2 screens (DID#817)

--- config/SqlCore/Queries/FailedTransactions/__FAILED_TRANSACTIONS_SUMMARY_PAYERNAME_INTERVAL_TYPE__.Oracle.sql
+++ config/SqlCore/Queries/FailedTransactions/__FAILED_TRANSACTIONS_SUMMARY_PAYERNAME_INTERVAL_TYPE__.Oracle.sql
@@ -1,7 +1,7 @@
 
  /* Summary by payer and show interval type */
         select /*TOP 1000*/
-             (CASE WHEN id_PossiblePayerID = -1  THEN N'UNKNOWN PAYER' ELSE MAX(hn.hierarchyname) END) as PayerAccountHierarchyName,
+            MAX(hn.hierarchyname) as PayerAccountHierarchyName,  
             id_PossiblePayerID as PayerID,
             /*MAX(hn2.hierarchyname) as CorporateAccount,*/
             MAX(uc.id_cycle_type) as UsageCycleType,
@@ -10,18 +10,27 @@
             COUNT(*) as Count
         from 
           t_failed_transaction ft
-        left join VW_HIERARCHYNAME hn on ft.id_PossiblePayerID = hn.id_acc
-        left join t_acc_usage_cycle auc on auc.id_acc = ft.id_PossiblePayerID
-        left join t_usage_cycle uc on auc.id_usage_cycle = uc.id_usage_cycle
-        left join t_usage_cycle_type uct on uc.id_cycle_type = uct.id_cycle_type
-        /*Corporate Account Name*/
-        /*left join t_account_ancestor aa on id_PossiblePayerID = aa.id_descendent
-        left join VW_HIERARCHYNAME hn2 on aa.id_ancestor = hn2.id_acc
-        left join t_account a on aa.id_ancestor = a.id_acc
-        left join t_account_type at on at.id_type = a.id_type*/
+         inner join VW_HIERARCHYNAME hn on ft.id_PossiblePayerID = hn.id_acc 
+         inner join t_acc_usage_cycle auc on auc.id_acc = ft.id_PossiblePayerID 
+         inner join t_usage_cycle uc on auc.id_usage_cycle = uc.id_usage_cycle 
+         inner join t_usage_cycle_type uct on uc.id_cycle_type = uct.id_cycle_type  
         where
           /*at.b_IsCorporate = 1 and*/
           State in ('N','I', 'C')
         group by id_PossiblePayerID
-        order by Count desc
-            
+   union all 
+ 	      select /*TOP 1000*/ 
+	            N'UNKNOWN' as PayerAccountHierarchyName, 
+	            id_PossiblePayerID as PayerID, 
+	            null as UsageCycleType, 
+	            NULL as UsageCycleTypeName, 
+ 	            NULL as UsageCycleValue, 
+ 	            COUNT(*) as Count 
+ 	        from  
+ 	          t_failed_transaction ft 
+ 	        where 
+ 	          /*at.b_IsCorporate = 1 and*/ 
+ 	          State in ('N','I', 'C') 
+ 	           And Id_Possiblepayerid = -1 
+ 	        group by id_PossiblePayerID 
+ 	        order by Count DESC     

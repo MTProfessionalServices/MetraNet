383a21df9a4e24933eab29c5ddeef6baf8fbe39d
apyrshev <apyrshev@metratech.com>
2014-06-16 07:54:24 -0400

    CORE-7385 CLONE - Subscription level attributes - too short column for storing value (nm_value in t_char_values) (QC-3686)

--- SqlCore/Install/ProductCatalog/CreateTables/__CREATE_T_CHAR_VALUES_HISTORY__.Oracle.sql
+++ SqlCore/Install/ProductCatalog/CreateTables/__CREATE_T_CHAR_VALUES_HISTORY__.Oracle.sql
@@ -3,7 +3,7 @@
           (
 	          id_scv int NOT NULL,
 	          id_entity int NOT NULL,
-	          nm_value nvarchar2(20) NULL,
+	          nm_value nvarchar2(255) NULL,
 	          c_start_date timestamp NOT NULL,
 	          c_end_date timestamp NULL,
 	          c_spec_name nvarchar2(20) NOT NULL,
--- SqlCore/Install/ProductCatalog/CreateTables/__CREATE_T_CHAR_VALUES_HISTORY__.SqlServer.sql
+++ SqlCore/Install/ProductCatalog/CreateTables/__CREATE_T_CHAR_VALUES_HISTORY__.SqlServer.sql
@@ -3,7 +3,7 @@
           (
 	          id_scv int NOT NULL,
 	          id_entity int NOT NULL,
-	          nm_value nvarchar(20) NULL,
+	          nm_value nvarchar(255) NULL,
 	          c_start_date datetime NOT NULL,
 	          c_end_date datetime NULL,
 	          c_spec_name nvarchar(20) NOT NULL,

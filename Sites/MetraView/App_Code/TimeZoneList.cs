3ab086884f95b42f16cde46582093c5771cd54d6
Evgeniy Shvecov <yshvetsov@metratech.com>
2014-06-24 04:39:32 -0400

    CORE-7967 CLONE - The date/time in conference details does not reflect the TImezone setting

---
+++ Sites/MetraView/App_Code/TimeZoneList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+/// <summary>
+/// CORE-7967 Summary description for TimeZoneList
+/// </summary>
+public class TimeZoneList
+{
+  public String DisplayName { get; set; }
+  public String ID { get; set; }
+
+  public TimeZoneList()
+  {
+  }
+}

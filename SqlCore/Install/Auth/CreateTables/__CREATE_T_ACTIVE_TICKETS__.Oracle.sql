1014d99440085422ce12ce062996803df3c12943
skolesnik <skolesnik@metratech.com>
2014-07-04 11:24:21 -0400

    ESR-7584 Impossible to make subscription and Group Subscriptions (en-GB)

--- SqlCore/Install/Auth/CreateTables/__CREATE_T_ACTIVE_TICKETS__.Oracle.sql
+++ SqlCore/Install/Auth/CreateTables/__CREATE_T_ACTIVE_TICKETS__.Oracle.sql
@@ -1,12 +1,11 @@
-
-       CREATE TABLE t_active_tickets (
-		id_ticket number(10) NOT NULL,
-		nm_salt nvarchar2(2000) NOT NULL,
-		id_acc number(25) NOT NULL,
-		nm_space nvarchar2(255) NOT NULL,
-		nm_login nvarchar2(255) NOT NULL,
-		n_lifespanminutes number(10) NOT NULL,
-		dt_create date NOT NULL,
-		dt_expiration date NOT NULL,
-		id_lang_code number(10) NOT NULL DEFAULT 840
-       CONSTRAINT PK_t_active_tickets PRIMARY KEY (id_ticket)) 
+CREATE TABLE t_active_tickets 
+        (
+id_ticket number(10) NOT NULL PRIMARY KEY,
+nm_salt nvarchar2(2000) NOT NULL,
+id_acc number(25) NOT NULL,
+nm_space nvarchar2(255) NOT NULL,
+nm_login nvarchar2(255) NOT NULL,
+n_lifespanminutes number(10) NOT NULL,
+dt_create date NOT NULL,
+dt_expiration date NOT NULL,
+id_lang_code number(10) DEFAULT 840 )

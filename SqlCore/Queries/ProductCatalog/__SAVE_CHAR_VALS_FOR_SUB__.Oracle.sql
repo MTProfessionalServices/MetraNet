2973b12dca3794232d182b41ee972f0f5e507981
apyrshev <apyrshev@metratech.com>
2014-07-07 10:20:41 -0400

    CORE-7745 CLONE - Encoding problem in subscription properties (QC-3491) Fixed for Oracle

--- SqlCore/Queries/ProductCatalog/__SAVE_CHAR_VALS_FOR_SUB__.Oracle.sql
+++ SqlCore/Queries/ProductCatalog/__SAVE_CHAR_VALS_FOR_SUB__.Oracle.sql
@@ -1,4 +1,4 @@
 
-                delete from t_char_values where id_scv = %%SPEC_CHAR_VAL_ID%% and id_entity = %%ENTITY_ID%%
+                delete from t_char_values where id_scv = %%SPEC_CHAR_VAL_ID%% and id_entity = %%ENTITY_ID%%;
                 insert into t_char_values VALUES(%%SPEC_CHAR_VAL_ID%%, %%ENTITY_ID%%, '%%VALUE%%', %%START_DATE%%, %%END_DATE%%, '%%SPEC_NAME%%', %%SPEC_TYPE%%)
             

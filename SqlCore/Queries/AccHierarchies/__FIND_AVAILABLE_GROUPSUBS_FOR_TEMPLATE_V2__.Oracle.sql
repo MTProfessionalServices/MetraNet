cd95a58614f29139f4e1fdd28e612765004d1867
Ley Missailidis <moisis.langley@missailidis.ca>
2014-07-08 16:39:17 -0300

    DOORSTOP

--- SqlCore/Queries/AccHierarchies/__FIND_AVAILABLE_GROUPSUBS_FOR_TEMPLATE_V2__.Oracle.sql
+++ SqlCore/Queries/AccHierarchies/__FIND_AVAILABLE_GROUPSUBS_FOR_TEMPLATE_V2__.Oracle.sql
@@ -30,17 +30,19 @@
     INNER JOIN t_pricelist pl1 on pl1.id_pricelist = tpo.id_nonshared_pl   
     INNER JOIN t_av_internal tav on tav.id_acc = %%ID_ACC%%      
     /* CR 13655 make sure that PO is either wide open or allows template account type */
+    /*LEFT OUTER JOIN t_acc_template at ON id_folder = %%ID_ACC%% and id_acc_type = %%ACCOUNT_TYPE%%*/
     LEFT OUTER JOIN t_po_account_type_map atm ON ts.id_po = atm.id_po
     LEFT OUTER JOIN t_acc_tmpl_types tp ON tp.id = 1
     WHERE
-      tg.id_corporate_account = %%CORPORATEACCOUNT%% 
+      ts.id_group IS NOT NULL AND tg.id_corporate_account = %%CORPORATEACCOUNT%% 
     AND ts.id_po not in 
     (
       select ts1.id_po from t_acc_template_subs_pub tsubs
       INNER JOIN t_group_sub tg1 on tsubs.id_group = tg1.id_group
       INNER JOIN t_sub ts1 on ts1.id_group = tg1.id_group
-      INNER JOIN t_acc_template act on act.id_acc_template = tsubs.id_acc_template
-      WHERE act.id_folder = %%ID_ACC%% and act.id_acc_type = %%ACCOUNT_TYPE%% 
+     /* where id_acc_template = at.id_acc_template */
+     left outer join t_acc_template act on act.id_acc_template = tsubs.id_acc_template
+     and id_folder = %%ID_ACC%% and id_acc_type = %%ACCOUNT_TYPE%%
     )
     AND (atm.id_account_type IS NULL OR atm.id_account_type = %%ACCOUNT_TYPE%% OR tp.all_types = 1) 
     AND (%%CURRENCYFILTER1%%)    

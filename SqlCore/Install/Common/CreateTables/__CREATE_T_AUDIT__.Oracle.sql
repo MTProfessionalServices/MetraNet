82fba41fae2e2915611b3bccc2a40be045d4d59b
apyrshev <apyrshev@metratech.com>
2014-07-09 11:09:08 -0400

    CORE-8079 CLONE -Application Audit Log - Audit ID column sort Search Query Failed to Execute (DID#1376)

--- SqlCore/Install/Common/CreateTables/__CREATE_T_AUDIT__.Oracle.sql
+++ SqlCore/Install/Common/CreateTables/__CREATE_T_AUDIT__.Oracle.sql
@@ -1,6 +1,7 @@
-
-        CREATE TABLE t_audit (
-            id_audit number(10),
+    begin
+        execute immediate
+       'CREATE TABLE t_audit (
+            id_audit number(10) NOT NULL,
             id_Event number(10),
             id_UserId number(10),
             id_entitytype number(10),
@@ -10,4 +11,7 @@
             dt_crt date NOT NULL,
             CONSTRAINT PK_t_audit PRIMARY KEY  (id_audit)
             )
+       ';
+       execute immediate 'create index idx_t_audit_dt_crt on t_audit(dt_crt desc)';
+    end;
       

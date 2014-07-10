558fcd0383333d071a75750554ca4a11079f7464
aproskura <aproskura@metratech.com>
2014-07-10 07:03:52 -0400

    failed install OracleDB (fix)

--- SqlCore/Install/Common/CreateTables/__CREATE_T_AUDIT__.Oracle.sql
+++ SqlCore/Install/Common/CreateTables/__CREATE_T_AUDIT__.Oracle.sql
@@ -1,6 +1,5 @@
-    begin
-        execute immediate
-       'CREATE TABLE t_audit (
+
+CREATE TABLE t_audit (
             id_audit number(10) NOT NULL,
             id_Event number(10),
             id_UserId number(10),
@@ -10,8 +9,6 @@
 				    tx_application_name nvarchar2(50),
             dt_crt date NOT NULL,
             CONSTRAINT PK_t_audit PRIMARY KEY  (id_audit)
-            )
-       ';
-       execute immediate 'create index idx_t_audit_dt_crt on t_audit(dt_crt desc)';
-    end;
-      
+            );
+            
+create index idx_t_audit_dt_crt on t_audit(dt_crt desc);    

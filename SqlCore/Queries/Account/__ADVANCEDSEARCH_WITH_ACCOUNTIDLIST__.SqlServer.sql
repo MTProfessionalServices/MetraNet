c6aca710fe09d0415c0d14aa8747f53f01248598
Nick Krasikov <nkrasikov@metratech.com>
2014-07-15 09:17:10 -0400

    CORE-8102 CLONE - Operator IN does not work with integer identifiers (QC-3788)

--- SqlCore/Queries/Account/__ADVANCEDSEARCH_WITH_ACCOUNTIDLIST__.SqlServer.sql
+++ SqlCore/Queries/Account/__ADVANCEDSEARCH_WITH_ACCOUNTIDLIST__.SqlServer.sql
@@ -1,3 +1,3 @@
 SELECT * FROM (
-           select distinct _AccountID, name_space, username %%SORT_COLUMN%%
+           select distinct _AccountID, name_space, username %%SORT_COLUMN%% %%FILTER_COLUMN%%
            from (%%INNER_QUERY%%) iq1) iq2 WHERE _ACCOUNTID IN (%%ACCOUNTIDLIST%%)

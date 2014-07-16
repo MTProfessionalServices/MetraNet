3f81703806a1a49507bb53eb2b9e6e56d3238025
DStepanov <dstepanov@metratech.com>
2014-07-16 08:34:17 -0400

    Fix incorrect merge of CMI & Billing branches Reviewer RPuzikov

--- TicketToMAM.aspx.cs
+++ TicketToMAM.aspx.cs
@@ -1,30 +1,47 @@
 using System;
 using MetraTech.UI.Common;
 using MetraTech.Security;
+using MetraTech.SecurityFramework;
 
 public partial class UserControls_ticketToMAM : MTPage
 {
-	public string URL = "";
+  public string URL = "";
 
-	protected void Page_Load(object sender, EventArgs e)
-	{
-		if (Request.QueryString["Title"] != null)
-		{
-			Title = Server.HtmlEncode(Request.QueryString["Title"]);
-		}
+  protected void Page_Load(object sender, EventArgs e)
+  {
+    if (Request.QueryString["Title"] != null)
+    {
+      Title = Server.HtmlEncode(Request.QueryString["Title"]);
+    }
 
-	  if (Request.QueryString["URL"] == null) return;
+    if (Request.QueryString["URL"] == null) return;
 
-	  Session["IsMAMActive"] = true;
+    Session["IsMAMActive"] = true;
 
-	  // replace | with ? and ** with &
-	  var gotoURL = Request.QueryString["URL"].Replace("|", "?").Replace("**", "&");
+    // replace | with ? and ** with &
+    var gotoURL = Request.QueryString["URL"].Replace("|", "?").Replace("**", "&");
+    try
+    {
+      gotoURL = gotoURL + (gotoURL.Contains("?") ? "&" : "?") + "language=" + Session["MTSelectedLanguage"];
+      var input = new ApiInput(gotoURL);
+      SecurityKernel.AccessController.Api.ExecuteDefaultByCategory(AccessControllerEngineCategory.UrlController.ToString(), input);
+    }
+    catch (AccessControllerException accessExp)
+    {
+      Session[Constants.ERROR] = accessExp.Message;
+      gotoURL = string.Empty;
+    }
+    catch (Exception exp)
+    {
+      Session[Constants.ERROR] = exp.Message;
+      throw;
+    }
+       
+    HelpPage = MetraTech.Core.UI.CoreUISiteGateway.GetDefaultHelpPage(Server, Session, gotoURL, Logger);
 
-	  HelpPage = MetraTech.Core.UI.CoreUISiteGateway.GetDefaultHelpPage(Server, Session, gotoURL, Logger);
-
-	  var auth = new Auth();
-	  auth.Initialize(UI.User.UserName, UI.User.NameSpace);
-	  var accountId = UI.Subscriber.SelectedAccount == null ? 0 : int.Parse(UI.Subscriber["_AccountID"]);
-	  URL = auth.CreateEntryPoint("mam", "system_user", accountId, gotoURL, false, true);
-	}
+    var auth = new Auth();
+    auth.Initialize(UI.User.UserName, UI.User.NameSpace);
+    var accountId = UI.Subscriber.SelectedAccount == null ? 0 : int.Parse(UI.Subscriber["_AccountID"]);
+    URL = auth.CreateEntryPoint("mam", "system_user", accountId, gotoURL, false, true);
+  }
 }

684dbb8d0df9007df0052a1a8cf66e49f26c2935
skolesnik <skolesnik@metratech.com>
2014-07-17 19:17:50 -0400

    Core-7976 Shared Properties overwrite each other

--- SqlCore/Install/ProductCatalog/CreateTriggers/__CREATE_T_CHAR_VALUES_DELETE_UPDATE_TRIGGER__.Oracle.sql
+++ SqlCore/Install/ProductCatalog/CreateTriggers/__CREATE_T_CHAR_VALUES_DELETE_UPDATE_TRIGGER__.Oracle.sql
@@ -4,21 +4,11 @@
             BEFORE DELETE OR UPDATE ON T_CHAR_VALUES
             FOR EACH ROW 
             BEGIN
-              if inserting THEN
-                if :NEW.nm_value <> :OLD.nm_value THEN
-                  insert into t_char_values_history
-                  (id_scv, id_entity, nm_value, c_start_date, c_end_date, c_spec_name, c_spec_type)
+             
+				insert into t_char_values_history
+                 (id_scv, id_entity, nm_value, c_start_date, c_end_date, c_spec_name, c_spec_type)
                   values 
                   (:OLD.id_scv, :OLD.id_entity, :OLD.nm_value, :OLD.c_start_date, sysdate, :OLD.c_spec_name, :OLD.c_spec_type);
-                END IF;
-              END IF;
-  
-              if deleting THEN
-                  insert into t_char_values_history
-                  (id_scv, id_entity, nm_value, c_start_date, c_end_date, c_spec_name, c_spec_type)
-                  values 
-                  (:OLD.id_scv, :OLD.id_entity, :OLD.nm_value, :OLD.c_start_date, sysdate, :OLD.c_spec_name,:OLD.c_spec_type); 
-              END IF;
-  
+               
             END;
       

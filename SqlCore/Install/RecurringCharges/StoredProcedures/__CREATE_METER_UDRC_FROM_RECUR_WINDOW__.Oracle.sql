bb70f4ba83a324aeecc60c2aec6f8173a8c22d86
Nikita Kuklich <nkuklich@metratech.com>
2014-07-21 07:44:41 -0400

    CORE-8094:"UDRC: port SQL fixes to ORACLE" 1. Port logic to Oracle; 2. Remove extra global temp tables.

--- SqlCore/Install/RecurringCharges/StoredProcedures/__CREATE_METER_UDRC_FROM_RECUR_WINDOW__.Oracle.sql
+++ SqlCore/Install/RecurringCharges/StoredProcedures/__CREATE_METER_UDRC_FROM_RECUR_WINDOW__.Oracle.sql
@@ -1,6 +1,5 @@
-
 CREATE OR REPLACE
-PROCEDURE MeterUdrcFromRecurWindow (currentDate date) AS
+PROCEDURE MeterUdrcFromRecurWindow (currentDate date, actionType VARCHAR2) AS
   enabled       VARCHAR2(10);
 BEGIN
   SELECT value INTO enabled FROM t_db_values WHERE parameter = N'InstantRc';
@@ -25,8 +24,7 @@ BEGIN
       ,case when rcr.b_prorate_on_activate ='Y' then '1' else '0'end         AS c_ProrateOnSubscription
 	  ,dbo.MTMaxOfTwoDates(rw_new.c_UnitValueStart, trv.vt_start) AS c_UnitValueStart
       ,dbo.MTMinOfTwoDates(rw_new.c_UnitValueEnd, trv.vt_end) AS c_UnitValueEnd
-      ,tou.n_value AS c_UnitValueAdvanceCorrection
-      ,rw_new.c_UnitValue AS c_UnitValueDebitCorrection
+      ,trv.n_value AS c_UnitValue
       ,rcr.n_rating_type AS c_RatingType
       ,case when rcr.b_prorate_on_deactivate = 'Y' then '1' else '0'end AS c_ProrateOnUnsubscription
       ,CASE WHEN rcr.b_fixed_proration_length = 'Y' THEN fxd.n_proration_length ELSE 0 END          AS c_ProrationCycleLength
@@ -39,10 +37,9 @@ BEGIN
       ,rw_new.c__subscriptionid      AS c__SubscriptionID
       ,currentui.id_interval AS c__IntervalID
   FROM tmp_newrw rw_new
-	INNER JOIN t_recur_window trw ON rw_new.c__AccountID = trw.c__AccountID AND rw_new.c__SubscriptionID = trw.c__SubscriptionID
-    INNER JOIN t_recur_value trv on trv.id_sub = rw_new.C__SubscriptionID AND trv.tt_end = dbo.MTMaxDate()
-	  and trv.vt_start < rw_new.c_UnitValueEnd AND trv.vt_end > rw_new.c_UnitValueStart
-    INNER JOIN t_usage_interval ui ON  
+    INNER JOIN TMP_CHANGED_UNITS trv on trv.id_sub = rw_new.C__SubscriptionID AND trv.id_prop = rw_new.c__PriceableItemInstanceID
+        AND trv.vt_start < rw_new.c_UnitValueEnd AND trv.vt_end > rw_new.c_UnitValueStart
+    INNER JOIN t_usage_interval ui ON
 	    rw_new.c_UnitValueStart < ui.dt_end and rw_new.c_UnitValueEnd > ui.dt_start
     INNER JOIN t_recur rcr ON rw_new.c__priceableiteminstanceid = rcr.id_prop
     INNER JOIN t_usage_cycle ccl ON ccl.id_usage_cycle = 
@@ -59,19 +56,17 @@ BEGIN
                                    AND rw_new.c_membershipstart     < pci.dt_end AND rw_new.c_membershipend     > pci.dt_start /* rc overlaps with this membership */
                                    AND rw_new.c_cycleeffectivestart < pci.dt_end AND rw_new.c_cycleeffectiveend > pci.dt_start /* rc overlaps with this cycle */
                                    AND rw_new.c_SubscriptionStart   < pci.dt_end AND rw_new.c_subscriptionend   > pci.dt_start /* rc overlaps with this subscription */
-      
     INNER JOIN t_usage_cycle_type fxd ON fxd.id_cycle_type = ccl.id_cycle_type
-	INNER JOIN tmp_old_units tou ON tou.n_value IS NOT NULL
-    inner join t_usage_interval currentui on currentDate between currentui.dt_start and currentui.dt_end and currentui.id_usage_cycle = ui.id_usage_cycle
-   where 
-      /*Don't issue corrections for old values that are going to stay the same.*/
-      NOT EXISTS (SELECT 1 FROM tmp_old_units tou WHERE rw_new.c_UnitValueStart = tou.vt_start OR rw_new.c_UnitValueEnd = tou.vt_end)
+    INNER JOIN t_usage_interval currentui on currentDate between currentui.dt_start and currentui.dt_end and currentui.id_usage_cycle = ui.id_usage_cycle
+   WHERE
       /*Only issue corrections if there's a previous iteration.*/
-      AND EXISTS (SELECT 1 FROM t_recur_value trv WHERE trv.id_sub = rw_new.c__SubscriptionID AND trv.tt_end < dbo.MTMaxDate())
-	  AND rw_new.c__IsAllowGenChargeByTrigger = 1;
-	  
+      EXISTS (SELECT 1 FROM t_recur_value rv WHERE rv.id_sub = rw_new.c__SubscriptionID AND rv.tt_end < dbo.MTMaxDate())
+      /* Do we need this check? */
+      AND rw_new.c_UnitValue IS NOT NULL
+      AND rw_new.c__IsAllowGenChargeByTrigger = 1;
+
     insert INTO tmp_rc  
-    SELECT 'AdvanceCorrection' AS c_RCActionType
+    SELECT actionType AS c_RCActionType
            ,c_RCIntervalStart
            ,c_RCIntervalEnd
            ,c_BillingIntervalStart
@@ -85,37 +80,7 @@ BEGIN
            ,'N' AS c_ProrateInstantly
            ,c_UnitValueStart
            ,c_UnitValueEnd
-           ,c_UnitValueAdvanceCorrection AS c_UnitValue
-           ,c_RatingType
-           ,c_ProrateOnUnsubscription
-           ,c_ProrationCycleLength
-           ,c__AccountID
-           ,c__PayingAccount
-           ,c__PriceableItemInstanceID
-           ,c__PriceableItemTemplateID
-           ,c__ProductOfferingID
-           ,c_BilledRateDate
-           ,c__SubscriptionID
-           ,c__IntervalID
-           ,sys_guid() AS idSourceSess FROM tmp_udrc 
-           
-    UNION ALL
-           
-    SELECT 'DebitCorrection' AS c_RCActionType
-           ,c_RCIntervalStart
-           ,c_RCIntervalEnd
-           ,c_BillingIntervalStart
-           ,c_BillingIntervalEnd
-           ,c_RCIntervalSubscriptionStart
-           ,c_RCIntervalSubscriptionEnd
-           ,c_SubscriptionStart
-           ,c_SubscriptionEnd
-           ,c_Advance
-           ,c_ProrateOnSubscription
-           ,'N' AS c_ProrateInstantly
-           ,c_UnitValueStart
-           ,c_UnitValueEnd
-           ,c_UnitValueDebitCorrection AS c_UnitValue
+           ,c_UnitValue
            ,c_RatingType
            ,c_ProrateOnUnsubscription
            ,c_ProrationCycleLength

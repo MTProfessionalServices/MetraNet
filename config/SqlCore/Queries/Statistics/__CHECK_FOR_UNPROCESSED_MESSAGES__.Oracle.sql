48f3c939e7b86d7e5eea5f12dd9609204761318d
msukhovarov <msukhovarov@metratech.com>
2014-07-24 12:36:19 -0400

    CORE-8160 - [ICE validation] Errors after running ICE validation in feature_Billing branch
    Fixed ICE validation errors for ORACLE queries.

--- config/SqlCore/Queries/Statistics/__CHECK_FOR_UNPROCESSED_MESSAGES__.Oracle.sql
+++ config/SqlCore/Queries/Statistics/__CHECK_FOR_UNPROCESSED_MESSAGES__.Oracle.sql
@@ -2,7 +2,7 @@ select
 	dbo.GenGuid() "ID", /* dummy filed as identifier for GridLayout*/
 	enum.nm_enum_data, 
 	id_pipeline,
-	count(*) as '# Messages'
+	count(*) "# Messages"
 from t_session_set ss 
 join t_message tm 
 on tm.id_message=ss.id_message 

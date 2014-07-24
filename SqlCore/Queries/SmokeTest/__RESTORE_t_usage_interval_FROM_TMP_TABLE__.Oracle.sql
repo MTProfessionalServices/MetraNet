0f6deb07ed692ba1c24b04ab03e8230c4b756c6f
msukhovarov <msukhovarov@metratech.com>
2014-07-24 12:36:19 -0400

    CORE-8160 - [ICE validation] Errors after running ICE validation in feature_Billing branch
    Fixed ICE validation errors for ORACLE queries.

--- SqlCore/Queries/SmokeTest/__RESTORE_t_usage_interval_FROM_TMP_TABLE__.Oracle.sql
+++ SqlCore/Queries/SmokeTest/__RESTORE_t_usage_interval_FROM_TMP_TABLE__.Oracle.sql
@@ -5,14 +5,14 @@ begin
 		execute immediate 'ALTER TABLE t_usage_interval DISABLE CONSTRAINT FK1_T_USAGE_INTERVAL';
 		execute immediate 'ALTER TABLE t_usage_interval DISABLE CONSTRAINT CK1_T_USAGE_INTERVAL';
 		execute immediate 'ALTER TABLE t_billgroup DISABLE CONSTRAINT FK1_t_billgroup';
-		execute immediate 'ALTER TABLE t_acc_usage_interval DISABLE CONSTRAINT fk2_t_acc_usage_interval';
+		execute immediate 'ALTER TABLE t_acc_usage_interval DISABLE CONSTRAINT FK2_t_acc_usage_interval';
 		execute immediate 'ALTER TABLE t_billgroup_constraint  DISABLE CONSTRAINT FK1_t_billgroup_constraint';
 		execute immediate 'ALTER TABLE t_billgroup_constraint_tmp DISABLE  CONSTRAINT FK1_t_billgroup_constraint_tmp';
 		execute immediate 'ALTER TABLE t_billgroup_materialization  DISABLE  CONSTRAINT FK3_billgroup_materialization';
 		
 		execute immediate 'truncate table t_usage_interval'; 
 	
-		INSERT INTO t_usage_interval  ( id_interval,
+		execute immediate 'INSERT INTO t_usage_interval  ( id_interval,
 										id_usage_cycle,
 										dt_start,
 										dt_end,
@@ -22,13 +22,13 @@ begin
 				dt_start,
 				dt_end,
 				tx_interval_status
-		FROM %%TMP_T_ACC_USAGE_TABLE%%;
+		FROM %%TMP_T_ACC_USAGE_TABLE%%';
 		
 		/* enable all constraints */
 		execute immediate 'ALTER TABLE t_usage_interval ENABLE CONSTRAINT FK1_T_USAGE_INTERVAL';
 		execute immediate 'ALTER TABLE t_usage_interval ENABLE CONSTRAINT CK1_T_USAGE_INTERVAL';
 		execute immediate 'ALTER TABLE t_billgroup ENABLE CONSTRAINT FK1_t_billgroup';
-		execute immediate 'ALTER TABLE t_acc_usage_interval ENABLE CONSTRAINT fk2_t_acc_usage_interval';
+		execute immediate 'ALTER TABLE t_acc_usage_interval ENABLE CONSTRAINT FK2_t_acc_usage_interval';
 		execute immediate 'ALTER TABLE t_billgroup_constraint  ENABLE CONSTRAINT FK1_t_billgroup_constraint';
 		execute immediate 'ALTER TABLE t_billgroup_constraint_tmp ENABLE  CONSTRAINT FK1_t_billgroup_constraint_tmp';
 		execute immediate 'ALTER TABLE t_billgroup_materialization  ENABLE  CONSTRAINT FK3_billgroup_materialization';

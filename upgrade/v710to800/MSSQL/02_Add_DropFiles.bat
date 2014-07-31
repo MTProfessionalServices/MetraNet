855589040a1b0795e21ace882c7112de5618ac7d
Alexander Skrinnik <askrinnik@metratech.com>
2014-07-31 04:58:26 -0400

    DEVX-366  Create upgrade script for SQL server

--- upgrade/v710to800/MSSQL/02_Add_DropFiles.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting addidng and removing files:
-
-del %MTRMP%\Extensions\MvmAmp\MvmConfig\DatabaseInstall\tables\agg_charge_definition.SqlServer.sql
-copy .\agg_charge_definition.SqlServer.sql  %MTRMP%\Extensions\MvmAmp\MvmConfig\DatabaseInstall\views\agg_charge_definition.SqlServer.sql
-
-echo Finished adding and removing files.
-pause

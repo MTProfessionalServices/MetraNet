218b7095e303f585c93351ea1ab991722910ece6
ipisanka <ipisanka@metratech.com>
2014-08-14 03:14:30 -0400

    CORE-8305 Localization ( French): it's impossible to set misc. adjustment for account under French localization
    Code reviewer: ZMartynov

--- Adjustments/IssueMiscellaneousAdjustment.aspx.js
+++ Adjustments/IssueMiscellaneousAdjustment.aspx.js
@@ -1,34 +1,34 @@
 ﻿jQuery.noConflict();
 (function ($) {
-    $(function () {
-      $("#adjustmentSummary input:not([id$='adjAmountFldTaxToatl'])").live("keyup", function () {
-        var input = this.value;
-        if (input != "-") {
-          var totalSum = 0.00;
-          
-          $.each($("#adjustmentSummary input:not([id$='adjAmountFldTaxToatl'])"), function() {
-            var currentValue = this.value;
-            totalSum += Number(parseValue(currentValue));
-          });
+  $(function () {
+    $("#adjustmentSummary input:not([id$='adjAmountFldTaxToatl'])").live("keyup", function () {
+      var input = this.value;
+      if (input != "-") {
+        var totalSum = 0.00;
 
-          var regDecimal = /\./;
-          if (!regDecimal.test(totalSum)) {
-            totalSum += '.00';
-          } else {
-            regDecimal = /\d+\.\d$/;
-            if (regDecimal.test(totalSum))
-              totalSum += '0';
-          }
+        $.each($("#adjustmentSummary input:not([id$='adjAmountFldTaxToatl'])"), function () {
+          var currentValue = this.value;
+          totalSum += Number(parseValue(currentValue));
+        });
 
-          $("input[id$='adjAmountFldTaxToatl']").css("color", "#000");
-
-          if (isNaN(totalSum))
-            $("input[id$='adjAmountFldTaxToatl']").val("");
-          else
-            $("input[id$='adjAmountFldTaxToatl']").val(parseFloat(totalSum).toLocaleString(CURRENT_LOCALE, {maximumFractionDigits: FRACTION_DIGITS, minimumFractionDigits: FRACTION_DIGITS}));
+        var regDecimal = /\./;
+        if (!regDecimal.test(totalSum)) {
+          totalSum += '.00';
+        } else {
+          regDecimal = /\d+\.\d$/;
+          if (regDecimal.test(totalSum))
+            totalSum += '0';
         }
-      });
+
+        $("input[id$='adjAmountFldTaxToatl']").css("color", "#000");
+
+        if (isNaN(totalSum))
+          $("input[id$='adjAmountFldTaxToatl']").val("");
+        else
+          $("input[id$='adjAmountFldTaxToatl']").val(parseFloat(totalSum).toLocaleString(CURRENT_LOCALE, { maximumFractionDigits: FRACTION_DIGITS, minimumFractionDigits: FRACTION_DIGITS }));
+      }
     });
+  });
 })(jQuery);
 
 
@@ -47,19 +47,20 @@ function resetButtonClickCount() {
 }
 
 // private
-  // Can't call parseFloat() because value might be too large.
-  // Instead, just replaces decimal separator with standard '.' and checks for NaN.
-  // Returns number or empty string.
-  function parseValue (value) {
-    value = String(value).split(DECIMAL_SEPARATOR).join("_");
-    ;
-    ;
-    value = String(value).split(DIGIT_SEPARATOR + DIGIT_SEPARATOR).join("?")
-    value = String(value).split(DIGIT_SEPARATOR).join("")
-    value = String(value).split("_").join(".");
-    value = Number(value);
-    return isNaN(value) ? '' : value;
-  }
+// Can't call parseFloat() because value might be too large.
+// Instead, just replaces decimal separator with standard '.' and checks for NaN.
+// Returns number or empty string.
+function parseValue(value) {
+  value = String(value).split(DECIMAL_SEPARATOR + DECIMAL_SEPARATOR).join("?");
+  value = String(value).split(DECIMAL_SEPARATOR).join("_");
+  if (DIGIT_SEPARATOR != " ")
+    value = String(value).split(DIGIT_SEPARATOR).join("");
+  else
+    value = String(value).split(/\s+/).join("");
+  value = String(value).split("_").join(".");
+  value = Number(value);
+  return isNaN(value) ? '' : value;
+}
 
 // Use this function to ensure that only one button click gets executed on the page
 function checkButtonClickCount() {

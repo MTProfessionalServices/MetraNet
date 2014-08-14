ec0c0fbfbebd78b3db36852688be10cf12e3c34d
dgaz <dgaz@metratech.com>
2014-08-14 15:47:54 -0400

    CORE-8241 ICE validation errors

--- SqlCore/Queries/UsageServer/__GET_USAGE_INTERVAL_DATA_FOR_BILLGROUPS_WITHOUT_ACCT_STATS__.Oracle.sql
+++ SqlCore/Queries/UsageServer/__GET_USAGE_INTERVAL_DATA_FOR_BILLGROUPS_WITHOUT_ACCT_STATS__.Oracle.sql
@@ -45,22 +45,21 @@
       accounts as
       (
       select count (case when unassigned.id_acc is null and aui.tx_status = 'H' then aui.id_acc end) HardClosedUnassignedAcctsCnt,
-          count (case when unassigned.id_acc is null and aui.tx_status = 'O' then aui.id_acc end) OpenUnassignedAcctsCnt,       
-          count (*) TotalPayingAcctsForInterval,
-          aui.id_usage_interval         
-   from t_acc_usage_interval aui
-   inner join t_account_mapper amap ON amap.id_acc = aui.id_acc
-   inner join t_namespace nmspace ON nmspace.nm_space = amap.nm_space
-   left outer join /* unassigned accounts */
-     (select id_acc, id_usage_interval
-	  from t_billgroup_member bgm 
-	  inner join t_billgroup bg 
-	  on bg.id_billgroup = bgm.id_billgroup) unassigned on unassigned.id_acc = aui.id_acc and
-                                             unassigned.id_usage_interval = aui.id_usage_interval
-   where  nmspace.tx_typ_space = 'system_mps' and
-          aui.id_usage_interval = %%ID_INTERVAL%%
-   group by aui.id_usage_interval) accounts on accounts.id_usage_interval = ui.id_interval
-      )
+             count (case when unassigned.id_acc is null and aui.tx_status = 'O' then aui.id_acc end) OpenUnassignedAcctsCnt,       
+             count (*) TotalPayingAcctsForInterval,
+             aui.id_usage_interval         
+      from t_acc_usage_interval aui
+      inner join t_account_mapper amap ON amap.id_acc = aui.id_acc
+      inner join t_namespace nmspace ON nmspace.nm_space = amap.nm_space
+      left outer join /* unassigned accounts */
+          (select id_acc, id_usage_interval
+	         from t_billgroup_member bgm 
+	         inner join t_billgroup bg 
+	         on bg.id_billgroup = bgm.id_billgroup) unassigned on unassigned.id_acc = aui.id_acc 
+                                    and unassigned.id_usage_interval = aui.id_usage_interval
+           where  nmspace.tx_typ_space = 'system_mps' and
+           aui.id_usage_interval = %%ID_INTERVAL%%
+           group by aui.id_usage_interval)
 			select 
 				ui.id_interval IntervalID,
 				uct.tx_desc CycleType,
@@ -87,5 +86,6 @@
 			left outer join  /* materialization */ mat on mat.id_usage_interval = ui.id_interval
 			left outer join /* billing group status */ billgroups on billgroups.id_usage_interval = ui.id_interval
 			left outer join /* adapter data */ adapters on adapters.id_arg_interval = ui.id_interval
+			left outer join accounts on accounts.id_usage_interval = ui.id_interval
 			where ui.id_interval = %%ID_INTERVAL%%
 		

b705a2ad6dde6ba1c5ef6843336e71e8b1a19d96
vkurylovich <vkurylovich@metratech.com>
2014-08-15 16:07:55 -0400

    CORE-8241: ICE validation errors on SQL

--- SqlCore/Queries/Pipeline/_CREATE_FAILED_SESSION_STATE_STAGING_.Oracle.sql
+++ SqlCore/Queries/Pipeline/_CREATE_FAILED_SESSION_STATE_STAGING_.Oracle.sql
@@ -1,7 +1,7 @@
 
 begin
-    if not table_exists('%%NETMETERSTAGE%%.t_failed_session_state_stage') then
-        execute immediate 'CREATE TABLE %%NETMETERSTAGE%%.t_failed_session_state_stage (tx_FailureID RAW(16) NOT NULL, dt_FailureTime date NOT NULL)';
+    if not table_exists('%%%NETMETERSTAGE_PREFIX%%%t_failed_session_state_stage') then
+        execute immediate 'CREATE TABLE %%%NETMETERSTAGE_PREFIX%%%t_failed_session_state_stage (tx_FailureID RAW(16) NOT NULL, dt_FailureTime date NOT NULL)';
     end if;
 end;
 			

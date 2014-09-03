f63acd68723d30e3cc3398d82f5ffeae74a6f133
damin <damin@metratech.com>
2014-09-03 09:17:43 -0400

    FEAT-3849 Bling: Productize ProductCatalog Dashboard - Custom serialization for json related to MRR graphs since they contain amount fields
    Code reviewer: dgaz

--- MetraOffer/AjaxServices/VisualizeService.aspx.cs
+++ MetraOffer/AjaxServices/VisualizeService.aspx.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
 using MetraTech.ActivityServices.Common;
 using MetraTech.Debug.Diagnostics;
+using MetraTech.SecurityFramework;
 using MetraTech.UI.Common;
 
 
@@ -117,7 +120,7 @@ public partial class AjaxServices_VisualizeService : MTListServicePage
                 return;
             }
 
-            string json = VisualizeService.SerializeItems(items);
+            string json = SerializeItems(operation, items);
             Logger.LogInfo("Returning " + json);
             Response.Write(json);
             Response.End();
@@ -125,6 +128,93 @@ public partial class AjaxServices_VisualizeService : MTListServicePage
 
     }
 
+    private string SerializeItems(string operation, MTList<SQLRecord> items)
+    {
+      string json;
 
-    
+      if (operation.Equals("AnalyticsTopMRR") || operation.Equals("AnalyticsTopMRRGain") ||
+          operation.Equals("AnalyticsTopMRRLoss"))
+      {
+        json = ConstructJson(operation, items);
+      }
+      else
+      {
+        json = VisualizeService.SerializeItems(items);
+      }
+      return json;
+    }
+
+    private string ConstructJson(string operation, MTList<SQLRecord> items)
+    {
+       var json = new StringBuilder();
+      string item = string.Empty;
+      //format dates/amounts used as data in client side for formatting or plotting graphs using invaraint culture otherwise culture specific decimal/group seperators mess up with json returned
+      var invariantCulture = CultureInfo.InvariantCulture;
+
+      json.Append("{\"Items\":[");
+
+      for (int i = 0; i < items.Items.Count; i++)
+      {
+        SQLRecord record = items.Items[i];
+
+        if (i > 0)
+        {
+          json.Append(",");
+        }
+
+        json.Append("{");
+
+        if (record.Fields.Count > 0)
+        {
+            item = string.Format("\"ordernum\":{0},", FormatFieldValue(record.Fields[0]));
+            json.Append(item);
+            item = string.Format("\"productcode\":{0},", FormatFieldValue(record.Fields[2]));
+            json.Append(item);
+            item = string.Format("\"month\":{0},", FormatFieldValue(record.Fields[3]));
+            json.Append(item);
+            item = string.Format("\"mrr\":{0},", FormatFieldValue(record.Fields[4], invariantCulture));
+            json.Append(item);
+            item = string.Format("\"mrrAsString\":\"{0}\",", FormatFieldValue(record.Fields[4]));
+            json.Append(item);
+            item = string.Format("\"mrrprevious\":{0},", FormatFieldValue(record.Fields[5], invariantCulture));
+            json.Append(item);
+            item = string.Format("\"mrrpreviousAsString\":\"{0}\",", FormatFieldValue(record.Fields[5]));
+            json.Append(item);
+            item = string.Format("\"mrrchange\":{0},", FormatFieldValue(record.Fields[6], invariantCulture));
+            json.Append(item);
+            item = string.Format("\"mrrchangeAsString\":\"{0}\"", FormatFieldValue(record.Fields[6]));
+            json.Append(item);
+        }
+        json.Append("}");
+      }
+
+      json.Append("]");
+
+      json.Append("}");
+
+      return json.ToString();
+    }
+    private string FormatFieldValue(SQLField field, CultureInfo formatCulture = null)
+    {
+      if (field.FieldValue == null) return "null";
+      string fieldValue;
+      fieldValue = (formatCulture == null) ? field.FieldValue.ToString() : Convert.ToString(field.FieldValue, formatCulture);// field.ToString();
+
+      // CORE-5487 HtmlEncode the field so XSS tags don't show up in UI.
+      //StringBuilder sb = new StringBuilder(HttpUtility.HtmlEncode(value));
+      // CORE-5938: Audit log: incorrect character encoding in Details row 
+      var sb = new StringBuilder((fieldValue ?? string.Empty).EncodeForHtml());
+      sb = sb.Replace("\"", "\\\"");
+      //CORE-5320: strip all the new line characters. They are not allowed in jason
+      // Oracle can return them and breeak our ExtJs grid with an ugly "Session Time Out" catch all error message
+      // TODO: need to find other places where JSON is generated and strip new line characters.
+      sb = sb.Replace("\n", "<br />");
+      sb = sb.Replace("\r", "");
+
+      return ((typeof(String) == field.FieldDataType || typeof(DateTime) == field.FieldDataType ||
+               typeof(Guid) == field.FieldDataType || typeof(Byte[]) == field.FieldDataType))
+               ? string.Format("\"{0}\"", sb)
+               : string.Format("{0}", sb);
+
+    }
 }

45bad9e6aad5829e6d5cfed3daf1107671e7568b
vkurylovich <vkurylovich@metratech.com>
2014-09-04 16:54:14 -0400

    FEAT-4257: Write Oracle versions of the queries if required

--- config/SqlCore/Queries/UI/Dashboard/__SubscriptionSummary_TopMRRGain__.Oracle.sql
+++ config/SqlCore/Queries/UI/Dashboard/__SubscriptionSummary_TopMRRGain__.Oracle.sql
@@ -1,4 +1,6 @@
-SELECT 
+SELECT * 
+FROM (
+  SELECT 
     ROW_NUMBER() OVER (ORDER BY SUM(NVL(ss.MRRPrimaryCurrency, 0.0))-SUM(NVL(prev.MRRPrimaryCurrency, 0.0)) DESC) AS ordernum, 
 	po.ProductOfferingName AS productname, 
 	po.ProductOfferingId AS productcode,
@@ -6,20 +8,20 @@ SELECT
 	SUM(NVL(ss.MRRPrimaryCurrency, 0.0)) AS MRR, 
 	SUM(NVL(prev.MRRPrimaryCurrency, 0.0)) AS MRRPrevious, 
 	SUM(NVL(ss.MRRPrimaryCurrency, 0.0))-SUM(NVL(prev.MRRPrimaryCurrency, 0.0)) AS MRRChange
-FROM SubscriptionSummary ss
-INNER JOIN ProductOffering po 
- ON po.ProductOfferingId = ss.ProductOfferingId 
- AND ss.InstanceId = po.InstanceId
-LEFT JOIN SubscriptionSummary prev 
- ON ss.InstanceId = prev.InstanceId 
- AND ss.ProductOfferingId = prev.ProductOfferingId 
- AND prev.Month = TO_NUMBER (TO_CHAR (ADD_MONTHS (%%METRATIME%%, -2),'mm'))  
- AND prev.Year = TO_NUMBER (TO_CHAR (ADD_MONTHS (%%METRATIME%%, -2),'yyyy'))
-WHERE ss.Month = TO_NUMBER (TO_CHAR (ADD_MONTHS (%%METRATIME%%, -1),'mm')) 
- AND ss.Year = TO_NUMBER (TO_CHAR (ADD_MONTHS (%%METRATIME%%, -1),'yyyy'))
- AND ROWNUM <=10	 
-GROUP BY ss.InstanceId, po.ProductOfferingName,  po.ProductOfferingId, ss.Month 
-HAVING SUM(NVL(ss.MRRPrimaryCurrency, 0.0))-SUM(NVL(prev.MRRPrimaryCurrency, 0.0)) > 0
+  FROM SubscriptionSummary ss
+  INNER JOIN ProductOffering po 
+   ON po.ProductOfferingId = ss.ProductOfferingId 
+   AND ss.InstanceId = po.InstanceId
+  LEFT JOIN SubscriptionSummary prev 
+   ON ss.InstanceId = prev.InstanceId 
+   AND ss.ProductOfferingId = prev.ProductOfferingId 
+   AND prev.Month = TO_NUMBER (TO_CHAR (ADD_MONTHS (%%METRATIME%%, -2),'mm'))  
+   AND prev.Year = TO_NUMBER (TO_CHAR (ADD_MONTHS (%%METRATIME%%, -2),'yyyy'))
+  WHERE ss.Month = TO_NUMBER (TO_CHAR (ADD_MONTHS (%%METRATIME%%, -1),'mm')) 
+   AND ss.Year = TO_NUMBER (TO_CHAR (ADD_MONTHS (%%METRATIME%%, -1),'yyyy'))  
+  GROUP BY ss.InstanceId, po.ProductOfferingName,  po.ProductOfferingId, ss.Month 
+  HAVING SUM(NVL(ss.MRRPrimaryCurrency, 0.0))-SUM(NVL(prev.MRRPrimaryCurrency, 0.0)) > 0 ) a
+WHERE a.ordernum <=10 
 
 
 
--- config/SqlCore/Queries/UI/Dashboard/__SubscriptionSummary_TopMRR__.Oracle.sql
+++ config/SqlCore/Queries/UI/Dashboard/__SubscriptionSummary_TopMRR__.Oracle.sql
@@ -1,4 +1,6 @@
-SELECT 
+SELECT * 
+FROM (
+  SELECT 
     ROW_NUMBER () OVER (ORDER BY SUM(NVL(ss.MRRPrimaryCurrency, 0.0)) DESC) AS ordernum,
 	po.ProductOfferingName AS productname,
     po.ProductOfferingId AS productcode,
@@ -12,16 +14,16 @@ SELECT
 	SUM(NVL(ss.NewParticipants, 0.0)) AS NewCustomers, 
 	SUM(NVL(prev.NewParticipants, 0.0)) AS NewCustomersPrevious, 
 	SUM(NVL(ss.NewParticipants, 0.0))-SUM(NVL(prev.NewParticipants, 0.0)) AS NewCustomersChange
-FROM SubscriptionSummary ss
-INNER JOIN ProductOffering po 
-  ON po.ProductOfferingId = ss.ProductOfferingId 
-  AND ss.InstanceId = po.InstanceId
-LEFT JOIN SubscriptionSummary prev 
-  ON ss.InstanceId = prev.InstanceId 
-  AND ss.ProductOfferingId = prev.ProductOfferingId 
-  AND prev.Month = TO_NUMBER (TO_CHAR (ADD_MONTHS(%%METRATIME%%, -2),'mm'))  
-  AND prev.Year = TO_NUMBER (TO_CHAR (ADD_MONTHS(%%METRATIME%%, -2),'yyyy'))
-WHERE ss.Month = TO_NUMBER (TO_CHAR (ADD_MONTHS(%%METRATIME%%, -1),'mm')) 
-  AND ss.Year = TO_NUMBER (TO_CHAR (ADD_MONTHS(%%METRATIME%%, -1),'yyyy'))
-  AND ROWNUM <=10
-GROUP BY ss.InstanceId, po.ProductOfferingName, po.ProductOfferingId, ss.Month
+  FROM SubscriptionSummary ss
+  INNER JOIN ProductOffering po 
+    ON po.ProductOfferingId = ss.ProductOfferingId 
+    AND ss.InstanceId = po.InstanceId
+  LEFT JOIN SubscriptionSummary prev 
+    ON ss.InstanceId = prev.InstanceId 
+    AND ss.ProductOfferingId = prev.ProductOfferingId 
+    AND prev.Month = TO_NUMBER (TO_CHAR (ADD_MONTHS(%%METRATIME%%, -2),'mm'))  
+    AND prev.Year = TO_NUMBER (TO_CHAR (ADD_MONTHS(%%METRATIME%%, -2),'yyyy'))
+  WHERE ss.Month = TO_NUMBER (TO_CHAR (ADD_MONTHS(%%METRATIME%%, -1),'mm')) 
+    AND ss.Year = TO_NUMBER (TO_CHAR (ADD_MONTHS(%%METRATIME%%, -1),'yyyy'))
+  GROUP BY ss.InstanceId, po.ProductOfferingName, po.ProductOfferingId, ss.Month ) a
+WHERE a.ordernum <=10 

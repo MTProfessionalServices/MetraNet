ee5080de70400231dc11665134ec13ab90447973
vkurylovich <vkurylovich@metratech.com>
2014-09-04 14:04:36 -0400

    FEAT-4257: Write Oracle versions of the queries if required

---
+++ config/SqlCore/Queries/UI/Dashboard/__SubscriptionSummary_TopMRRGain__.Oracle.sql
@@ -0,0 +1,25 @@
+SELECT 
+    ROW_NUMBER() OVER (ORDER BY SUM(NVL(ss.MRRPrimaryCurrency, 0.0))-SUM(NVL(prev.MRRPrimaryCurrency, 0.0)) DESC) AS ordernum, 
+	po.ProductOfferingName AS productname, 
+	po.ProductOfferingId AS productcode,
+	ss.Month, 
+	SUM(NVL(ss.MRRPrimaryCurrency, 0.0)) AS MRR, 
+	SUM(NVL(prev.MRRPrimaryCurrency, 0.0)) AS MRRPrevious, 
+	SUM(NVL(ss.MRRPrimaryCurrency, 0.0))-SUM(NVL(prev.MRRPrimaryCurrency, 0.0)) AS MRRChange
+FROM SubscriptionSummary ss
+INNER JOIN ProductOffering po 
+ ON po.ProductOfferingId = ss.ProductOfferingId 
+ AND ss.InstanceId = po.InstanceId
+LEFT JOIN SubscriptionSummary prev 
+ ON ss.InstanceId = prev.InstanceId 
+ AND ss.ProductOfferingId = prev.ProductOfferingId 
+ AND prev.Month = TO_NUMBER (TO_CHAR (ADD_MONTHS (%%METRATIME%%, -2),'mm'))  
+ AND prev.Year = TO_NUMBER (TO_CHAR (ADD_MONTHS (%%METRATIME%%, -2),'yyyy'))
+WHERE ss.Month = TO_NUMBER (TO_CHAR (ADD_MONTHS (%%METRATIME%%, -1),'mm')) 
+ AND ss.Year = TO_NUMBER (TO_CHAR (ADD_MONTHS (%%METRATIME%%, -1),'yyyy'))
+ AND ROWNUM <=10	 
+GROUP BY ss.InstanceId, po.ProductOfferingName,  po.ProductOfferingId, ss.Month 
+HAVING SUM(NVL(ss.MRRPrimaryCurrency, 0.0))-SUM(NVL(prev.MRRPrimaryCurrency, 0.0)) > 0
+
+
+

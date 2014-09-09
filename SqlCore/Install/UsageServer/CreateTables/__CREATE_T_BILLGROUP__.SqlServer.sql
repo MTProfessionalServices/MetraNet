72aade0ef2721151a87e17d191ea3ea271296e23
Jeff <jjohnson@metratech.com>
2014-09-09 14:32:56 -0400

    FEAT-4445 -  Modify t_billgroup and t_billgroup_member_tmp to be partition aware.

--- SqlCore/Install/UsageServer/CreateTables/__CREATE_T_BILLGROUP__.SqlServer.sql
+++ SqlCore/Install/UsageServer/CreateTables/__CREATE_T_BILLGROUP__.SqlServer.sql
@@ -6,7 +6,8 @@ CREATE TABLE t_billgroup
   tx_description NVARCHAR(2048) NULL,            -- description for this billing group
   id_usage_interval INT NOT NULL,                      -- interval associated with this billing group
   id_parent_billgroup  INT NULL,                         -- id of the parent billing group, if this is the materialization of a pull list
-  tx_type VARCHAR(20) NOT NULL                     -- The type of materialization. One of: Full, Rematerialization, PullList 
+  tx_type VARCHAR(20) NOT NULL,                     -- The type of materialization. One of: Full, Rematerialization, PullList
+  partition_id_acc  INT NOT NULL              -- id_acc of the partition account this bill group belongs to.
 
   CONSTRAINT PK_t_billgroup PRIMARY KEY (id_billgroup),
   CONSTRAINT FK1_t_billgroup FOREIGN KEY (id_usage_interval) REFERENCES t_usage_interval (id_interval),

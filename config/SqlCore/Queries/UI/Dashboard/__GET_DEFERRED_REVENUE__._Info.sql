572235427122763d4c96146dfb4f231d4a68e630
ipisanka <ipisanka@metratech.com>
2014-09-09 03:30:31 -0400

    FEAT-4373 Prototype for Revenue Recognition FEAT-4378 Add SQL query for getting data from DataBase
    Add queries into RMP\Extensions\SystemConfig\config\SqlCore\Queries\UI\Dashboard

---
+++ config/SqlCore/Queries/UI/Dashboard/__GET_DEFERRED_REVENUE__._Info.sql
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?>
+<Query>
+    <Action>Get</Action>
+    <Implementations>
+        <Implementation>SqlServer</Implementation>
+        <Implementation>Oracle</Implementation>
+    </Implementations>
+    <Parameters>
+        <Parameter>
+            <Name>ID_USAGE_INTERVAL</Name>
+            <DataType>int32</DataType>
+            <ReplacementType>DoublePercent</ReplacementType>
+            <Description>ID Usage Interval</Description>
+        </Parameter>
+    </Parameters>
+    <Description></Description>
+    <ConversionAudit>
+        <ConversionDuplicateName></ConversionDuplicateName>
+        <CommonQuerySourceFile></CommonQuerySourceFile>
+        <SqlServerQuerySourceFile></SqlServerQuerySourceFile>
+        <OracleQuerySourceFile></OracleQuerySourceFile>
+    </ConversionAudit>
+</Query>
---
+++ config/SqlCore/Queries/UI/Dashboard/__GET_EARNED_REVENUE__.SqlServer.sql
@@ -0,0 +1,36 @@
+DECLARE @startDate datetime = '%%START_DATE%%'
+DECLARE @endDate datetime = '%%END_DATE%%'
+
+SELECT 
+	 acc.c_Currency
+	,udrc.id_usage_interval
+	,udrc.c_RCIntervalSubscriptionStart
+	,udrc.c_RCIntervalSubscriptionEnd
+	,udrc.c_ProratedIntervalStart
+	,udrc.c_ProratedIntervalEnd
+	,udrc.c_ProratedDays
+	,udrc.c_ProratedDailyRate
+FROM t_pv_UDRecurringCharge AS udrc
+INNER JOIN t_sub AS sub ON udrc.c__SubscriptionID = sub.id_sub
+INNER JOIN t_av_Internal AS acc ON sub.id_acc = acc.id_acc
+WHERE
+	c_RCIntervalSubscriptionStart >= '19000101'
+	AND c_RCIntervalSubscriptionEnd < @startDate
+
+UNION
+
+SELECT 
+	 acc.c_Currency
+	,rc.id_usage_interval
+	,rc.c_RCIntervalSubscriptionStart
+	,rc.c_RCIntervalSubscriptionEnd
+	,rc.c_ProratedIntervalStart
+	,rc.c_ProratedIntervalEnd
+	,rc.c_ProratedDays
+	,rc.c_ProratedDailyRate
+FROM t_pv_FlatRecurringCharge AS rc
+INNER JOIN t_sub AS sub ON rc.c__SubscriptionID = sub.id_sub
+INNER JOIN t_av_Internal AS acc ON sub.id_acc = acc.id_acc
+WHERE
+	c_RCIntervalSubscriptionStart >= '19000101'
+	AND c_RCIntervalSubscriptionEnd < @startDate

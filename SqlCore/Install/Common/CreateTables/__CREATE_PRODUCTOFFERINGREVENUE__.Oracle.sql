04e0489ec17736b4804bf7a5f64c1f61d2a78ae2
damin <damin@metratech.com>
2014-09-09 10:49:19 -0400

    FEAT-3849 Bling: Productize ProductCatalog Dashboard - Reverting  the change made to calculate total revenue for each product offering

--- SqlCore/Install/Common/CreateTables/__CREATE_PRODUCTOFFERINGREVENUE__.Oracle.sql
+++
@@ -1,8 +0,0 @@
-create table ProductOfferingRevenue
-(
-InstanceId nvarchar2(64),
-ProductOfferingId int NOT NULL,
-Year int NOT NULL,
-Month int NOT NULL,
-RevPrimaryCurrency number(22,10) NOT NULL
-)
--- SqlCore/Install/Common/CreateTables/__CREATE_SUM_REV_BY_MONTH__.Oracle.sql
+++
@@ -1,1 +0,0 @@
-create table sum_rev_by_month (InstanceId varchar2(64), ProductOfferingId int, Currency nvarchar2(3), Year int, Month int, SubscriptionRev number(22,10));

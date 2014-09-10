d65edc49ca109d40333fb317ae9942bbe01d6a82
vkurylovich <vkurylovich@metratech.com>
2014-09-10 14:18:40 -0400

    FEAT-4442: Fix __ASSIGN_BILLING_GROUPS_BY_NAMESPACE__ query

--- SqlCore/Queries/UsageServer/Samples/__ASSIGN_BILLING_GROUPS_BY_NAMESPACE__.Common.sql
+++ SqlCore/Queries/UsageServer/Samples/__ASSIGN_BILLING_GROUPS_BY_NAMESPACE__.Common.sql
@@ -22,6 +22,23 @@ JOIN t_account_mapper amap on cg.id_acc = amap.id_acc
 JOIN t_namespace ns on amap.nm_space = ns.nm_space and ns.tx_typ_space = 'system_mps' and ns.nm_method = 'Partition'
 UNION ALL
 SELECT 
+  %%ID_MATERIALIZATION%%, 
+  cg.id_acc,
+  lower(t_account_mapper.nm_login)
+FROM t_billgroup_constraint_tmp cg
+JOIN
+(
+  SELECT 
+    id_group,
+    MAX(id_acc) id_acc
+  FROM t_billgroup_constraint_tmp
+  GROUP BY id_group
+) singleacc ON singleacc.id_group = cg.id_group
+JOIN t_account on cg.id_acc = t_account.id_acc
+JOIN t_account_type on t_account.id_type = t_account_type.id_type and t_account_type.name = 'Partition'
+JOIN t_account_mapper on cg.id_acc = t_account_mapper.id_acc  and t_account_mapper.nm_space = 'mt'
+UNION ALL
+SELECT 
 	%%ID_MATERIALIZATION%%, 
 	cg.id_acc,
 	N'Default'
@@ -36,3 +53,12 @@ JOIN
 ) singleacc ON singleacc.id_group = cg.id_group
 JOIN t_account_mapper amap on cg.id_acc = amap.id_acc
 JOIN t_namespace ns on amap.nm_space = ns.nm_space and ns.tx_typ_space = 'system_mps' and (ns.nm_method IS NULL OR ns.nm_method !=  'Partition')
+WHERE amap.id_acc NOT IN
+(
+  SELECT 
+    t_account.id_acc
+  FROM t_account
+  INNER JOIN t_account_type on t_account.id_type = t_account_type.id_type and t_account_type.name = 'Partition'
+  INNER JOIN t_account_mapper on t_account.id_acc = t_account_mapper.id_acc  and t_account_mapper.nm_space = 'mt'
+)
+

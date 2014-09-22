616165b820c6b58d73488045ae12c748a39f4b60
ipisanka <ipisanka@metratech.com>
2014-09-22 05:02:42 -0400

    FEAT-4461 As a MetraNet Finance User I want to be able to run a Deferred Revenue Schedule Report under Reports and the Accounting Folder. FEAT-4500 Add NRCs into sql queries
    Code Reviewer: ZMartynov

--- config/SqlCore/Queries/UI/Dashboard/__GET_EARNED_REVENUE__.SqlServer.sql
+++ config/SqlCore/Queries/UI/Dashboard/__GET_EARNED_REVENUE__.SqlServer.sql
@@ -1,8 +1,8 @@
 SELECT 
   acc.am_currency
 	,udrc.id_usage_interval
-	,udrc.c_RCIntervalSubscriptionStart
-	,udrc.c_RCIntervalSubscriptionEnd
+	,udrc.c_RCIntervalSubscriptionStart AS SubscriptionStart
+	,udrc.c_RCIntervalSubscriptionEnd AS SubscriptionEnd
 	,udrc.c_ProratedIntervalStart
 	,udrc.c_ProratedIntervalEnd
 	,udrc.c_ProratedDays
@@ -20,16 +20,35 @@ UNION
 SELECT 
   acc.am_currency
  ,frc.id_usage_interval
- ,frc.c_RCIntervalSubscriptionStart
- ,frc.c_RCIntervalSubscriptionEnd
+ ,frc.c_RCIntervalSubscriptionStart AS SubscriptionStart
+ ,frc.c_RCIntervalSubscriptionEnd AS SubscriptionEnd
  ,frc.c_ProratedIntervalStart
  ,frc.c_ProratedIntervalEnd
  ,frc.c_ProratedDays
  ,frc.c_ProratedDailyRate
- ,frc_ep.c_IsLiabilityProduct
- ,frc_ep.c_RevenueCode
- ,frc_ep.c_DeferredRevenueCode
+ ,frc__ep.c_IsLiabilityProduct
+ ,frc__ep.c_RevenueCode
+ ,frc__ep.c_DeferredRevenueCode
 FROM t_pv_FlatRecurringCharge AS frc
 INNER JOIN t_acc_usage AS acc ON frc.id_sess = acc.id_sess
-LEFT JOIN t_ep_recurring AS frc_ep ON frc_ep.id_prop = acc.id_pi_template
+LEFT JOIN t_ep_recurring AS frc__ep ON frc_ep.id_prop = acc.id_pi_template
 WHERE c_RCIntervalSubscriptionStart < %%START_DATE%%
+
+UNION
+
+SELECT 
+  acc.am_currency
+ ,nrc.id_usage_interval
+ ,nrc.c_NRCIntervalSubscriptionStart AS SubscriptionStart
+ ,nrc.c_NRCIntervalSubscriptionEnd AS SubscriptionEnd
+ ,NULL AS c_ProratedIntervalStart
+ ,NULL AS c_ProratedIntervalEnd
+ ,1 AS c_ProratedDays
+ ,acc.amount AS c_ProratedDailyRate
+ ,nrc_ep.c_IsLiabilityProduct
+ ,nrc_ep.c_RevenueCode
+ ,nrc_ep.c_DeferredRevenueCode
+FROM t_pv_NonRecurringCharge AS nrc
+INNER JOIN t_acc_usage AS acc ON nrc.id_sess = acc.id_sess
+LEFT JOIN t_ep_nonrecurring AS nrc_ep ON nrc_ep.id_prop = acc.id_pi_template
+WHERE c_NRCIntervalSubscriptionStart < %%START_DATE%%

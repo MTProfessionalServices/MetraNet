df3ee342aef6fa6251cc4e58e1e519d3961e8279
dgaz <dgaz@metratech.com>
2014-09-23 18:40:44 -0400

    FEAT-3979 MetraControl Partitions: Bill Groups
    code review damin

--- SqlCore/Queries/UsageServer/Samples/__DESCRIBE_BILLING_GROUPS_BY_PARTITION_AND_CONTINENT__.SqlServer.sql
+++ SqlCore/Queries/UsageServer/Samples/__DESCRIBE_BILLING_GROUPS_BY_PARTITION_AND_CONTINENT__.SqlServer.sql
@@ -7,7 +7,16 @@ INSERT INTO t_billgroup_tmp (id_materialization, tx_name, tx_description, id_par
 SELECT DISTINCT 
   %%ID_MATERIALIZATION%%, 
   tx_name,
-  tx_name + N' Bill Group',
+    CASE tx_name 
+    WHEN N'North America ' + CONVERT(varchar(10), id_partition) THEN N'Accounts with Bill-To addresses residing in a North American country in ' + tx_name
+    WHEN N'South America ' + CONVERT(varchar(10), id_partition) THEN N'Accounts with Bill-To addresses residing in a South American country in ' + tx_name 
+    WHEN N'Europe ' + CONVERT(varchar(10), id_partition) THEN N'Accounts with Bill-To addresses residing in an European country in ' + tx_name  
+    WHEN N'Default ' + CONVERT(varchar(10), id_partition) THEN N'Accounts that did not match other billing group rules in ' + tx_name 
+    WHEN N'North America' THEN N'Accounts with Bill-To addresses residing in a North American country'
+    WHEN N'South America' THEN N'Accounts with Bill-To addresses residing in a South American country' 
+    WHEN N'Europe' THEN N'Accounts with Bill-To addresses residing in an European country'  
+    WHEN N'Default' THEN N'Accounts that did not match other billing group rules'
+    END as description,
   id_partition
 FROM t_billgroup_member_tmp
 WHERE id_materialization = %%ID_MATERIALIZATION%%

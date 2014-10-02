5b0eaf7a31fb3d107de4dcac0a5c9035f071ddd4
skolesnik <skolesnik@metratech.com>
2014-10-02 07:29:38 -0400

    CORE-8585 CLONE - Archive Queue procedure for t_svc tables - CLONE

--- SqlCore/Install/Partitioning/StoredProcedures/__ARCHIVE_QUEUE__.SqlServer.sql
+++ SqlCore/Install/Partitioning/StoredProcedures/__ARCHIVE_QUEUE__.SqlServer.sql
@@ -1,4 +1,4 @@
-CREATE PROCEDURE archive_queue(
+alter PROCEDURE archive_queue(
     @update_stats    CHAR(1) = 'N',
     @sampling_ratio  VARCHAR(3) = '30',
     @current_time    DATETIME = NULL,
@@ -22,34 +22,52 @@ AS
 	PRINT @result	
 	*/
 	
-	SET NOCOUNT ON
+SET NOCOUNT ON;
+DECLARE @TRYAGAIN INT = 1;
+DECLARE @GainedAccess BIT = 1;
+DECLARE @AccessTable table (
+	[archival_in_progress] [bit] NULL,
+	[reconciliation_in_progress] [bit] NULL) ;
 
-	WHILE ( (select TOP 1 reconciliation_in_progress  from t_execution_monitoring)=1)
-       WAITFOR DELAY '00:00:02';
+WHILE ( @TRYAGAIN = 1)
+begin
+  update [t_execution_monitoring] 
+       SET archival_in_progress = 1
+	  OUTPUT INSERTED.archival_in_progress, INSERTED.reconciliation_in_progress into @AccessTable
+	  WHERE reconciliation_in_progress = 0;
 
-    update t_execution_monitoring set archival_in_progress = 1
-BEGIN
-Begin Try	
-	IF dbo.IsSystemPartitioned() = 1
-	    EXEC archive_queue_partition
+select top 1 @GainedAccess=reconciliation_in_progress from @AccessTable;
+
+ if (@GainedAccess = 1)
+    begin
+      /* Update didn't succeed goto sleep and try again */
+      WAITFOR DELAY '00:00:02';
+      CONTINUE;
+    END    
+  ELSE
+    BEGIN TRY
+  	  IF dbo.IsSystemPartitioned() = 1
+          EXEC archive_queue_partition
 	         @update_stats = @update_stats,
 	         @sampling_ratio = @sampling_ratio,
 	         @current_time = @current_time,
 	         @result = @result OUTPUT
-	ELSE
-	    EXEC archive_queue_nonpartition
+        ELSE
+          EXEC archive_queue_nonpartition
 	         @update_stats = @update_stats,
 	         @sampling_ratio = @sampling_ratio,
 	         @result = @result OUTPUT
-End Try
-Begin Catch
-SELECT
+        
+    END TRY
+    Begin Catch
+        SELECT
              ERROR_NUMBER() AS ErrorNumber
             ,ERROR_SEVERITY() AS ErrorSeverity
             ,ERROR_STATE() AS ErrorState
             ,ERROR_PROCEDURE() AS ErrorProcedure
             ,ERROR_LINE() AS ErrorLine
             ,ERROR_MESSAGE() AS ErrorMessage;
- End Catch
-End
-    update t_execution_monitoring set archival_in_progress = 0
+    End Catch
+  SET  @TRYAGAIN = 0
+END --while
+  update t_execution_monitoring  set archival_in_progress = 0

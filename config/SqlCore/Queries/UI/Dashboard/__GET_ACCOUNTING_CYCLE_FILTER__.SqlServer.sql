325c375d0d10f63a15591b014dc8c7f7676314dc
ZMartynov <zmartynov@metratech.com>
2014-10-07 06:42:06 -0400

    Fix queries with accounting cycles.

--- config/SqlCore/Queries/UI/Dashboard/__GET_ACCOUNTING_CYCLE_FILTER__.SqlServer.sql
+++ config/SqlCore/Queries/UI/Dashboard/__GET_ACCOUNTING_CYCLE_FILTER__.SqlServer.sql
@@ -7,4 +7,5 @@ SELECT DISTINCT
   FROM t_be_sys_rep_accountingcycle ac,
        t_be_sys_rep_accountingcycl acc2c
   WHERE ac.c_AccountingCycle_Id = acc2c.c_AccountingCycle_Id
-  order by ac.c_IsDefault DESC, ac.c_Name
+	OR ac.c_IsDefault = 'T'
+  ORDER BY ac.c_IsDefault DESC, ac.c_Name

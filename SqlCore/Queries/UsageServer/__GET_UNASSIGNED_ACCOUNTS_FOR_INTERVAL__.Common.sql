eb0eb229065f0ce747b308018d8489cf1a21fac5
dgaz <dgaz@metratech.com>
2014-10-07 11:56:24 -0400

    CORE-8601 "Manually Assign Accounts" displays all accounts for partitioned CSR
    Fix query so works for both Oracle and Sql Server
    
    Code reviewed by akaboli

--- SqlCore/Queries/UsageServer/__GET_UNASSIGNED_ACCOUNTS_FOR_INTERVAL__.Common.sql
+++ SqlCore/Queries/UsageServer/__GET_UNASSIGNED_ACCOUNTS_FOR_INTERVAL__.Common.sql
@@ -19,7 +19,7 @@ FROM vw_unassigned_accounts ua
 INNER JOIN vw_mps_or_system_acc_mapper av 
   ON ua.AccountID = av.id_acc 
 LEFT OUTER JOIN t_account_mapper tam on tam.id_acc = ua.AccountID
-LEFT OUTER JOIN t_account_mapper partition_root on partition_root.nm_login = tam.nm_space
+LEFT OUTER JOIN t_account_mapper partition_root on lower(partition_root.nm_login) = lower(tam.nm_space)
 %%WHERE_CLAUSE%%
 %%ORDER_BY_CLAUSE%%
    

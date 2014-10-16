d9ff1cd682b60524e0a2ae64f5201fae29c4df81
Jeff <jjohnson@metratech.com>
2014-10-16 16:38:33 -0400

    FEAT-4728 - Hooked up query handler.

---
+++ SqlCore/Queries/NotificationEvents/__INSERT_NOTIFICATION_EVENT_METADATA__.Oracle.sql
@@ -0,0 +1,6 @@
+INSERT INTO [dbo].[t_Notification_Event_Types]
+           ([id_notification_event_type]
+           ,[notification_event_name])
+     VALUES
+           (@NotificationEventMetaDataID
+           ,@NotificationEventName)
---
+++ SqlCore/Queries/NotificationEvents/__INSERT_NOTIFICATION_EVENT_METADATA__.SqlServer.sql
@@ -0,0 +1,7 @@
+INSERT INTO [dbo].[t_Notification_Event_Types]
+           ([id_notification_event_type]
+           ,[notification_event_name])
+     VALUES
+           (@NotificationEventMetaDataID
+           ,@NotificationEventName)
+

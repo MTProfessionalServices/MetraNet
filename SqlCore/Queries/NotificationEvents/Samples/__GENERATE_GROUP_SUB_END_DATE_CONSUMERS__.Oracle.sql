1ae3f6587cef4170d8edce7d298f40bc1e2210cf
Jeff <jjohnson@metratech.com>
2014-10-17 15:04:07 -0400

    FEAT-4728

---
+++ SqlCore/Queries/NotificationEvents/Samples/__GENERATE_GROUP_SUB_END_DATE_CONSUMERS__.Oracle.sql
@@ -0,0 +1,1 @@
+SELECT id_acc FROM t_account
---
+++ SqlCore/Queries/NotificationEvents/Samples/__INSERT_NOTIFICATION_EVENT_CONSUMER_ACCOUNT_IDS__.Oracle.sql
@@ -0,0 +1,8 @@
+INSERT INTO [t_Notification_Event_Consumers]
+           ([id_not_evnt_consumer]
+           ,[id_notification_event]
+           ,[id_acc])
+     VALUES
+           (@NotificationEventConsumerID
+           ,@NotificationEventID
+           ,@AccountID)

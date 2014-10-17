31219003e077082a3488fe63b6d6390268e475e9
Jeff <jjohnson@metratech.com>
2014-10-17 18:23:59 -0400

    FEAT-4728

--- SqlCore/Queries/NotificationEvents/Samples/__GENERATE_GROUP_ SUB_END_DATE_RECORDS__.Oracle.sql
+++ SqlCore/Queries/NotificationEvents/Samples/__GENERATE_GROUP_ SUB_END_DATE_RECORDS__.Oracle.sql
@@ -4,13 +4,13 @@ DECLARE @NOTIFICATION_DAYS int
 
 SET @NOTIFICATION_DAYS = 10 
 SET @MAX_SUB_END_DATE = DATEADD(DAY, DATEDIFF(day, 0, GETDATE()), @NOTIFICATION_DAYS) + '23:59:59'
-SEET @LAST_NOTIFY_FROM = DATEADD(D, @NOTIFICATION_DAYS, %%LAST_RUN_DATE%%) 
+SET @LAST_NOTIFY_FROM = DATEADD(D, @NOTIFICATION_DAYS, @LAST_RUN_DATE) 
 
 SELECT
-'SUB' varSubType, sub.id_acc varAccount, sub.vt_end varEndDate, tbp.nm_display_name varDisplayName
-FROM t_sub sub
-INNER JOIN t_vw_base_props tbp 
-	ON sub.id_po = tbp.id_prop 
-	AND tbp.id_lang_code = %%LANGUAGE_CODE%%
-WHERE sub.vt_end <= @MAX_SUB_END_DATE 
-AND sub.vt_end >= @LAST_NOTIFY_FROM
+'GROUP' varSubType, gsubmember.id_acc varAccount, gsubmember.vt_end varEndDate, gsub.tx_name varDisplayName
+FROM t_gsubmember gsubmember
+INNER JOIN t_group_sub gsub on gsubmember.id_group = gsub.id_group
+INNER JOIN t_account_mapper map 
+  ON map.id_acc = gsubmember.id_acc
+WHERE gsubmember.vt_end <= @MAX_SUB_END_DATE 
+AND gsubmember.vt_end >= @LAST_NOTIFY_FROM
--- SqlCore/Queries/NotificationEvents/Samples/__GENERATE_SUB_END_DATE_RECORDS__.Oracle.sql
+++ SqlCore/Queries/NotificationEvents/Samples/__GENERATE_SUB_END_DATE_RECORDS__.Oracle.sql
@@ -4,13 +4,15 @@ DECLARE @NOTIFICATION_DAYS int
 
 SET @NOTIFICATION_DAYS = 10 
 SET @MAX_SUB_END_DATE = DATEADD(DAY, DATEDIFF(day, 0, GETDATE()), @NOTIFICATION_DAYS) + '23:59:59'
-SET @LAST_NOTIFY_FROM = DATEADD(D, @NOTIFICATION_DAYS, %%LAST_RUN_DATE%%) 
+SET @LAST_NOTIFY_FROM = DATEADD(D, @NOTIFICATION_DAYS, @LAST_RUN_DATE)
 
 SELECT
-'SUB' varSubType, sub.id_acc varAccount, sub.vt_end varEndDate, tbp.nm_display_name varDisplayName
+'SUB' varSubType, map.nm_login, sub.id_acc varAccount, sub.vt_end varEndDate, tbp.nm_display_name varDisplayName
 FROM t_sub sub
 INNER JOIN t_vw_base_props tbp
 	ON sub.id_po = tbp.id_prop 
-	AND tbp.id_lang_code = %%LANGUAGE_CODE%%
+	AND tbp.id_lang_code = @LANGUAGE_CODE
+INNER JOIN t_account_mapper map 
+  ON map.id_acc = sub.id_acc
 WHERE sub.vt_end <= @MAX_SUB_END_DATE 
 AND sub.vt_end >= @LAST_NOTIFY_FROM

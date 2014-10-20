d1a85b5ee3ae71f084b38c85da60c061dd5ac7b2
vkurylovich <vkurylovich@metratech.com>
2014-10-20 16:52:18 -0400

    FEAT-4826: Write sample queries for subscription end date to generate the events
    -replaced Product Offering Display Name with its Internal Name

--- SqlCore/Queries/NotificationEvents/Samples/__GENERATE_GROUP_ SUB_END_DATE_RECORDS__.SqlServer.sql
+++ SqlCore/Queries/NotificationEvents/Samples/__GENERATE_GROUP_ SUB_END_DATE_RECORDS__.SqlServer.sql
@@ -14,15 +14,15 @@ SELECT
   END,
   gsubmember.vt_end varEndDate,
   sub.id_po varPOID,
-  gsub.tx_name varDisplayName,
+  tbp.nm_name varPOInternalName,
   t_po.c_POPartitionId varPartitionID
 FROM t_gsubmember gsubmember
-  INNER JOIN t_group_sub gsub ON gsubmember.id_group = gsub.id_group
-  INNER JOIN t_sub sub ON gsub.id_group=sub.id_group
+  INNER JOIN t_sub sub ON gsubmember.id_group=sub.id_group
+  INNER JOIN t_vw_base_props tbp ON sub.id_po = tbp.id_prop AND tbp.id_lang_code = @LANGUAGE_CODE
   INNER JOIN t_po ON sub.id_po = t_po.id_po
   LEFT OUTER JOIN t_account_mapper partition_owner ON partition_owner.id_acc = t_po.c_POPartitionId and partition_owner.nm_space = 'mt' and partition_owner.id_acc != 1
   /*now we will get the nm_login for the subscriber account*/
   LEFT OUTER JOIN t_account_mapper tmap_for_nonpartitioned_account ON tmap_for_nonpartitioned_account.id_acc = gsubmember.id_acc and tmap_for_nonpartitioned_account.nm_space = 'mt'
-  LEFT OUTER JOIN t_account_mapper tmap_for_partitioned_account ON tmap_for_partitioned_account.id_acc = gsubmember.id_acc and tmap_for_partitioned_account.nm_space = partition_owner.nm_login   
+  LEFT OUTER JOIN t_account_mapper tmap_for_partitioned_account ON tmap_for_partitioned_account.id_acc = gsubmember.id_acc and tmap_for_partitioned_account.nm_space = LOWER (partition_owner.nm_login)  
 WHERE gsubmember.vt_end <= @MAX_SUB_END_DATE 
   AND gsubmember.vt_end >= @LAST_NOTIFY_FROM
--- SqlCore/Queries/NotificationEvents/Samples/__GENERATE_SUB_END_DATE_RECORDS__.SqlServer.sql
+++ SqlCore/Queries/NotificationEvents/Samples/__GENERATE_SUB_END_DATE_RECORDS__.SqlServer.sql
@@ -16,7 +16,7 @@ SELECT
   END,
   sub.vt_end varEndDate,
   sub.id_po varPOID,
-  tbp.nm_display_name varDisplayName,
+  tbp.nm_name varPOInternalName,
   t_po.c_POPartitionId varPartitionID
 FROM t_sub sub
   INNER JOIN t_vw_base_props tbp ON sub.id_po = tbp.id_prop AND tbp.id_lang_code = @LANGUAGE_CODE
@@ -24,12 +24,12 @@ FROM t_sub sub
   LEFT OUTER JOIN t_account_mapper partition_owner ON partition_owner.id_acc = t_po.c_POPartitionId and partition_owner.nm_space = 'mt' and partition_owner.id_acc != 1
   /*now we will get the nm_login for the subscriber account*/
   LEFT OUTER JOIN t_account_mapper tmap_for_nonpartitioned_account ON tmap_for_nonpartitioned_account.id_acc = sub.id_acc and tmap_for_nonpartitioned_account.nm_space = 'mt'
-  LEFT OUTER JOIN t_account_mapper tmap_for_partitioned_account ON tmap_for_partitioned_account.id_acc = sub.id_acc and tmap_for_partitioned_account.nm_space = partition_owner.nm_login  
+  LEFT OUTER JOIN t_account_mapper tmap_for_partitioned_account ON tmap_for_partitioned_account.id_acc = sub.id_acc and tmap_for_partitioned_account.nm_space = LOWER (partition_owner.nm_login)  
 WHERE sub.vt_end <= @MAX_SUB_END_DATE 
   AND sub.vt_end >= @LAST_NOTIFY_FROM
   AND sub.id_group IS NULL  
 UNION
-SELECT sq.varSubType, sq.varAccount, sq.varLoginName,sq.varEndDate, sq.varPOID, sq.varDisplayName, sq.varPartitionID
+SELECT sq.varSubType, sq.varAccount, sq.varLoginName,sq.varEndDate, sq.varPOID, sq.varPOInternalName, sq.varPartitionID
 FROM(
        SELECT
          'SUB' varSubType,
@@ -41,14 +41,14 @@ FROM(
          END,
          sub.vt_end varEndDate,
          sub.id_po varPOID,
-         tbp.nm_display_name varDisplayName,
+         tbp.nm_name varPOInternalName,
          t_po.c_POPartitionId varPartitionID
        FROM t_sub sub
          INNER JOIN t_po ON sub.id_po = t_po.id_po
          LEFT OUTER JOIN t_account_mapper partition_owner ON partition_owner.id_acc = t_po.c_POPartitionId and partition_owner.nm_space = 'mt' and partition_owner.id_acc != 1
          /*now we will get the nm_login for the subscriber account*/
          LEFT OUTER JOIN t_account_mapper tmap_for_nonpartitioned_account ON tmap_for_nonpartitioned_account.id_acc = sub.id_acc and tmap_for_nonpartitioned_account.nm_space = 'mt'
-         LEFT OUTER JOIN t_account_mapper tmap_for_partitioned_account ON tmap_for_partitioned_account.id_acc = sub.id_acc and tmap_for_partitioned_account.nm_space = partition_owner.nm_login  
+         LEFT OUTER JOIN t_account_mapper tmap_for_partitioned_account ON tmap_for_partitioned_account.id_acc = sub.id_acc and tmap_for_partitioned_account.nm_space = LOWER (partition_owner.nm_login)  
          INNER JOIN t_sub_history tsh ON sub.id_sub = tsh.id_sub AND sub.vt_end = tsh.vt_end
          INNER JOIN t_sub_history tsh2 ON sub.id_sub = tsh2.id_sub
          INNER JOIN t_vw_base_props tbp ON sub.id_po = tbp.id_prop AND tbp.id_lang_code = @LANGUAGE_CODE
@@ -60,10 +60,4 @@ FROM(
          AND CAST(tsh2.dt_crt AS DATE) <= CAST(tsh.dt_crt AS DATE)
          AND tsh2.vt_end > @MAX_SUB_END_DATE
          AND tsh2.vt_end <> tsh.vt_end) sq
-GROUP BY sq.varSubType, sq.varAccount, sq.varLoginName, sq.varEndDate, sq.varPOID, sq.varDisplayName, sq.varPartitionID
-
-
-
-
-
-
+GROUP BY sq.varSubType, sq.varAccount, sq.varLoginName, sq.varEndDate, sq.varPOID, sq.varPOInternalName, sq.varPartitionID

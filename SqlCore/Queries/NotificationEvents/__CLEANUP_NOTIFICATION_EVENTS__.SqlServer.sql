9cbecec5b79b5a37b3cffee91cd2f149f0629c1e
dgaz <dgaz@metratech.com>
2014-10-21 15:59:13 -0400

    FEAT-4581 Generate Notification Events - Scheduled Adapter
    code review by akaboli

--- SqlCore/Queries/NotificationEvents/__CLEANUP_NOTIFICATION_EVENTS__.SqlServer.sql
+++ SqlCore/Queries/NotificationEvents/__CLEANUP_NOTIFICATION_EVENTS__.SqlServer.sql
@@ -1,5 +1,9 @@
 begin 
 
+delete consumer from t_notification_event_consumers consumer
+inner join t_notification_events event on (consumer.id_notification_event = event.id_notification_event)
+where event.dt_crt < @cleanupdate;
+
 delete from t_notification_events
 where dt_crt < @cleanupdate;
 

383c7ca53581fd7a62f645e44372b77fd7317216
dgaz <dgaz@metratech.com>
2014-10-22 18:51:09 -0400

    FEAT-4728 Create Notification Event Handler

--- SqlCore/Queries/NotificationEvents/Samples/__INSERT_NOTIFICATION_EVENT_CONSUMER_ACCOUNT_IDS__.SqlServer.sql
+++ SqlCore/Queries/NotificationEvents/Samples/__INSERT_NOTIFICATION_EVENT_CONSUMER_ACCOUNT_IDS__.SqlServer.sql
@@ -1,8 +1,10 @@
 INSERT INTO t_Notification_Event_Consumers
            (id_not_evnt_consumer
            ,id_notification_event
-           ,id_acc)
+           ,id_acc
+           ,dt_crt)
      VALUES
            (@NotificationEventConsumerID
            ,@NotificationEventID
-           ,@AccountID)
+           ,@AccountID
+           ,@DtCrt)

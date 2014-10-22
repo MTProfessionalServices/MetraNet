e9725bae1fa4b852535a78cf3067e06d0e442308
damin <damin@metratech.com>
2014-10-22 16:30:50 -0400

    FEAT-3980 Subscription End Date Notification FEAT-4760 AjaxService to get list of notifications for the logged in user
    Implemented sorting, paging functionality

--- SqlCore/Queries/NotificationEvents/__GET_NOTIFICATION_EVENTS_FOR_LOGGED_IN_USER__.Common.sql
+++ SqlCore/Queries/NotificationEvents/__GET_NOTIFICATION_EVENTS_FOR_LOGGED_IN_USER__.Common.sql
@@ -7,4 +7,3 @@ FROM t_notification_event_consumers consumers
 INNER JOIN t_Notification_Events events ON events.id_notification_event = consumers.id_notification_event
 INNER JOIN t_notification_event_types eventTypes ON eventTypes.id_notification_event_type = events.id_notification_event_type
 WHERE consumers.id_acc = %%ID_ACC%%
-ORDER BY events.dt_crt DESC

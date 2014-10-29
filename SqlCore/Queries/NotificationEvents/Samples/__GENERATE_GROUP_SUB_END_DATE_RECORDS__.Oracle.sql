38b2f28eac0a80840e7f9b830256d6760d893d2f
unknown <Administrator@devtmploraw.metratech.com>
2014-10-29 16:52:16 -0400

    CORE-8707: Generate Notification Events Adapter failed on Oracle
    -fixed oracle syntax errors

--- SqlCore/Queries/NotificationEvents/Samples/__GENERATE_GROUP_SUB_END_DATE_RECORDS__.Oracle.sql
+++ SqlCore/Queries/NotificationEvents/Samples/__GENERATE_GROUP_SUB_END_DATE_RECORDS__.Oracle.sql
@@ -1,30 +1,27 @@
-DECLARE @NOTIFICATION_DAYS int
-DECLARE @LAST_NOTIFY_FROM DateTime
-DECLARE @MAX_SUB_END_DATE DateTime
-
-SET @NOTIFICATION_DAYS = 10 /*Number of days before the subscription expires*/
-SET @MAX_SUB_END_DATE = DATEADD(DAY, DATEDIFF(day, 0, @CURRENT_RUN_DATE), @NOTIFICATION_DAYS) + '23:59:59'
-SET @LAST_NOTIFY_FROM = DATEADD(D, @NOTIFICATION_DAYS, @LAST_RUN_DATE) 
-
+WITH 
+constants AS ( SELECT 
+    10 AS NOTIFICATION_DAYS
+    FROM DUAL
+)
 SELECT
   'GROUPSUB' varSubType,
-  gsubmember.id_acc varAccount,
-  varLoginName = 
+  gsubmember.id_acc varAccount,   
   CASE 
-    WHEN tmap_for_partitioned_account.nm_login IS NULL THEN tmap_for_nonpartitioned_account.nm_login
-    ELSE tmap_for_partitioned_account.nm_login
-  END,
+    WHEN tmap_for_partitioned_acc.nm_login IS NULL THEN tmap_for_nonpartitioned_acc.nm_login
+    ELSE tmap_for_partitioned_acc.nm_login
+  END varLoginName,
   gsubmember.vt_end varEndDate,
   sub.id_po varPOID,
   tbp.nm_name varPOInternalName,
   t_po.c_POPartitionId id_Partition
-FROM t_gsubmember gsubmember
+FROM constants, t_gsubmember gsubmember
   INNER JOIN t_sub sub ON gsubmember.id_group=sub.id_group
-  INNER JOIN t_vw_base_props tbp ON sub.id_po = tbp.id_prop AND tbp.id_lang_code = @LANGUAGE_CODE
+  INNER JOIN t_vw_base_props tbp ON sub.id_po = tbp.id_prop AND tbp.id_lang_code = :LANGUAGE_CODE
   INNER JOIN t_po ON sub.id_po = t_po.id_po
-  LEFT OUTER JOIN t_account_mapper partition_owner ON partition_owner.id_acc = t_po.c_POPartitionId and partition_owner.nm_space = 'mt' and partition_owner.id_acc != 1
+  LEFT OUTER JOIN t_account_mapper partition_owner ON partition_owner.id_acc = t_po.c_POPartitionId AND partition_owner.nm_space = 'mt' AND partition_owner.id_acc != 1
   /*now we will get the nm_login for the subscriber account*/
-  LEFT OUTER JOIN t_account_mapper tmap_for_nonpartitioned_account ON tmap_for_nonpartitioned_account.id_acc = gsubmember.id_acc and tmap_for_nonpartitioned_account.nm_space = 'mt'
-  LEFT OUTER JOIN t_account_mapper tmap_for_partitioned_account ON tmap_for_partitioned_account.id_acc = gsubmember.id_acc and tmap_for_partitioned_account.nm_space = LOWER (partition_owner.nm_login)  
-WHERE gsubmember.vt_end <= @MAX_SUB_END_DATE 
-  AND gsubmember.vt_end >= @LAST_NOTIFY_FROM
+  LEFT OUTER JOIN t_account_mapper tmap_for_nonpartitioned_acc ON tmap_for_nonpartitioned_acc.id_acc = gsubmember.id_acc AND tmap_for_nonpartitioned_acc.nm_space = 'mt'
+  LEFT OUTER JOIN t_account_mapper tmap_for_partitioned_acc ON tmap_for_partitioned_acc.id_acc = gsubmember.id_acc AND tmap_for_partitioned_acc.nm_space = LOWER (partition_owner.nm_login) 
+  WHERE gsubmember.vt_end <= :CURRENT_RUN_DATE + constants.NOTIFICATION_DAYS 
+  AND gsubmember.vt_end >= :LAST_RUN_DATE + constants.NOTIFICATION_DAYS 
+  
--- SqlCore/Queries/NotificationEvents/Samples/__GENERATE_SUB_END_DATE_RECORDS__.Oracle.sql
+++ SqlCore/Queries/NotificationEvents/Samples/__GENERATE_SUB_END_DATE_RECORDS__.Oracle.sql
@@ -1,70 +1,64 @@
-DECLARE @NOTIFICATION_DAYS int
-DECLARE @SUBSCRIPTION_WINDOW int
-DECLARE @LAST_NOTIFY_FROM DateTime
-DECLARE @MAX_SUB_END_DATE DateTime
-
-SET @NOTIFICATION_DAYS = 10 /*Number of days before the subscription expires*/
-SET @SUBSCRIPTION_WINDOW = 90
-SET @MAX_SUB_END_DATE = DATEADD(DAY, DATEDIFF(day, 0, @CURRENT_RUN_DATE), @NOTIFICATION_DAYS) + '23:59:59'
-SET @LAST_NOTIFY_FROM = DATEADD(D, @NOTIFICATION_DAYS, @LAST_RUN_DATE) 
-
+WITH 
+constants AS ( SELECT 
+    10 AS NOTIFICATION_DAYS, /*Number of days before the subscription expires*/
+    20 AS SUBSCRIPTION_WINDOW
+    FROM DUAL
+)
 SELECT
   'SUB' varSubType,
-  sub.id_acc varAccount,
-  varLoginName = 
+  sub.id_acc varAccount,  
   CASE 
-    WHEN tmap_for_partitioned_account.nm_login IS NULL THEN tmap_for_nonpartitioned_account.nm_login
-    ELSE tmap_for_partitioned_account.nm_login
-  END,
+    WHEN tmap_for_partitioned_acc.nm_login IS NULL THEN tmap_for_nonpartitioned_acc.nm_login
+    ELSE tmap_for_partitioned_acc.nm_login
+  END varLoginName,
   sub.vt_end varEndDate,
   sub.id_po varPOID,
   tbp.nm_name varPOInternalName,
   t_po.c_POPartitionId id_Partition
-FROM t_sub sub
-  INNER JOIN t_vw_base_props tbp ON sub.id_po = tbp.id_prop AND tbp.id_lang_code = @LANGUAGE_CODE
+FROM constants, t_sub sub
+  INNER JOIN t_vw_base_props tbp ON sub.id_po = tbp.id_prop AND tbp.id_lang_code = :LANGUAGE_CODE
   INNER JOIN t_po ON sub.id_po = t_po.id_po
   INNER JOIN t_sub_history tsh ON sub.id_sub = tsh.id_sub AND sub.vt_end = tsh.vt_end
-  LEFT OUTER JOIN t_account_mapper partition_owner ON partition_owner.id_acc = t_po.c_POPartitionId and partition_owner.nm_space = 'mt' and partition_owner.id_acc != 1
+  LEFT OUTER JOIN t_account_mapper partition_owner ON partition_owner.id_acc = t_po.c_POPartitionId AND partition_owner.nm_space = 'mt' AND partition_owner.id_acc != 1
   /*now we will get the nm_login for the subscriber account*/
-  LEFT OUTER JOIN t_account_mapper tmap_for_nonpartitioned_account ON tmap_for_nonpartitioned_account.id_acc = sub.id_acc and tmap_for_nonpartitioned_account.nm_space = 'mt'
-  LEFT OUTER JOIN t_account_mapper tmap_for_partitioned_account ON tmap_for_partitioned_account.id_acc = sub.id_acc and tmap_for_partitioned_account.nm_space = LOWER (partition_owner.nm_login)  
-WHERE sub.vt_end <= @MAX_SUB_END_DATE 
-  AND sub.vt_end >= @LAST_NOTIFY_FROM
+  LEFT OUTER JOIN t_account_mapper tmap_for_nonpartitioned_acc ON tmap_for_nonpartitioned_acc.id_acc = sub.id_acc AND tmap_for_nonpartitioned_acc.nm_space = 'mt'
+  LEFT OUTER JOIN t_account_mapper tmap_for_partitioned_acc ON tmap_for_partitioned_acc.id_acc = sub.id_acc AND tmap_for_partitioned_acc.nm_space = LOWER (partition_owner.nm_login)  
+WHERE sub.vt_end <= :CURRENT_RUN_DATE + constants.NOTIFICATION_DAYS
+  AND sub.vt_end >= :LAST_RUN_DATE + constants.NOTIFICATION_DAYS
   AND sub.id_group IS NULL
-   /* if the end date for the subscription was set within @SUBSCRIPTION_WINDOW, then don’t warn about it */
-  AND tsh.dt_crt <= DATEADD(d, -@SUBSCRIPTION_WINDOW, @CURRENT_RUN_DATE)
+   /* if the end date for the subscription was set within SUBSCRIPTION_WINDOW, then don’t warn about it */
+  AND tsh.dt_crt <= :CURRENT_RUN_DATE - constants.SUBSCRIPTION_WINDOW    
 UNION
 SELECT sq.varSubType, sq.varAccount, sq.varLoginName,sq.varEndDate, sq.varPOID, sq.varPOInternalName, sq.id_Partition
 FROM(
        SELECT
          'SUB' varSubType,
-         sub.id_acc varAccount,
-         varLoginName = 
+         sub.id_acc varAccount,         
          CASE 
-          WHEN tmap_for_partitioned_account.nm_login IS NULL THEN tmap_for_nonpartitioned_account.nm_login
-          ELSE tmap_for_partitioned_account.nm_login
-         END,
+          WHEN tmap_for_partitioned_acc.nm_login IS NULL THEN tmap_for_nonpartitioned_acc.nm_login
+          ELSE tmap_for_partitioned_acc.nm_login
+         END varLoginName,
          sub.vt_end varEndDate,
          sub.id_po varPOID,
          tbp.nm_name varPOInternalName,
          t_po.c_POPartitionId id_Partition
-       FROM t_sub sub
+       FROM constants, t_sub sub
          INNER JOIN t_po ON sub.id_po = t_po.id_po
-         LEFT OUTER JOIN t_account_mapper partition_owner ON partition_owner.id_acc = t_po.c_POPartitionId and partition_owner.nm_space = 'mt' and partition_owner.id_acc != 1
+         LEFT OUTER JOIN t_account_mapper partition_owner ON partition_owner.id_acc = t_po.c_POPartitionId AND partition_owner.nm_space = 'mt' AND partition_owner.id_acc != 1
          /*now we will get the nm_login for the subscriber account*/
-         LEFT OUTER JOIN t_account_mapper tmap_for_nonpartitioned_account ON tmap_for_nonpartitioned_account.id_acc = sub.id_acc and tmap_for_nonpartitioned_account.nm_space = 'mt'
-         LEFT OUTER JOIN t_account_mapper tmap_for_partitioned_account ON tmap_for_partitioned_account.id_acc = sub.id_acc and tmap_for_partitioned_account.nm_space = LOWER (partition_owner.nm_login)  
+         LEFT OUTER JOIN t_account_mapper tmap_for_nonpartitioned_acc ON tmap_for_nonpartitioned_acc.id_acc = sub.id_acc AND tmap_for_nonpartitioned_acc.nm_space = 'mt'
+         LEFT OUTER JOIN t_account_mapper tmap_for_partitioned_acc ON tmap_for_partitioned_acc.id_acc = sub.id_acc AND tmap_for_partitioned_acc.nm_space = LOWER (partition_owner.nm_login)  
          INNER JOIN t_sub_history tsh ON sub.id_sub = tsh.id_sub AND sub.vt_end = tsh.vt_end
          INNER JOIN t_sub_history tsh2 ON sub.id_sub = tsh2.id_sub
-         INNER JOIN t_vw_base_props tbp ON sub.id_po = tbp.id_prop AND tbp.id_lang_code = @LANGUAGE_CODE
-       WHERE sub.vt_end <= @LAST_NOTIFY_FROM
-         AND sub.vt_end > @CURRENT_RUN_DATE
+         INNER JOIN t_vw_base_props tbp ON sub.id_po = tbp.id_prop AND tbp.id_lang_code = :LANGUAGE_CODE
+       WHERE sub.vt_end <= :LAST_RUN_DATE + constants.NOTIFICATION_DAYS
+         AND sub.vt_end > :CURRENT_RUN_DATE
          AND sub.id_group IS NULL 
-         AND tsh.dt_crt <= @LAST_NOTIFY_FROM
-          /* if the end date for the subscription was set within @SUBSCRIPTION_WINDOW, then don’t warn about it */
-         AND tsh.dt_crt <= DATEADD(d, -@SUBSCRIPTION_WINDOW, @CURRENT_RUN_DATE)
-         AND tsh.dt_crt >= @LAST_RUN_DATE         
-         AND CAST(tsh2.dt_crt AS DATE) <= CAST(tsh.dt_crt AS DATE)
-         AND tsh2.vt_end > @MAX_SUB_END_DATE
-         AND tsh2.vt_end <> tsh.vt_end) sq
+         AND tsh.dt_crt <= :LAST_RUN_DATE + constants.NOTIFICATION_DAYS
+          /* if the end date for the subscription was set within SUBSCRIPTION_WINDOW, then don’t warn about it */
+         AND tsh.dt_crt <= :CURRENT_RUN_DATE - constants.SUBSCRIPTION_WINDOW
+         AND tsh.dt_crt >= :LAST_RUN_DATE         
+         AND CAST(tsh2.dt_crt AS DATE) <= CAST(tsh.dt_crt AS DATE) 
+         AND tsh2.vt_end > :CURRENT_RUN_DATE + constants.NOTIFICATION_DAYS
+         AND tsh2.vt_end <> tsh.vt_end) sq         
 GROUP BY sq.varSubType, sq.varAccount, sq.varLoginName, sq.varEndDate, sq.varPOID, sq.varPOInternalName, sq.id_Partition

a566c4da9fa455ab17f6f141c69f60a76621ebf6
akaboli <akaboli@metratech.com>
2014-10-30 13:45:14 -0400

    FEAT-4846
    MetraNet Notifications Screen
    FEAT-4846 MetraNet Notifications Screen FEAT-4928
    FEAT-4846 MetraNet Notifications Screen FEAT-4927
    Modify the query to return partition name so it can be displayed (hidden by default?) and filtered/sorted on

--- SqlCore/Queries/NotificationEvents/__GET_NOTIFICATION_EVENTS_FOR_LOGGED_IN_USER__.Common.sql
+++ SqlCore/Queries/NotificationEvents/__GET_NOTIFICATION_EVENTS_FOR_LOGGED_IN_USER__.Common.sql
@@ -1,7 +1,9 @@
 SELECT 
 	eventTypes.notification_event_name,
 	events.notification_event_prop_values,
-	events.dt_crt 
+	events.dt_crt,
+  events.id_partition,
+events.id_notification_event  
 FROM t_notification_event_consumers consumers
 INNER JOIN t_Notification_Events events ON events.id_notification_event = consumers.id_notification_event
 INNER JOIN t_notification_event_types eventTypes ON eventTypes.id_notification_event_type = events.id_notification_event_type

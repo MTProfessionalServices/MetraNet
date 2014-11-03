3b277b7b6ff17d31ef3e3c912b519fd89be0406f
damin <damin@metratech.com>
2014-11-03 13:33:12 -0500

    CORE-7898 [Bling] Operations Dashboard @ ORA: Bill Close Synopsis has wrong values

--- config/SqlCore/Queries/UI/Dashboard/__GET_BILLCLOSESYNOPSIS_SUMMARY__.Oracle.sql
+++ config/SqlCore/Queries/UI/Dashboard/__GET_BILLCLOSESYNOPSIS_SUMMARY__.Oracle.sql
@@ -6,12 +6,12 @@
          WHEN 19 THEN 'M19'
          WHEN 26 THEN 'M26'
          ELSE 'EOM'
-      END AS IntervalType,
+      END AS "type",
       (select COUNT(*) from t_billgroup_materialization where id_usage_interval = %%ID_USAGE_INTERVAL%%) as BillGroups,
       ui.id_interval AS IntervalID,
-      ui.dt_start AS DtStart,
-      ui.dt_end AS DtEnd,
+      ui.dt_start AS "start",
+      ui.dt_end AS "end",
       tx_interval_status AS Interval_Status, 
-      TRUNC(GETUTCDATE())-TRUNC(ui.dt_end)+1 AS Days_Until_Run
+      TRUNC(ui.dt_end - GETUTCDATE()) + 1 AS Days_Until_Run
       from t_usage_interval ui
       where ui.id_interval = %%ID_USAGE_INTERVAL%%

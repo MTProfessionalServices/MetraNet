b312f7a3c3a66a182c14465fdd5e8209a7dcb39d
damin <damin@metratech.com>
2014-11-06 14:21:43 -0500

    FEAT-4846 MetraNet Notifications Screen FEAT-4976 Localize Event Type names in filter and grid results
    Code reviewer: dgaz

--- SqlCore/Queries/NotificationEvents/__GET_NOTIFICATION_EVENTS_FOR_LOGGED_IN_USER__.SqlServer.sql
+++ SqlCore/Queries/NotificationEvents/__GET_NOTIFICATION_EVENTS_FOR_LOGGED_IN_USER__.SqlServer.sql
@@ -3,8 +3,11 @@ SELECT
 	events.notification_event_prop_values,
 	events.dt_crt,
   events.id_partition,
-events.id_notification_event  
+	events.id_notification_event,
+	ISNULL(eventnamedesc.tx_desc, eventTypes.notification_event_name)  as 'localized_event_name'
 FROM t_notification_event_consumers consumers
 INNER JOIN t_Notification_Events events ON events.id_notification_event = consumers.id_notification_event
 INNER JOIN t_notification_event_types eventTypes ON eventTypes.id_notification_event_type = events.id_notification_event_type
+LEFT OUTER JOIN t_enum_data edata on LOWER(edata.nm_enum_data) = CONCAT('ui/notificationeventtype/', LOWER(eventTypes.notification_event_name))
+LEFT OUTER JOIN t_description eventnamedesc on eventnamedesc.id_desc = edata.id_enum_data and eventnamedesc.id_lang_code = %%ID_LANG%%
 WHERE consumers.id_acc = %%ID_ACC%%

aedc11eb04deef39046cafc6d7f2cf68d6b5878c
dgaz <dgaz@metratech.com>
2014-11-06 19:19:56 -0500

    CORE-8746 [FEAT-3980] [ORA] Adapter run info display "deleted -1 events" instead of actual amount

--- SqlCore/Queries/NotificationEvents/__CLEANUP_NOTIFICATION_EVENTS__.SqlServer.sql
+++ SqlCore/Queries/NotificationEvents/__CLEANUP_NOTIFICATION_EVENTS__.SqlServer.sql
@@ -1,10 +1,2 @@
-begin 
-
-delete consumer from t_notification_event_consumers consumer
-inner join t_notification_events event on (consumer.id_notification_event = event.id_notification_event)
-where event.dt_crt < @cleanupdate;
-
 delete from t_notification_events
-where dt_crt < @cleanupdate;
-
-end;
+where dt_crt < @cleanupdate

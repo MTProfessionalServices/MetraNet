3f75d03a1fb6ef0d052136d308547f8e88648b9c
dgaz <dgaz@metratech.com>
2014-11-12 11:06:06 -0500

    FEAT-3850 Bling: Productize Finance DashboardFEAT-5071 Add start and end date fields in Customer table and use the new fields in GET_REPORT_NEWCUSTOMERS_CHART query
    code review by damin

--- config/SqlCore/Queries/UI/Dashboard/__GET_REPORT_NEWCUSTOMERS_CHART__.Oracle.sql
+++ config/SqlCore/Queries/UI/Dashboard/__GET_REPORT_NEWCUSTOMERS_CHART__.Oracle.sql
@@ -1,10 +1,5 @@
-select 1 from dual
-/* The above query has be removed once we have Subscription table implemented*/
-/* Subscription table is not implemented yet, so the query was commented out*/
-/*SELECT c.AccountId as Account,
-       trunc(st.StartDate,'MON')
+SELECT c.MetraNetId as Account,
+       trunc(c.StartDate,'MON') as "Date"
   FROM Customer c
-       join Subscription st on c.AccountId = st.AccountId
- WHERE st.StartDate IS NOT NULL
-   AND st.StartDate >= add_months(GETUTCDATE(), -13)
-   AND st.StartDate <= add_months(GETUTCDATE(), -1)*/
+ WHERE c.StartDate >= add_months(GETUTCDATE(), -13)
+   AND c.StartDate <= add_months(GETUTCDATE(), -1)
--- config/SqlCore/Queries/UI/Dashboard/__GET_REPORT_NEWCUSTOMERS_CHART__.SqlServer.sql
+++ config/SqlCore/Queries/UI/Dashboard/__GET_REPORT_NEWCUSTOMERS_CHART__.SqlServer.sql
@@ -1,11 +1,8 @@
-/* Subscription table is not implemented yet, so the query was commented out*/
-/*DECLARE @dateFrom DATE, @dateTo DATE
+DECLARE @dateFrom DATE, @dateTo DATE
 SET @dateFrom = DATEADD(month, -13, GETUTCDATE());
 SET @dateTo = DATEADD(month, -1, GETUTCDATE());
-SELECT c.AccountId as Account,
-       CONVERT(DATE, CONCAT(DATEPART(month, st.StartDate),'-','01','-',DATEPART(year, st.StartDate)), 110) as [Date]
+SELECT c.MetraNetId as Account,
+       CONVERT(DATE, CONCAT(DATEPART(month, c.StartDate),'-','01','-',DATEPART(year, c.StartDate)), 110) as [Date]
   FROM Customer c
-       join Subscription st on c.AccountId = st.AccountId
- WHERE st.StartDate IS NOT NULL
-   AND st.StartDate >= @dateFrom
-   AND st.StartDate <= @dateTo*/
+ WHERE c.StartDate >= @dateFrom
+   AND c.StartDate <= @dateTo

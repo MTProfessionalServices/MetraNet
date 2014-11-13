b5051b6609b769f296c2766b55a794c783b4155f
Jeff <jjohnson@metratech.com>
2014-11-13 15:52:03 -0500

    CORE-7908

--- config/SqlCore/Queries/UI/Dashboard/__GET_BILLCLOSESYNOPSIS_SUMMARY__.Oracle.sql
+++ config/SqlCore/Queries/UI/Dashboard/__GET_BILLCLOSESYNOPSIS_SUMMARY__.Oracle.sql
@@ -1,12 +1,12 @@
 --Get Interval Data
 	SELECT
-      CASE extract(day from ui.dt_end)
-         WHEN 5 THEN 'M5'
-         WHEN 12 THEN 'M12'
-         WHEN 19 THEN 'M19'
-         WHEN 26 THEN 'M26'
-         ELSE 'EOM'
-      END AS "type",
+      --CASE extract(day from ui.dt_end)
+       --  WHEN 5 THEN 'M5'
+       --  WHEN 12 THEN 'M12'
+       --  WHEN 19 THEN 'M19'
+       --  WHEN 26 THEN 'M26'
+       --  ELSE 'EOM'
+     -- END AS "type",
       (select COUNT(*) from t_billgroup_materialization where id_usage_interval = %%ID_USAGE_INTERVAL%%) as BillGroups,
       ui.id_interval AS IntervalID,
       ui.dt_start AS "start",
--- config/SqlCore/Queries/UI/Dashboard/__GET_BILLCLOSESYNOPSIS_SUMMARY__.SqlServer.sql
+++ config/SqlCore/Queries/UI/Dashboard/__GET_BILLCLOSESYNOPSIS_SUMMARY__.SqlServer.sql
@@ -4,13 +4,14 @@ DECLARE @BillGroupCount int
 
 set @id_interval = %%ID_USAGE_INTERVAL%% --set interval here
 set @BillGroupCount = (select COUNT(*) from t_billgroup_materialization where id_usage_interval = @id_interval)
-select CASE DAY(ui.dt_end)
-         WHEN '5' THEN 'M5'
-         WHEN '12' THEN 'M12'
-         WHEN '19' THEN 'M19'
-         WHEN '26' THEN 'M26'
-         ELSE 'EOM'
-      END as [Type],
+select
+ -- CASE DAY(ui.dt_end)
+   --      WHEN '5' THEN 'M5'
+     --    WHEN '12' THEN 'M12'
+     --    WHEN '19' THEN 'M19'
+     --    WHEN '26' THEN 'M26'
+     --    ELSE 'EOM'
+     -- END as [Type],
       @BillGroupCount as [BillGroups],
       ui.id_interval as [IntervalID],
       ui.dt_start as [Start],

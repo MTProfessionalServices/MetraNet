8d461cf66ccfd8e6662d2c472e2e57ebbe66c596
damin <damin@metratech.com>
2014-11-14 15:43:13 -0500

    FEAT-3850 Bling: Productize Finance Dashboard FEAT-4284 New Customers Chart widget

--- config/SqlCore/Queries/UI/Dashboard/__GET_REPORT_NEWCUSTOMERS_CHART__.Oracle.sql
+++ config/SqlCore/Queries/UI/Dashboard/__GET_REPORT_NEWCUSTOMERS_CHART__.Oracle.sql
@@ -1,5 +1,8 @@
-SELECT c.MetraNetId as Account,
-       trunc(c.StartDate,'MON') as "Date"
-  FROM Customer c
- WHERE c.StartDate >= add_months(GETUTCDATE(), -13)
+SELECT 
+      trunc(c.StartDate,'MON') as "Date",
+			COUNT(c.MetraNetId) AS CustomerCount			 
+FROM Customer c
+WHERE c.StartDate >= add_months(GETUTCDATE(), -13)
    AND c.StartDate <= add_months(GETUTCDATE(), -1)
+GROUP BY trunc(c.StartDate,'MON')
+
--- config/SqlCore/Queries/UI/Dashboard/__GET_REPORT_NEWCUSTOMERS_CHART__.SqlServer.sql
+++ config/SqlCore/Queries/UI/Dashboard/__GET_REPORT_NEWCUSTOMERS_CHART__.SqlServer.sql
@@ -1,8 +1,10 @@
 DECLARE @dateFrom DATE, @dateTo DATE
 SET @dateFrom = DATEADD(month, -13, GETUTCDATE());
 SET @dateTo = DATEADD(month, -1, GETUTCDATE());
-SELECT c.MetraNetId as Account,
-       CONVERT(DATE, CONCAT(DATEPART(month, c.StartDate),'-','01','-',DATEPART(year, c.StartDate)), 110) as [Date]
+SELECT 
+       CONVERT(DATE, CONCAT(DATEPART(month, c.StartDate),'-','01','-',DATEPART(year, c.StartDate)), 110) AS [Date],
+			 COUNT(c.MetraNetId) AS CustomerCount
   FROM Customer c
  WHERE c.StartDate >= @dateFrom
    AND c.StartDate <= @dateTo
+GROUP BY CONVERT(DATE, CONCAT(DATEPART(month, c.StartDate),'-','01','-',DATEPART(year, c.StartDate)), 110)

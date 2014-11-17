44564d9998d5d67be3f57aa464a1f92dabb9ea88
damin <damin@metratech.com>
2014-11-17 14:19:53 -0500

    FEAT-3850 Bling: Productize Finance Dashboard FEAT-5046 MRR Graph widget

--- config/SqlCore/Queries/UI/Dashboard/__GET_REPORT_MRR_CHART__.Oracle.sql
+++ config/SqlCore/Queries/UI/Dashboard/__GET_REPORT_MRR_CHART__.Oracle.sql
@@ -4,6 +4,8 @@ SELECT
        SUM(sm.MrrPrimaryCurrency) as Amount,
 			 NVL(lc.tx_desc, sm.ReportingCurrency) as LocalizedCurrency
 FROM SubscriptionsByMonth sm
+LEFT OUTER JOIN t_enum_data c on LOWER(c.nm_enum_data) = CONCAT('global/systemcurrencies/systemcurrencies/', LOWER(sm.ReportingCurrency))
+LEFT OUTER JOIN t_description lc on lc.id_desc = c.id_enum_data and lc.id_lang_code = %%ID_LANG_CODE%%
 WHERE TO_DATE(LPAD(TO_CHAR(sm.Month), 2, '0') || '-01-' || TO_CHAR(sm.Year), 'MM-DD-YYYY') >= %%FROM_DATE%%
   AND TO_DATE(LPAD(TO_CHAR(sm.Month), 2, '0') || '-01-' || TO_CHAR(sm.Year), 'MM-DD-YYYY') <= %%TO_DATE%%
-GROUP BY TO_DATE(LPAD(TO_CHAR(sm.Month), 2, '0') || '-01-' || TO_CHAR(sm.Year), 'MM-DD-YYYY'), sm.ReportingCurrency
+GROUP BY TO_DATE(LPAD(TO_CHAR(sm.Month), 2, '0') || '-01-' || TO_CHAR(sm.Year), 'MM-DD-YYYY'), sm.ReportingCurrency, lc.tx_desc

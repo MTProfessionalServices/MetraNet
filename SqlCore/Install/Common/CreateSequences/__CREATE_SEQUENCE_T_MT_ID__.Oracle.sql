6cfeadef8481a4bfba016158cb28a402ff16d222
josephbarnett <joseph.barnett@gmail.com>
2014-11-19 14:54:48 -0500

    Simplifying installation files to accommodate binding issues experienced after installing new oracle server. It appears the Schemamaster is not doing the correct binding if a method is used in the insert.

--- SqlCore/Install/Common/CreateSequences/__CREATE_SEQUENCE_T_MT_ID__.Oracle.sql
+++ SqlCore/Install/Common/CreateSequences/__CREATE_SEQUENCE_T_MT_ID__.Oracle.sql
@@ -1,3 +1,3 @@
 
-          CREATE SEQUENCE seq_t_mt_id increment by 1 start with 0 minvalue 0 order nocache nocycle
+          CREATE SEQUENCE seq_t_mt_id increment by 1 start with 1 minvalue 1 order nocache nocycle
                 
--- SqlCore/Install/ProductCatalog/Data/__INSERT_0_INTO_T_MT_ID_TABLE__.Oracle.sql
+++ SqlCore/Install/ProductCatalog/Data/__INSERT_0_INTO_T_MT_ID_TABLE__.Oracle.sql
@@ -3,4 +3,5 @@
 		<identity_insert/>
 	</initialization>
 */
-      	insert into t_MT_ID (id_mt) values (decode(seq_t_mt_id.nextval,0,0,0))
+insert into t_MT_ID (id_mt) VALUES(0);
+

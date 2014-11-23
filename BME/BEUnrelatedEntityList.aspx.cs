c4cbd4dcc798c93b6df3670c018be1d16ada225b
yzapara <yzapara@metratech.com>
2014-11-23 13:36:24 -0500

    CORE-8841 FEAT-4761 Add Machine Role: captions of the grid are not localized CORE-8839 FEAT-4761 Machine Roles: title and caption of grid are not localized CORE-8840 FEAT-4761 Machine Roles: blank page on adding relationship

--- BME/BEUnrelatedEntityList.aspx.cs
+++ BME/BEUnrelatedEntityList.aspx.cs
@@ -2,23 +2,19 @@
 //         &Association=Account&ParentId=123&ParentName=Core.OrderManagement.Parent 
 // Loads list of orders with the OrderTemplate.xml grid layout in the Account extension
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using MetraTech.ActivityServices.Common;
 using MetraTech.BusinessEntity.Core;
-using MetraTech.BusinessEntity.Service.ClientProxies;
-using MetraTech.UI.Common;
 using MetraTech.BusinessEntity.DataAccess.Metadata;
+using MetraTech.BusinessEntity.Service.ClientProxies;
+using MetraTech.UI.CDT;
+using MetraTech.UI.Common;
 using MetraTech.UI.Controls;
+using MetraTech.UI.Controls.CDT;
 
 public partial class BEUnrelatedEntityList : MTPage
 {
 
   #region Properties
- 
+
 
   public string BEName
   {
@@ -32,7 +28,7 @@ public partial class BEUnrelatedEntityList : MTPage
     set { ViewState["MultiSelect"] = value; }
   }
 
- public string RefererUrl
+  public string RefererUrl
   {
     get { return ViewState["RefererURL"] as string; }
     set { ViewState["RefererURL"] = value; }
@@ -85,7 +81,7 @@ public partial class BEUnrelatedEntityList : MTPage
     get { return ViewState["Unrelated"] as string; }
     set { ViewState["Unrelated"] = value; }
   }
-  
+
   #endregion
 
   #region Events
@@ -138,15 +134,26 @@ public partial class BEUnrelatedEntityList : MTPage
         ReturnUrl = Request["ReturnURL"].Replace("'", "").Replace("|", "?").Replace("**", "&");
       }
 
-	  ReadOnly = "false";
+      ReadOnly = "false";
       BEName = Server.HtmlEncode(Request.QueryString["Name"]);
 
       BMEGrid.ExtensionName = Request.QueryString["Extension"];
       BMEGrid.TemplateFileName = BEName + ".xml";
       //LblcurrentEntityName.Text = String.IsNullOrEmpty(Request["currentEntityName"]) ? null : Resources.Resource.TEXT_ADD_RELATED_ENTITIES + Request["currentEntityName"];
-      LblcurrentEntityName.Text = String.IsNullOrEmpty(Request["currentEntityName"]) ? null : Resources.Resource.TEXT_RELATED_ENTITIES_1 + BEName.Substring(BEName.LastIndexOf(".") + 1) + Resources.Resource.TEXT_RELATED_ENTITIES_2  + Request["currentEntityName"];
-      
+      // ReSharper disable StringLastIndexOfIsCultureSpecific.1
+      LblcurrentEntityName.Text = String.IsNullOrEmpty(Request["currentEntityName"]) ? null : Resources.Resource.TEXT_RELATED_ENTITIES_1 + GetBMELocalizedName(BEName) + Resources.Resource.TEXT_RELATED_ENTITIES_2 + Request["currentEntityName"];
+      // ReSharper restore StringLastIndexOfIsCultureSpecific.1
+
     }
+  }
+
+  private string GetBMELocalizedName(string bmename)
+  {
+    var BMEInstanceForm = new MTGenericForm();
+    PageLayout layout = BMEInstanceForm.GetPageLayoutByName(BEName);
+    if (layout == null)
+      return bmename;
+    return layout.Header.PageTitle.GetValue();
   }
 
   protected override void OnLoadComplete(EventArgs e)
@@ -154,15 +161,16 @@ public partial class BEUnrelatedEntityList : MTPage
 
     MultiSelect = String.IsNullOrEmpty(Request["MultiSelect"]) ? "false" : Request["MultiSelect"];
     Unrelated = String.IsNullOrEmpty(Request["Unrelated"]) ? "false" : Request["Unrelated"];
-    
-    if (String.IsNullOrEmpty(BMEGrid.Title))
+
+    var gridTitle = GetBMELocalizedName(BEName);
+    //if (String.IsNullOrEmpty())
     {
       string Text1 = Resources.Resource.TEXT_UNRELATED_1;
       string Text2 = Resources.Resource.TEXT_UNRELATED_2;
       //BMEGrid.Title = Server.HtmlEncode(Text1 + BEName.Substring(BEName.LastIndexOf(".") + 1) + " " + Text2);
 
-      BMEGrid.Title = Text1 + BEName.Substring(BEName.LastIndexOf(".") + 1) + Text2;
-    
+      BMEGrid.Title = Text1 + gridTitle + Text2;
+
       // Set additional argument for grid     
       if (!String.IsNullOrEmpty(AccountDefProp))
       {
@@ -185,7 +193,7 @@ public partial class BEUnrelatedEntityList : MTPage
       }
 
       BMEGrid.DataSourceURL += "&UnrelatedEntityList=true";
-      
+
       if (!MenuManager.CheckBECapability(BMEGrid.ExtensionName, AccessType.Write, UI.SessionContext.SecurityContext))
       {
         ReadOnly = "true";
@@ -199,7 +207,7 @@ public partial class BEUnrelatedEntityList : MTPage
       {
         ReadOnly = "false";
       }
-      
+
 
       if (Unrelated == "true")
       {
@@ -213,9 +221,9 @@ public partial class BEUnrelatedEntityList : MTPage
           BMEGrid.MultiSelect = false;
           BMEGrid.SelectionModel = MTGridSelectionModel.Checkbox;
         }
-        
+
         BMEGrid.ToolbarButtons.RemoveAll(b => b.ButtonID.ToLower() == "bulkupdate");
-        
+
         BMEGrid.Buttons = MTButtonType.OKCancel;
       }
       base.OnLoadComplete(e);
@@ -225,6 +233,6 @@ public partial class BEUnrelatedEntityList : MTPage
   }
   #endregion
 
- 
+
 }
 

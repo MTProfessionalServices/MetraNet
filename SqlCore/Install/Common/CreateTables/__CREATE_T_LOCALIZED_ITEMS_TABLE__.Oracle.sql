29e4648b4bbb40734b01d4228c694fe2a0b5d3df
msukhovarov <msukhovarov@metratech.com>
2014-12-01 17:10:39 -0500

    FEAT-5104 - Localization of Capability Names and Descriptions Tasks: FEAT-5287 [Dev: C#\SQL] Add id_item_second field to t_localized_items FEAT-5249 [Dev: C#] Start using LocalizedIems to Read\Save Composite and Atomic capabilities to DB with localization implemented

--- SqlCore/Install/Common/CreateTables/__CREATE_T_LOCALIZED_ITEMS_TABLE__.Oracle.sql
+++ SqlCore/Install/Common/CreateTables/__CREATE_T_LOCALIZED_ITEMS_TABLE__.Oracle.sql
@@ -1,10 +1,11 @@
 
 CREATE TABLE t_localized_items
 (
-	id_local_type int NOT NULL,     /* Composite key: This is foreign key to t_localized_items_type.*/
-	id_item int NOT NULL,			/* Composite key: Localize identifier. This is foreign key to t_recevent and other tables (see constraints) */
-	id_lang_code int NOT NULL,      /* Composite key: Language identifier displayed on the MetraNet Presentation Server */
-	tx_name NVARCHAR2(255) NULL,   	/* The localized DisplayName */
-	tx_desc NVARCHAR2(2000) NULL,  	/* The localized Description */
-CONSTRAINT PK_t_localized_items PRIMARY KEY (id_local_type, id_item, id_lang_code)
+	id_local_type int NOT NULL,     			/* Composite key: This is foreign key to t_localized_items_type.*/
+	id_item int NOT NULL,						/* Composite key: Localize identifier. This is foreign key to t_recevent and other tables*/
+	id_item_second_key int default -1 NOT NULL,	/* Composite key: Second localize identifier. This is foreign key, for example, to t_compositor (it is atomoc capability) and other tables with composite PK. In case second key is not used set -1 as default value */
+	id_lang_code int NOT NULL,      			/* Composite key: Language identifier displayed on the MetraNet Presentation Server */
+	tx_name NVARCHAR2(255) NULL,   				/* The localized DisplayName */
+	tx_desc NVARCHAR2(2000) NULL,  				/* The localized Description */
+CONSTRAINT PK_t_localized_items PRIMARY KEY (id_local_type, id_item, id_item_second_key, id_lang_code)
 )
--- SqlCore/Queries/Localization/__SELECT_LOCALIZED_ITEMS_BY_TYPE__.Common.sql
+++ SqlCore/Queries/Localization/__SELECT_LOCALIZED_ITEMS_BY_TYPE__.Common.sql
@@ -3,6 +3,7 @@ SELECT id_lang_code
       , tx_desc
   FROM t_localized_items
   WHERE	id_local_type = %%LOCALIZED_TYPE%%
-		AND id_item = %%ID_PARENT%%
+		AND id_item = %%ID_PARENT%% %%ADD_WHERE%%;
+		
 
       

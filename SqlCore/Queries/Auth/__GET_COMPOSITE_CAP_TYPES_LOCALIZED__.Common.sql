3168863bc998f2eab471b4a00f670340dfcf510b
yzapara <yzapara@metratech.com>
2014-12-01 11:05:01 -0500

    FEAT-5104 [8] CLONE - Localization of Capability Names and Descriptions FEAT-5253 [DEV c++\SQL] Retrieve capabilities according to language into IMTSessionContext

---
+++ SqlCore/Queries/Auth/__GET_COMPOSITE_CAP_TYPES_LOCALIZED__.Common.sql
@@ -0,0 +1,17 @@
+
+				SELECT 
+				/* Query Tag: __GET_COMPOSITE_CAP_TYPES__ */
+				type.id_cap_type, 
+        type.tx_guid,
+        COALESCE(li.tx_name, type.tx_name) tx_name,        
+        COALESCE(li.tx_desc, type.tx_desc) tx_desc,                        
+        type.tx_progid, 
+        type.tx_editor, 
+        type.csr_assignable, 
+        type.subscriber_assignable, 
+        type.multiple_instances
+				,(SELECT COUNT(*) FROM t_compositor WHERE id_composite = type.id_cap_type) num_atomic
+				, type.umbrella_sensitive
+				FROM t_composite_capability_type type 
+        LEFT OUTER JOIN t_localized_items li ON type.id_cap_type = li.id_item AND li.id_lang_code = %%LANG_ID%% AND li.id_local_type = 2 --2 - composite capability type
+    	

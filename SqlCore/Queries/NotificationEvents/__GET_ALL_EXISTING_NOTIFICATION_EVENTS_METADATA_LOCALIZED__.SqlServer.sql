f3d90b6d43688bd6454727d3f50b6bb3e15f5a27
akaboli <akaboli@metratech.com>
2014-12-01 17:42:01 -0500

    FEAT-5162 Improve usability of configuration of notification events FEAT-5291
    Create separate template localization files for each decision type

--- SqlCore/Queries/NotificationEvents/__GET_ALL_EXISTING_NOTIFICATION_EVENTS_METADATA_LOCALIZED__.SqlServer.sql
+++ SqlCore/Queries/NotificationEvents/__GET_ALL_EXISTING_NOTIFICATION_EVENTS_METADATA_LOCALIZED__.SqlServer.sql
@@ -1,4 +1,4 @@
 select  id_notification_event_type, tx_desc As notification_event_name, notification_event_name as default_name from 
-t_enum_data  join t_notification_event_types on Lower(nm_enum_data) = Lower('MetraTech.MetraNet.NotificationEvents/NotificationEventType/'+notification_event_name) 
+t_enum_data  join t_notification_event_types on Lower(nm_enum_data) = Lower('Notification/EventType/'+notification_event_name) 
 left outer join t_description on id_enum_data = id_desc
 where id_lang_code = @ID_LANG_CODE
--- SqlCore/Queries/NotificationEvents/__GET_ALL_EXISTING_NOTIFICATION_EVENTS_TEMPLATES_LOCALIZED__.Oracle.sql
+++ SqlCore/Queries/NotificationEvents/__GET_ALL_EXISTING_NOTIFICATION_EVENTS_TEMPLATES_LOCALIZED__.Oracle.sql
@@ -1,4 +1,4 @@
 select  notification_event_name, d1.tx_desc As notification_event_template, d2.tx_desc  as default_template from
-t_enum_data  join t_notification_event_types on Lower(nm_enum_data) = Lower('MetraTech.MetraNet.NotificationEvents/NotificationEventTemplate/'||notification_event_name)  
+t_enum_data  join t_notification_event_types on Lower(nm_enum_data) = Lower('Notification/Template/'||notification_event_name)  
 left outer join t_description d1 on id_enum_data = d1.id_desc left outer join t_description d2 on id_enum_data = d2.id_desc 
 where id_lang_code = :ID_LANG_CODE and d2.id_lang_code = 840

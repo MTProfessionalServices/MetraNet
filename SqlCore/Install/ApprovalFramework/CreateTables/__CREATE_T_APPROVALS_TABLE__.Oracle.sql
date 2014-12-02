58fa75f00dea8cba05e6f85d354d00ad9663fd8e
Nikita Kuklich <nkuklich@metratech.com>
2014-12-02 09:54:34 -0500

    FEAT-4221 FEAT-5112:"[DEV] Add Partition Id column to domain" Correct domain object and table. Code Reviewer: MSukhovarov

--- SqlCore/Install/ApprovalFramework/CreateTables/__CREATE_T_APPROVALS_TABLE__.Oracle.sql
+++ SqlCore/Install/ApprovalFramework/CreateTables/__CREATE_T_APPROVALS_TABLE__.Oracle.sql
@@ -1,16 +1,17 @@
 
-		    CREATE TABLE t_approvals(
-                       id_approval Int NOT NULL,
-                       c_SubmittedDate date DEFAULT sysdate NULL,
-                       c_SubmitterId number(10) NOT NULL,
-                       c_ChangeType varchar2(100) NOT NULL,
-                       c_ChangeDetails BLOB NOT NULL,
-                       c_ApproverId number(10) NULL,
-                       c_ChangeLastModifiedDate date NULL,
-                       c_ItemDisplayName varchar2(100) NULL,
-                       c_UniqueItemId varchar2(100) NULL,
-                       c_Comment varchar2(255) NULL,
-                       c_CurrentState varchar2(50) NOT NULL,
-
-                       CONSTRAINT PK_t_approvals PRIMARY KEY (id_approval))
-			 
+CREATE TABLE t_approvals
+(
+	id_approval               INT NOT NULL,
+	c_SubmittedDate           date DEFAULT sysdate NULL,
+	c_SubmitterId             number(10) NOT NULL,
+	c_ChangeType              varchar2(100) NOT NULL,
+	c_ChangeDetails           BLOB NOT NULL,
+	c_ApproverId              number(10) NULL,
+	c_ChangeLastModifiedDate  date NULL,
+	c_ItemDisplayName         varchar2(100) NULL,
+	c_UniqueItemId            varchar2(100) NULL,
+	c_Comment                 varchar2(255) NULL,
+	c_CurrentState            varchar2(50) NOT NULL,
+	c_PartitionId             number(10) DEFAULT 1 NOT NULL,
+	CONSTRAINT PK_t_approvals PRIMARY KEY(id_approval)
+)

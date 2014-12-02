872633228b031b196b7633defb93073cf1ec23e6
msukhovarov <msukhovarov@metratech.com>
2014-12-02 05:44:20 -0500

    FEAT-5104 - Localization of Capability Names and Descriptions Tasks: FEAT-5287 [Dev: C#\SQL] Add id_item_second field to t_localized_items FEAT-5249 [Dev: C#] Start using LocalizedIems to Read\Save Composite and Atomic capabilities to DB with localization corrected work with SQL parameters

--- SqlCore/Queries/Localization/__SELECT_LOCALIZED_ITEMS_BY_TYPE__.Common.sql
+++ SqlCore/Queries/Localization/__SELECT_LOCALIZED_ITEMS_BY_TYPE__.Common.sql
@@ -3,7 +3,4 @@ SELECT id_lang_code
       , tx_desc
   FROM t_localized_items
   WHERE	id_local_type = %%LOCALIZED_TYPE%%
-		AND id_item = %%ID_PARENT%% %%ADD_WHERE%%;
-		
-
-      
+		AND id_item = %%ID_PARENT%% %%ADD_WHERE%%

f43e27e15b692d8750fcc01f6c121f7d163f700a
Vadim Vasilenko <vvasilenko@metratech.com>
2014-12-03 11:44:37 -0500

    FEAT-5029: Adapter run details pop-up window has been added to the AdapterInstanceInformation web page.

--- SqlCore/Queries/mom/__GET_ADAPTER_INSTANCE_RUN_DETAILS__.Common.sql
+++ SqlCore/Queries/mom/__GET_ADAPTER_INSTANCE_RUN_DETAILS__.Common.sql
@@ -1,4 +1,4 @@
      
-        select tx_type, tx_detail, dt_crt from t_recevent_run_details 
+        select id_detail, tx_type, tx_detail, dt_crt from t_recevent_run_details 
 				where id_run = %%ID_RUN%%   
  			

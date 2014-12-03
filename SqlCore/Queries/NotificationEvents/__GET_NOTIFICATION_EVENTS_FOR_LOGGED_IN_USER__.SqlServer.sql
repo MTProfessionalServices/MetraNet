c691d682d0ec0365dd9a9245f80c07f577c52c03
akaboli <akaboli@metratech.com>
2014-12-03 17:13:12 -0500

    FEAT-5162 Improve usability of configuration of notification events FEAT-5215
    Move notification types localization to the new folder created in the previous task

--- SqlCore/Queries/NotificationEvents/__GET_NOTIFICATION_EVENTS_FOR_LOGGED_IN_USER__.SqlServer.sql
+++ SqlCore/Queries/NotificationEvents/__GET_NOTIFICATION_EVENTS_FOR_LOGGED_IN_USER__.SqlServer.sql
@@ -9,7 +9,7 @@ SELECT
 FROM t_notification_event_consumers consumers
 INNER JOIN t_Notification_Events events ON events.id_notification_event = consumers.id_notification_event
 INNER JOIN t_notification_event_types eventTypes ON eventTypes.id_notification_event_type = events.id_notification_event_type
-LEFT OUTER JOIN t_enum_data edata on LOWER(edata.nm_enum_data) = CONCAT('ui/notificationeventtype/', LOWER(eventTypes.notification_event_name))
+LEFT OUTER JOIN t_enum_data edata on LOWER(edata.nm_enum_data) = CONCAT('Notification/EventType/', LOWER(eventTypes.notification_event_name))
 LEFT OUTER JOIN t_description eventnamedesc on eventnamedesc.id_desc = edata.id_enum_data and eventnamedesc.id_lang_code = %%ID_LANG%%
 LEFT OUTER JOIN t_account_mapper tamap ON tamap.id_acc = events.id_partition and tamap.nm_space = 'mt' and tamap.id_acc != 1
 WHERE consumers.id_acc = %%ID_ACC%%

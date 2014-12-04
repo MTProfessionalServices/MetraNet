70eabcc5fbb370045a65035a727d8a65301742de
Suhas Kasar <skasar@metratech.com>
2014-12-04 17:05:06 -0500

    CORE-8536 Can't create account if ancestor account has alias

--- SqlCore/Queries/Auth/__GET_ACCOUNT_CREDENTIALS__.Common.sql
+++ SqlCore/Queries/Auth/__GET_ACCOUNT_CREDENTIALS__.Common.sql
@@ -1,4 +1,11 @@
-
-        SELECT nm_login, nm_space FROM t_account_mapper WHERE
-        id_acc = %%ID%%
+		SELECT 
+			am.nm_login, am.nm_space 
+		FROM 
+			t_account_mapper am 
+			INNER JOIN t_namespace ns ON ns.nm_space = am.nm_space
+		WHERE 
+			am.id_acc = %%ID%%
+		/* this order by is to ensure that alias account (if any) always comes at the end */ 
+		ORDER BY 
+			ns.tx_typ_space DESC
 				

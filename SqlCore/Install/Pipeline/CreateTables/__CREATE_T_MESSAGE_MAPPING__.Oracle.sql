533a237ba95ffadeb62e774093b5cf04012db491
Alexander Skrinnik <askrinnik@metratech.com>
2014-12-11 03:26:03 -0500

    Fix table structure according to Stephen Insero suggestion

--- SqlCore/Install/Pipeline/CreateTables/__CREATE_T_MESSAGE_MAPPING__.Oracle.sql
+++ SqlCore/Install/Pipeline/CreateTables/__CREATE_T_MESSAGE_MAPPING__.Oracle.sql
@@ -1,6 +1,7 @@
 
- CREATE TABLE t_message_mapping( 
-	 	    id_message int NOT NULL, 
-	 	    id_origin_message int NOT NULL,
-         CONSTRAINT pk_t_message_mapping PRIMARY KEY  (id_origin_message));
+CREATE TABLE t_message_mapping( 
+         id_message        NUMBER(10) NOT NULL, 
+         id_origin_message NUMBER(10) NOT NULL,
+         CONSTRAINT pk_t_message_mapping PRIMARY KEY (id_origin_message))
+
 			

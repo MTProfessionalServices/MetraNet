a866ae165a811114054ee2d17a6f491fbab26622
damin <damin@metratech.com>
2014-12-12 13:22:38 -0500

    CORE-8877 [FEAT-3850] Currency dropdown list in "Billings" widget not localized for Japanese

--- config/SqlCore/Queries/UI/Dashboard/__GET_REPORT_REVENUE_CHART__.SqlServer.sql
+++ config/SqlCore/Queries/UI/Dashboard/__GET_REPORT_REVENUE_CHART__.SqlServer.sql
@@ -2,10 +2,7 @@ SELECT
   CONVERT(DATE, CONCAT(DATEPART(month, i.Invoice_date),'-','01','-',DATEPART(year, i.Invoice_date)), 110) as [Date]
   ,i.Invoice_currency AS [Currency]
   ,SUM(i.Invoice_amount) AS [Amount]
-  ,ISNULL(lc.tx_desc, i.Invoice_currency)  AS [LocalizedCurrency]
 FROM T_invoice i
-LEFT OUTER JOIN t_enum_data c on LOWER(c.nm_enum_data) = CONCAT('global/systemcurrencies/systemcurrencies/', LOWER(i.Invoice_currency))
-LEFT OUTER JOIN t_description lc on lc.id_desc = c.id_enum_data and lc.id_lang_code = %%ID_LANG_CODE%%
 WHERE i.Invoice_date >= %%FROM_DATE%% and i.Invoice_date < %%TO_DATE%%
-GROUP BY CONVERT(DATE, CONCAT(DATEPART(month, i.Invoice_date),'-','01','-',DATEPART(year, i.Invoice_date)), 110), i.Invoice_currency, lc.tx_desc
+GROUP BY CONVERT(DATE, CONCAT(DATEPART(month, i.Invoice_date),'-','01','-',DATEPART(year, i.Invoice_date)), 110), i.Invoice_currency
 ORDER BY [Date], [Currency]

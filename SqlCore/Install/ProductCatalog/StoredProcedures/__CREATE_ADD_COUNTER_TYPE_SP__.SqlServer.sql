58e15af6c2e4dcb09557f1e1480f86c9260c6f3c
msukhovarov <msukhovarov@metratech.com>
2014-12-15 06:37:58 -0500

    FEAT-5320 Localize Adjustment Type Displayname of Priceable Items Task: FEAT-5366 [Dev: C#] Upgrade BasePropsWriter class to create\update\delete localization implemented

--- SqlCore/Install/ProductCatalog/StoredProcedures/__CREATE_ADD_COUNTER_TYPE_SP__.SqlServer.sql
+++ SqlCore/Install/ProductCatalog/StoredProcedures/__CREATE_ADD_COUNTER_TYPE_SP__.SqlServer.sql
@@ -13,8 +13,10 @@
 			declare @temp_nm_name nvarchar(255)
 			declare @temp_id_lang_code int
 			declare @identity_value int
-			declare @t_base_props_count INT
-			
+			declare @t_base_props_count int
+			declare @id_display_name int
+			declare @id_display_desc int
+				  
 			select @id_prop = -1
       select @temp_nm_name = @nm_name
 			select @temp_id_lang_code = @id_lang_code
@@ -30,7 +32,7 @@
 
 			IF (@t_count = 0)
 			  begin
-				exec InsertBaseProps @id_lang_code, @n_kind, 'N', 'N', @nm_name, @nm_desc, null, @identity_value OUTPUT
+				exec InsertBaseProps @id_lang_code, @n_kind, 'N', 'N', @nm_name, @nm_desc, null, @identity_value OUTPUT, @id_display_name OUTPUT, @id_display_desc OUTPUT
 		    INSERT INTO t_counter_metadata (id_prop, FormulaTemplate, b_valid_for_dist) values (@identity_value, 
 				    @nm_formula_template, @valid_for_dist)
 				select @id_prop = @identity_value
--- SqlCore/Install/ProductCatalog/StoredProcedures/__CREATE_SP_UPDATEBASEPROPS__.SqlServer.sql
+++ SqlCore/Install/ProductCatalog/StoredProcedures/__CREATE_SP_UPDATEBASEPROPS__.SqlServer.sql
@@ -4,23 +4,23 @@
 			@a_id_lang int,
 			@a_nm_name NVARCHAR(255),
 			@a_nm_desc NVARCHAR(4000),
-			@a_nm_display_name NVARCHAR(255))
+			@a_nm_display_name NVARCHAR(255),
+			@updated_id_display_name int OUTPUT,
+			@updated_id_display_desc int OUTPUT )
 		AS
 		begin
       declare @old_id_name int
       declare @id_name int
       declare @old_id_desc int
-      declare @id_desc int
       declare @old_id_display_name int
-      declare @id_display_name int
 			select @old_id_name = n_name, @old_id_desc = n_desc, 
 			@old_id_display_name = n_display_name
      	from t_base_props where id_prop = @a_id_prop
 			exec UpsertDescription @a_id_lang, @a_nm_name, @old_id_name, @id_name output
-			exec UpsertDescription @a_id_lang, @a_nm_desc, @old_id_desc, @id_desc output
-			exec UpsertDescription @a_id_lang, @a_nm_display_name, @old_id_display_name, @id_display_name output
+			exec UpsertDescription @a_id_lang, @a_nm_desc, @old_id_desc, @updated_id_display_desc output
+			exec UpsertDescription @a_id_lang, @a_nm_display_name, @old_id_display_name, @updated_id_display_name output
 			UPDATE t_base_props
-				SET n_name = @id_name, n_desc = @id_desc, n_display_name = @id_display_name,
+				SET n_name = @id_name, n_desc = @updated_id_display_desc, n_display_name = @updated_id_display_name,
 						nm_name = @a_nm_name, nm_desc = @a_nm_desc, nm_display_name = @a_nm_display_name
 				WHERE id_prop = @a_id_prop
 		END

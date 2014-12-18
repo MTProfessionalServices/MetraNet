685c24d84374ab10ca49e6c1eaae3d62583c7bcd
Vadim Vasilenko <vvasilenko@metratech.com>
2014-12-18 09:43:04 -0500

    CORE-8990: 1) German localization has been fixed; 2) Localization for the Event Display Name column values has been added; 3) Localization for the Action column values has been added (Instance Audit History).

--- SqlCore/Queries/mom/__GET_SCHEDULED_ADAPTER_RUN_LIST__.Oracle.sql
+++ SqlCore/Queries/mom/__GET_SCHEDULED_ADAPTER_RUN_LIST__.Oracle.sql
@@ -1,7 +1,7 @@
 
 SELECT 
   inst.id_instance /*InstanceID*/,
-  evt.tx_display_name /*EventDisplayName*/,
+  COALESCE(loc.tx_name, evt.tx_display_name) tx_display_name /*EventDisplayName*/,
   inst.tx_status /*Status*/,
   case when inst.id_arg_interval is null then inst.dt_arg_start else run.dt_start end as dt_argstartdate, 
   case when inst.id_arg_interval is null then inst.dt_arg_end else run.dt_end end as dt_argenddate, 
@@ -9,6 +9,7 @@ SELECT
   run.tx_machine /*LastRunMachine*/
 FROM t_recevent_inst inst
 INNER JOIN t_recevent evt ON evt.id_event = inst.id_event
+LEFT OUTER JOIN t_localized_items loc on (id_local_type = 1  /*Adapter type*/ AND id_lang_code = %%ID_LANG_CODE%% AND evt.id_event=loc.id_item)
 LEFT OUTER JOIN
 (
   /* finds the last run's ID */

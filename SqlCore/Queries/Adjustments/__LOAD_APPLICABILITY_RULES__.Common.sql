b22c188e95c14eb5f3abb8a4de237ce8f4330d23
Nikita Kuklich <nkuklich@metratech.com>
2014-12-19 12:49:30 -0500

    FEAT-5320 FEAT-5376:"[Dev : C# AdjustmentHook] Correct ApplicabilityRule Reader and Writer" 1. Correct sql queries; 2. Load loc. entities in Reader; 3. Extend LocalizedEntity interface with needed method. Code Reviewer: MSukhovarov

--- SqlCore/Queries/Adjustments/__LOAD_APPLICABILITY_RULES__.Common.sql
+++ SqlCore/Queries/Adjustments/__LOAD_APPLICABILITY_RULES__.Common.sql
@@ -1,14 +1,20 @@
-
-     select 
-     /* __LOAD_APPLICABILITY_RULES__ */
-      art.id_prop	RuleID,	art.tx_guid RuleGUID,
-      base1.nm_name	RuleName,	base1.nm_desc	RuleDescription, desc1.tx_desc RuleDisplayName,	
-      arf.tx_formula RuleFormula, arf.id_engine RuleFormulaEngine,
-      art.id_formula RuleFormulaID
-      FROM t_applicability_rule art
-      INNER JOIN t_calc_formula arf ON art.id_formula = arf.id_formula
-      LEFT OUTER JOIN	t_base_props base1 ON	art.id_prop	=	base1.id_prop
-      LEFT OUTER JOIN	t_description	desc1	ON base1.n_display_name	=	desc1.id_desc
-      WHERE desc1.id_lang_code = %%ID_LANG_CODE%% 
-      %%PREDICATE%%
-			
+SELECT /* __LOAD_APPLICABILITY_RULES__ */
+       art.id_prop RuleID,
+       art.tx_guid RuleGUID,
+       base1.nm_name RuleName,
+       base1.nm_desc RuleDescription,
+       desc1.tx_desc RuleDisplayName,
+       arf.tx_formula RuleFormula,
+       arf.id_engine RuleFormulaEngine,
+       art.id_formula RuleFormulaID,
+       base1.n_display_name RuleDisplayNameID,
+       base1.n_desc RuleDisplayDescriptionID
+FROM   t_applicability_rule art
+       INNER JOIN t_calc_formula arf
+            ON  art.id_formula = arf.id_formula
+       LEFT OUTER JOIN t_base_props base1
+            ON  art.id_prop = base1.id_prop
+       LEFT OUTER JOIN t_description desc1
+            ON  base1.n_display_name = desc1.id_desc
+WHERE  desc1.id_lang_code = %%ID_LANG_CODE%% 
+       %%PREDICATE%%
--- SqlCore/Queries/Adjustments/__LOAD_APPLIC_RULES_FOR_AJ_TYPE__.Common.sql
+++ SqlCore/Queries/Adjustments/__LOAD_APPLIC_RULES_FOR_AJ_TYPE__.Common.sql
@@ -1,15 +1,22 @@
-
-      select 
-      /* __LOAD_APPLIC_RULES_FOR_AJ_TYPE__ */
-      art.id_prop	RuleID,	art.tx_guid RuleGUID,
-      base1.nm_name	RuleName,	base1.nm_desc	RuleDescription, desc1.tx_desc RuleDisplayName,	
-      arf.tx_formula RuleFormula, arf.id_engine RuleFormulaEngine,
-      art.id_formula RuleFormulaID
-      FROM T_AJ_TYPE_APPLIC_MAP map
-      INNER JOIN t_applicability_rule art ON map.id_applicability_rule = art.id_prop
-      INNER JOIN t_calc_formula arf ON art.id_formula = arf.id_formula
-      LEFT OUTER JOIN	t_base_props base1 ON	art.id_prop	=	base1.id_prop
-      LEFT OUTER JOIN	t_description	desc1	ON base1.n_display_name	=	desc1.id_desc
-      WHERE desc1.id_lang_code = %%ID_LANG_CODE%%
-      AND map.id_adjustment_type = %%ID_AJ_TYPE%% 
-			
+SELECT /* __LOAD_APPLIC_RULES_FOR_AJ_TYPE__ */
+       art.id_prop RuleID,
+       art.tx_guid RuleGUID,
+       base1.nm_name RuleName,
+       base1.nm_desc RuleDescription,
+       desc1.tx_desc RuleDisplayName,
+       arf.tx_formula RuleFormula,
+       arf.id_engine RuleFormulaEngine,
+       art.id_formula RuleFormulaID,
+       base1.n_display_name RuleDisplayNameID,
+       base1.n_desc RuleDisplayDescriptionID
+FROM   T_AJ_TYPE_APPLIC_MAP map
+       INNER JOIN t_applicability_rule art
+            ON  map.id_applicability_rule = art.id_prop
+       INNER JOIN t_calc_formula arf
+            ON  art.id_formula = arf.id_formula
+       LEFT OUTER JOIN t_base_props base1
+            ON  art.id_prop = base1.id_prop
+       LEFT OUTER JOIN t_description desc1
+            ON  base1.n_display_name = desc1.id_desc
+WHERE  desc1.id_lang_code = %%ID_LANG_CODE%%
+       AND map.id_adjustment_type = %%ID_AJ_TYPE%%

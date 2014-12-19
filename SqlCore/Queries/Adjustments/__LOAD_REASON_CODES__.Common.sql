751c856f5a422970a6e3f4887c9d7e7ff86d3cd6
Nikita Kuklich <nkuklich@metratech.com>
2014-12-19 12:36:26 -0500

    FEAT-5320 FEAT-5375:"[Dev : C# AdjustmentHook] Correct ReasonCode Reader and Writer" 1. Correct sql queries; 2. Load loc. entities in Reader; 3. Extend LocalizedEntity interface with needed method; 4. Remove obsolete code from Writer. Code Reviewer: MSukhovarov

--- SqlCore/Queries/Adjustments/__LOAD_REASON_CODES__.Common.sql
+++ SqlCore/Queries/Adjustments/__LOAD_REASON_CODES__.Common.sql
@@ -1,11 +1,15 @@
-
-     select 
-     /* __LOAD_REASON_CODES__ */
-      rc.id_prop ReasonCodeID,	rc.tx_guid ReasonCodeGUID,
-      base1.nm_name	ReasonCodeName,	base1.nm_desc	ReasonCodeDescription, base1.n_display_name ReasonCodeDisplayNameID, desc1.tx_desc ReasonCodeDisplayName
-      FROM t_reason_code rc
-      INNER JOIN	t_base_props base1 ON	rc.id_prop	=	base1.id_prop
-      INNER JOIN	t_description	desc1	ON base1.n_display_name	=	desc1.id_desc
-      WHERE desc1.id_lang_code = %%ID_LANG_CODE%% 
-      %%PREDICATE%%
-			
+SELECT /* __LOAD_REASON_CODES__ */
+       rc.id_prop ReasonCodeID,
+       rc.tx_guid ReasonCodeGUID,
+       base1.nm_name ReasonCodeName,
+       base1.nm_desc ReasonCodeDescription,
+       base1.n_display_name ReasonCodeDisplayNameID,
+       desc1.tx_desc ReasonCodeDisplayName,
+       base1.n_desc ReasonCodeDisplayDescriptionID
+FROM   t_reason_code rc
+       INNER JOIN t_base_props base1
+            ON  rc.id_prop = base1.id_prop
+       INNER JOIN t_description desc1
+            ON  base1.n_display_name = desc1.id_desc
+WHERE  desc1.id_lang_code = %%ID_LANG_CODE%% 
+       %%PREDICATE%%

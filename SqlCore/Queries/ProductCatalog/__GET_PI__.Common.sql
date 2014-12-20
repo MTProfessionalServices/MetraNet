881e62e34d27cecc642a87a9fe0077a658f04d17
msukhovarov <msukhovarov@metratech.com>
2014-12-20 17:49:21 -0500

    FEAT-5320 Localize Adjustment Type Displayname of Priceable Items Task: FEAT-5426 [Dev: c++] Add using MetraTech_Adjustments::INamedBaseProperty to c++ implemented write PYType through c++ writer

--- SqlCore/Queries/ProductCatalog/__GET_PI__.Common.sql
+++ SqlCore/Queries/ProductCatalog/__GET_PI__.Common.sql
@@ -1,8 +1,17 @@
 
         select 
-          id_pi, id_parent, nm_servicedef, nm_productview, b_constrain_cycle,
-          t_vw_base_props.n_name, t_vw_base_props.n_desc, t_vw_base_props.n_display_name,
-          t_vw_base_props.nm_name, t_vw_base_props.nm_desc, t_vw_base_props.nm_display_name, t_vw_base_props.n_kind
+          id_pi, 
+		  id_parent, 
+		  nm_servicedef, 
+		  nm_productview, 
+		  b_constrain_cycle,
+          t_vw_base_props.n_name, 
+		  t_vw_base_props.n_desc, 
+		  t_vw_base_props.n_display_name,
+          t_vw_base_props.nm_name, 
+		  t_vw_base_props.nm_desc, 
+		  t_vw_base_props.nm_display_name, 
+		  t_vw_base_props.n_kind
         from t_pi 
         join t_vw_base_props on t_vw_base_props.id_prop = t_pi.id_pi and t_vw_base_props.id_lang_code = %%ID_LANG%%
         where t_pi.id_pi = %%ID_PI%%

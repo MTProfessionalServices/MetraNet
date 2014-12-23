687d60862ba5cba3d2a829474a2e3826cf34f495
vkurylovich <vkurylovich@metratech.com>
2014-12-23 16:44:38 -0500

    CORE-9085: [FEAT-5162] [Notifications] Consumers Query should take into account capabilities from roles code reviewed by dgaz

--- config/SqlCore/Queries/NotificationEvents/NotificationGroups/__NOTIFICATION_GROUP_ALL_METRACARE_USERS__.Oracle.sql
+++ config/SqlCore/Queries/NotificationEvents/NotificationGroups/__NOTIFICATION_GROUP_ALL_METRACARE_USERS__.Oracle.sql
@@ -6,13 +6,15 @@ SELECT DISTINCT(capMapper.id_acc), capMapper.id_Partition
 	,acc.id_acc AS id_Partition
 FROM     
   T_PRINCIPAL_POLICY prp     
-  INNER JOIN t_capability_instance ci ON prp.ID_POLICY = CI.ID_POLICY    
+  INNER JOIN t_capability_instance ci ON prp.ID_POLICY = CI.ID_POLICY
+  LEFT OUTER JOIN t_policy_role pr ON prp.id_policy = pr.id_policy
+  LEFT OUTER JOIN t_role r ON r.id_role = pr.id_role  
   LEFT OUTER JOIN t_enum_capability etc ON ci.id_cap_instance = etc.id_cap_instance
   LEFT OUTER JOIN t_account_ancestor accancestor ON accancestor.id_descendent = prp.id_acc AND num_generations = 2
   LEFT OUTER JOIN t_account acc ON acc.id_acc = accancestor.id_ancestor AND acc.id_type = 9
 WHERE 1=1 
   AND prp.policy_type = 'A'
-  AND etc.param_value = (select id_enum_data from t_enum_data nm where nm.nm_enum_data = 'Global/Application/MAM')
+  AND (etc.param_value = (select id_enum_data from t_enum_data nm where nm.nm_enum_data = 'Global/Application/MAM') or r.tx_name = 'CSR (MetraCare)')
   AND prp.id_acc IS NOT NULL
 
 

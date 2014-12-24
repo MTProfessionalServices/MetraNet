0e1d048dd82912736e9126ea58658b4916203510
yzapara <yzapara@metratech.com>
2014-12-24 05:42:08 -0500

    FEAT-5320 [5] Localize Adjustment Type Displayname of Priceable Items FEAT-5441 [Dev: ASP] The ServiceChargePriceAbleItem.List.asp page should show localized PI
    Review by RPuzikov

--- SqlCore/Queries/ProductCatalog/__GET_PI_TMPL_CHILDREN__.Common.sql
+++ SqlCore/Queries/ProductCatalog/__GET_PI_TMPL_CHILDREN__.Common.sql
@@ -1,9 +1,16 @@
 
-        select id_template, t_vw_base_props.n_kind,
-        t_vw_base_props.n_name, t_vw_base_props.n_desc, t_vw_base_props.n_display_name,
-        t_vw_base_props.nm_name, t_vw_base_props.nm_desc, t_vw_base_props.nm_display_name,
-        t_vw_base_props.nm_name, t_vw_base_props.nm_desc, t_vw_base_props.nm_display_name, CAST(NULL AS INT) n_rating_type 
-        from t_pi_template
-        join t_vw_base_props on t_vw_base_props.id_prop = id_template and t_vw_base_props.id_lang_code = %%ID_LANG%%
+        select 
+        id_template,
+        COALESCE(tvp.n_kind, tbp.n_kind) n_kind,
+        COALESCE(tvp.n_name, tbp.n_name) n_name,
+        COALESCE(tvp.n_desc, tbp.n_desc) n_desc,
+        COALESCE(tvp.n_display_name, tbp.n_display_name) n_display_name,
+        COALESCE(tvp.nm_name, tbp.nm_name) nm_name,
+        COALESCE(tvp.nm_desc, tbp.nm_desc) nm_desc,
+        COALESCE(tvp.nm_display_name, tbp.nm_display_name) nm_display_name,
+        CAST(NULL AS INT) n_rating_type 
+        from t_pi_template tpt
+        INNER JOIN t_base_props tbp ON tbp.id_prop = tpt.id_template
+        LEFT JOIN t_vw_base_props tvp ON tvp.nm_name = tbp.nm_name AND tvp.id_lang_code = %%ID_LANG%%
         where id_template_parent = %%ID_PARENT%%
       

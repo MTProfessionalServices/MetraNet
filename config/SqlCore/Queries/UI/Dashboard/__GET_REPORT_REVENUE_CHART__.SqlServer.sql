65b6c7ec97ffb9f43ac6a8c4d321164b1200e299
damin <damin@metratech.com>
2015-01-03 10:38:16 -0500

    FEAT-3850 Bling: Productize Finance Dashboard FEAT-4283: Billing(Revenue) Graph Widget
    - Added null check for localized currency column

--- config/SqlCore/Queries/UI/Dashboard/__GET_REPORT_REVENUE_CHART__.SqlServer.sql
+++ config/SqlCore/Queries/UI/Dashboard/__GET_REPORT_REVENUE_CHART__.SqlServer.sql
@@ -3,9 +3,9 @@ SET @dateFrom = DATEADD(month, -13, GETUTCDATE());
 SET @dateTo = GETUTCDATE();--DATEADD(day, -1, CONVERT(DATE, CONCAT(DATEPART(month, GETUTCDATE()),'-','01','-',DATEPART(year, GETUTCDATE())), 110));
 SELECT 
   CONVERT(DATE, CONCAT(DATEPART(month, i.Invoice_date),'-','01','-',DATEPART(year, i.Invoice_date)), 110) as [Date]
-  ,i.Invoice_currency as [Currency]
-  ,SUM(i.Invoice_amount) as [Amount]
-	,lc.tx_desc as [LocalizedCurrency]
+  ,i.Invoice_currency AS [Currency]
+  ,SUM(i.Invoice_amount) AS [Amount]
+  ,ISNULL(lc.tx_desc, i.Invoice_currency)  AS [LocalizedCurrency]
 FROM T_invoice i
 LEFT OUTER JOIN t_enum_data c on LOWER(c.nm_enum_data) = CONCAT('global/systemcurrencies/systemcurrencies/', LOWER(i.Invoice_currency))
 LEFT OUTER JOIN t_description lc on lc.id_desc = c.id_enum_data and lc.id_lang_code = %%ID_LANG_CODE%%

cdb56e2d3a6807134e7706985dfec5971466558c
damin <damin@metratech.com>
2015-01-03 10:37:22 -0500

    FEAT-3850 Bling: Productize Finance Dashboard FEAT-5046 MRR Graph widget

--- config/SqlCore/Queries/UI/Dashboard/__GET_REPORT_MRR_CHART__.Oracle.sql
+++ config/SqlCore/Queries/UI/Dashboard/__GET_REPORT_MRR_CHART__.Oracle.sql
@@ -1,8 +1,9 @@
 SELECT
        TO_DATE(LPAD(TO_CHAR(sm.Month), 2, '0') || '-01-' || TO_CHAR(sm.Year), 'MM-DD-YYYY') as "Date",
        sm.ReportingCurrency as CurrencyCode,
-       SUM(sm.MrrPrimaryCurrency) as Amount
+       SUM(sm.MrrPrimaryCurrency) as Amount,
+			 NVL(lc.tx_desc, sm.ReportingCurrency) as LocalizedCurrency
 FROM SubscriptionsByMonth sm
 WHERE TO_DATE(LPAD(TO_CHAR(sm.Month), 2, '0') || '-01-' || TO_CHAR(sm.Year), 'MM-DD-YYYY') >= add_months(GETUTCDATE(), -13)
-  AND TO_DATE(LPAD(TO_CHAR(sm.Month), 2, '0') || '-01-' || TO_CHAR(sm.Year), 'MM-DD-YYYY') < trunc(add_months(GETUTCDATE(), 12),'MON')
+  AND TO_DATE(LPAD(TO_CHAR(sm.Month), 2, '0') || '-01-' || TO_CHAR(sm.Year), 'MM-DD-YYYY') <= GETUTCDATE()
 GROUP BY TO_DATE(LPAD(TO_CHAR(sm.Month), 2, '0') || '-01-' || TO_CHAR(sm.Year), 'MM-DD-YYYY'), sm.ReportingCurrency
--- config/SqlCore/Queries/UI/Dashboard/__GET_REPORT_MRR_CHART__.SqlServer.sql
+++ config/SqlCore/Queries/UI/Dashboard/__GET_REPORT_MRR_CHART__.SqlServer.sql
@@ -1,11 +1,14 @@
 DECLARE @dateFrom DATE, @dateTo DATE
 SET @dateFrom = DATEADD(month, -13, GETUTCDATE());
-SET @dateTo = DATEADD(month, 12, CONVERT(DATE, CONCAT(DATEPART(month, GETUTCDATE()),'-','01','-',DATEPART(year, GETUTCDATE())), 110));
+SET @dateTo = GETUTCDATE();--DATEADD(month, 12, CONVERT(DATE, CONCAT(DATEPART(month, GETUTCDATE()),'-','01','-',DATEPART(year, GETUTCDATE())), 110));
 SELECT 
       CONVERT(DATE, CAST(sm.[Month] AS VARCHAR) + '-' + '01' + '-' + CAST(sm.[Year] AS VARCHAR), 110) as [Date],
       sm.ReportingCurrency as CurrencyCode,
-      SUM(sm.MrrPrimaryCurrency) as Amount
+      SUM(sm.MrrPrimaryCurrency) as Amount,
+      ISNULL(lc.tx_desc, sm.ReportingCurrency)  AS LocalizedCurrency
 FROM SubscriptionsByMonth sm
+LEFT OUTER JOIN t_enum_data c on LOWER(c.nm_enum_data) = CONCAT('global/systemcurrencies/systemcurrencies/', LOWER(sm.ReportingCurrency))
+LEFT OUTER JOIN t_description lc on lc.id_desc = c.id_enum_data and lc.id_lang_code = %%ID_LANG_CODE%%
 WHERE CONVERT(DATE, CAST(sm.[Month] AS VARCHAR) + '-' + '01' + '-' + CAST(sm.[Year] AS VARCHAR), 110) >= @dateFrom
-	AND CONVERT(DATE, CAST(sm.[Month] AS VARCHAR) + '-' + '01' + '-' + CAST(sm.[Year] AS VARCHAR), 110) < @dateTo
-GROUP BY  CONVERT(DATE, CAST(sm.[Month] AS VARCHAR) + '-' + '01' + '-' + CAST(sm.[Year] AS VARCHAR), 110), sm.ReportingCurrency
+	AND CONVERT(DATE, CAST(sm.[Month] AS VARCHAR) + '-' + '01' + '-' + CAST(sm.[Year] AS VARCHAR), 110) <= @dateTo
+GROUP BY  CONVERT(DATE, CAST(sm.[Month] AS VARCHAR) + '-' + '01' + '-' + CAST(sm.[Year] AS VARCHAR), 110), sm.ReportingCurrency, lc.tx_desc

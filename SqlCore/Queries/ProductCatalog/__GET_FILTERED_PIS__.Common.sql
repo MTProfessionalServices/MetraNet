a6668902e5dde2f101bbb404924e856154ac453f
yzapara <yzapara@metratech.com>
2015-01-06 08:59:25 -0500

    CORE-9115 [Product Offerrings Localization] Types of discount are not available in New Discount window under all localizations except En-US. Also fixed columns height in PI wizards
    Review by DStepanov

--- SqlCore/Queries/ProductCatalog/__GET_FILTERED_PIS__.Common.sql
+++ SqlCore/Queries/ProductCatalog/__GET_FILTERED_PIS__.Common.sql
@@ -1,12 +1,20 @@
 
         select
-        t_vw_base_props.id_prop,
-        t_vw_base_props.n_name, t_vw_base_props.n_desc, t_vw_base_props.n_display_name,
-        t_vw_base_props.nm_name, t_vw_base_props.nm_desc, t_vw_base_props.nm_display_name
+        COALESCE(t_vw_base_props.id_prop, tbp.id_prop) id_prop,
+        COALESCE(t_vw_base_props.n_name, tbp.n_name) n_name, 
+        COALESCE(t_vw_base_props.n_desc, tbp.n_desc) n_desc, 
+        COALESCE(t_vw_base_props.n_display_name, tbp.n_display_name) n_display_name,
+        COALESCE(t_vw_base_props.nm_name, tbp.nm_name) nm_name,
+        COALESCE(t_vw_base_props.nm_desc, tbp.nm_desc) nm_desc,
+        COALESCE(t_vw_base_props.nm_display_name, tbp.nm_display_name) nm_display_name
         %%COLUMNS%%
-        from t_vw_base_props
-        join t_pi on t_vw_base_props.id_prop = t_pi.id_pi and t_vw_base_props.id_lang_code = %%ID_LANG%%
+        from t_pi        
+        inner join t_base_props tbp on tbp.id_prop = t_pi.id_pi 
+        left outer join t_vw_base_props on t_vw_base_props.id_prop = t_pi.id_pi and t_vw_base_props.id_lang_code = %%ID_LANG%%
+        
         %%JOINS%%
         where 1=1
         %%FILTERS%% order by t_vw_base_props.nm_name asc
       
+      
+          

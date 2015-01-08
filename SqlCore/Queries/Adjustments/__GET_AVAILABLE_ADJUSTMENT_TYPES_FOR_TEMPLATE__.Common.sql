67ebb707386ff982d8a4036bbeff9951096e1a4c
msukhovarov <msukhovarov@metratech.com>
2015-01-08 08:17:21 -0500

    CORE-8962 - Localization MetraOffer: Add Adjustments (NRC and RC) window for non-US localization does not work properly and is not localized. corrected loads localized DisplayName and Descriptions from XML in PI Hook corrected SQL queries to load non-localized DisplayName and Descriptions in case localization was not set
    Reviewed by NKuklich

--- SqlCore/Queries/Adjustments/__GET_AVAILABLE_ADJUSTMENT_TYPES_FOR_TEMPLATE__.Common.sql
+++ SqlCore/Queries/Adjustments/__GET_AVAILABLE_ADJUSTMENT_TYPES_FOR_TEMPLATE__.Common.sql
@@ -1,32 +1,21 @@
-
 select 
-aty.id_prop, 
-aty.n_adjustmentType, 
-aty.b_supportBulk, 
-case when namedesc.tx_desc is null then bp.nm_name else namedesc.tx_desc end as nm_name, 
-case when descdesc.tx_desc is null then bp.nm_desc else descdesc.tx_desc end as nm_desc, 
-case when dispdesc.tx_desc is null then bp.nm_display_name else dispdesc.tx_desc end as nm_display_name
+		aty.id_prop, 
+		aty.n_adjustmentType, 
+		aty.b_supportBulk, 
+		bp.nm_name nm_name, 
+		COALESCE(vbp.nm_desc, bp.nm_desc) nm_desc,
+		COALESCE(vbp.nm_display_name, bp.nm_display_name) nm_display_name
 from t_adjustment_type aty
 inner join t_base_props bp on aty.id_prop=bp.id_prop
 inner join t_pi_template tmp on aty.id_pi_type=tmp.id_pi
-left outer join t_description namedesc on namedesc.id_desc=bp.n_name
-left outer join t_description dispdesc on dispdesc.id_desc=bp.n_display_name
-left outer join t_description descdesc on descdesc.id_desc=bp.n_desc
-where
-tmp.id_template=%%ID_PI%%
-and
-(namedesc.id_lang_code is null or namedesc.id_lang_code=%%ID_LANG_CODE%%)
-and
-(dispdesc.id_lang_code is null or dispdesc.id_lang_code=%%ID_LANG_CODE%%)
-and
-(descdesc.id_lang_code is null or descdesc.id_lang_code=%%ID_LANG_CODE%%)
-and 
-not exists (
-	select *
-	from t_adjustment a
-	where 
-	a.id_adjustment_type=aty.id_prop
-	and a.id_pi_instance is null
-	and a.id_pi_template = tmp.id_template
-)
+left outer join t_vw_base_props vbp on bp.id_prop = vbp.id_prop and vbp.id_lang_code = %%ID_LANG_CODE%%
+where tmp.id_template=%%ID_PI%%
+		and not exists (
+						select *
+						from t_adjustment a
+						where 
+						a.id_adjustment_type=aty.id_prop
+						and a.id_pi_instance is null
+						and a.id_pi_template = tmp.id_template
+					)
 			

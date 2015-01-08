7ae07cfd8916d95feeee61ded34d363442031a92
armitakaboli <akaboli@metratech.com>
2015-01-08 17:50:02 -0500

    CORE-8899 removing an unused function
    [FEAT-3848] Line not highlighted when hover over Legend in "Active Bill Run" on Operations Dashboard

--- MetraControl/ControlCenter/js/D3Visualize.js
+++ MetraControl/ControlCenter/js/D3Visualize.js
@@ -443,11 +443,6 @@ function fnVisualizeLineChart2(objLineChartConfig) {
     return this;
 }
 
-function componentToHex(c) {
-  var hex = c.toString(16);
-  return hex.length == 1 ? "0" + hex : hex;
-}
-
 //CreateLegend('legend', svglegend);
 function CreateLegend(data,svglegend) {
     

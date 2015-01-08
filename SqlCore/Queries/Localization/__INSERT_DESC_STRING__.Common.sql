ef48e51e5c32f07896318c94ea25596f40b98c2c
ipisanka <ipisanka@metratech.com>
2015-01-08 09:25:15 -0500

    CORE-8361 MetraView Localization: Misc.Adjustments and Nonstandard charges text are not visible on MetraView for Esp (MX) localization on ORACLE DB Code Reviewer: RPuzikov

--- SqlCore/Queries/Localization/__INSERT_DESC_STRING__.Common.sql
+++ SqlCore/Queries/Localization/__INSERT_DESC_STRING__.Common.sql
@@ -1,6 +1,6 @@
 
       insert into t_description (id_desc, id_lang_code, tx_desc)
 				select %%DESC_ID%%,id_lang_code, N'%%TX_DESC%%' from t_language lang
-				where tx_lang_code = LOWER('%%COUNTRYCODE_STRING%%')
+				where LOWER(tx_lang_code) = LOWER('%%COUNTRYCODE_STRING%%')
 				AND NOT EXISTS (SELECT * from t_description td WHERE td.id_lang_code = lang.id_lang_code and td.id_desc = %%DESC_ID%%)
       

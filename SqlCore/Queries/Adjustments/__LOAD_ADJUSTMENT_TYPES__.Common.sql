86f81d4a67e4e086c7849fd82c8688b09abc8039
msukhovarov <msukhovarov@metratech.com>
2015-01-19 23:45:15 +0200

    CORE-9145 - [Localization] Unable to change DisplayName and localization for Adjustment
    - removed ID_LANG_CODE, due to now it loads separate Entity DisplayName and DispalyDescriptions
    - added copy DisplayName and DisplayDescriptions to CloneBaseProps()

--- SqlCore/Queries/Adjustments/__LOAD_ADJUSTMENT_TYPES__.Common.sql
+++ SqlCore/Queries/Adjustments/__LOAD_ADJUSTMENT_TYPES__.Common.sql
@@ -7,8 +7,10 @@
         ajt.n_adjustmentType TypeUOM, 
         ajt.b_SupportBulk SupportsBulk,
         base1.nm_name	TypeName,	
-        base1.nm_desc	TypeDescription, 
-        COALESCE(desc1.tx_desc, base1.nm_display_name) TypeDisplayName,	
+        base1.nm_desc TypeDescription,
+		base1.n_desc TypeDescriptionId,
+		base1.nm_display_name TypeDisplayName,
+		base1.n_display_name TypeDisplayNameId,
         ajf.tx_formula TypeFormula, 
         ajf.id_engine TypeFormulaEngine,
         ajtp.id_prop TypePropID, 
@@ -16,8 +18,12 @@
         ajtp.n_direction TypePropDirection,	
         ajtp.id_adjustment_type	TypePropAdjustmentTypeID,
         base2.nm_name	TypePropName,	
-        base2.nm_desc	TypePropDescription, 
-        COALESCE(desc2.tx_desc, base2.nm_display_name) TypePropDisplayName,	
+        base2.nm_desc TypePropDescription,
+		/* TODO: Needs to add using localization for PROPS */
+		base2.n_desc TypePropDescriptionId, 
+		base2.nm_display_name TypePropDisplayName,
+		/* TODO: Needs to add using localization for PROPS */
+		base2.n_display_name TypePropDisplayNameId,
         ajt.id_formula TypeFormulaID, 
         ajt.tx_default_desc AdjustmentDefaultDescription, 
         pv.nm_table_name ProductViewTableName,
@@ -29,13 +35,7 @@
         INNER JOIN t_calc_formula ajf ON ajt.id_formula = ajf.id_formula
         LEFT OUTER JOIN	t_adjustment_type_prop ajtp	ON ajt.id_prop = ajtp.id_adjustment_type
         LEFT OUTER JOIN	t_base_props base1 ON	ajt.id_prop	=	base1.id_prop
-        LEFT OUTER JOIN	t_description	desc1	ON 
-          base1.n_display_name = desc1.id_desc AND 
-          desc1.id_lang_code = %%ID_LANG_CODE%%
-        LEFT OUTER JOIN	t_base_props base2 ON	ajtp.id_prop = base2.id_prop
-        LEFT OUTER JOIN	t_description	desc2	ON 
-          base2.n_display_name = desc2.id_desc AND 
-          desc2.id_lang_code = %%ID_LANG_CODE%%
+        LEFT OUTER JOIN	t_base_props base2 ON	ajtp.id_prop = base2.id_prop        
       WHERE 1 = 1        
         %%PREDICATE%%
       ORDER BY ajt.id_prop

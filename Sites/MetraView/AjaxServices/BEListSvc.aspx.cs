d7b9a175943f9c61f0c575c2d63dfd6845fd6d67
apyrshev <apyrshev@metratech.com>
2015-01-19 10:45:21 -0500

    ESR-7911 MTHF_8.0.0-S003:Two 'sep=' are displayed in csv file when export data from MetraView Admin page

--- Sites/MetraView/AjaxServices/BEListSvc.aspx.cs
+++ Sites/MetraView/AjaxServices/BEListSvc.aspx.cs
@@ -109,7 +109,6 @@ public partial class AjaxServices_BEListSvc : MTListServicePage
       Response.ContentType = "application/csv";
       Response.AddHeader("Content-Disposition", "attachment; filename=export.csv");
       Response.BinaryWrite(BOM);
-      Response.Write(Environment.NewLine);
     }
 
     //if there are more records to process than we can process at once, we need to break up into multiple batches

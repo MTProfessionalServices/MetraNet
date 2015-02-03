1e80c2ba086fdf8fa4219c3acdcac4c587acb582
dgaz <dgaz@metratech.com>
2015-02-03 14:06:40 -0500

    CORE-9244 Resubmit is not working in case installing MetraNet with AR (Oracle only)
    code review by damin

--- SqlCore/Queries/AR/__GET_ADJUSTMENTS_TO_REVERSE__.Oracle.sql
+++ SqlCore/Queries/AR/__GET_ADJUSTMENTS_TO_REVERSE__.Oracle.sql
@@ -1,5 +1,5 @@
     
-          SELECT '%%ID_PREFIX%%' || ID as 'AdjustmentID',
+          SELECT '%%ID_PREFIX%%' || ID as AdjustmentID,
           Type
           FROM tmp_ARReverse WHERE Namespace = '%%ACC_NAME_SPACE%%'
         

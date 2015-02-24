1ee9b373fb6dcdca5c9ebad053765343e7828dee
Ley Missailidis <moisis.langley@missailidis.ca>
2015-02-24 11:43:36 -0800

    resolves rebase

--- SqlCore/Install/Tax/CreateTables/__CREATE_T_TAX_VENDOR_PARAMS__.Oracle.sql
+++ SqlCore/Install/Tax/CreateTables/__CREATE_T_TAX_VENDOR_PARAMS__.Oracle.sql
@@ -5,6 +5,7 @@
     tx_type varchar2(255) not null,
     tx_default varchar2(255) not null,
     tx_description varchar2(2000) not null,
+    tx_direction number(10) NULL,
     constraint pk_t_tax_vendor_params_id_vend primary key(id_vendor,tx_canonical_name)
     )
 		

f769e63cf7bc627350b63451760795b92a64629e
vkurylovich <vkurylovich@metratech.com>
2015-03-02 12:04:35 -0500

    CORE-9057: Operations Dashboard failed transaction count over 30 days only counts new transactions code reviewed by armitakaboli

--- config/SqlCore/Queries/UI/Dashboard/__GET_FAILEDTRANSACTIONS_OVERXDAYS__.SqlServer.sql
+++ config/SqlCore/Queries/UI/Dashboard/__GET_FAILEDTRANSACTIONS_OVERXDAYS__.SqlServer.sql
@@ -1,3 +1,3 @@
 select COUNT(*) as Count_Over_Set_Days from t_failed_transaction ft where  
 ft.dt_FailureTime < DATEADD(day, -(%%AGE_THRESHOLD%%), GETUTCDATE())
-and State = 'N'
+and State in ('N', 'I')

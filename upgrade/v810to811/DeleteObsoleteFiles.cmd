25ffd24f21c31c15593240ad97aa5f4801408ae7
yleontyuk <yleontyuk@metratech.com>
2015-03-10 07:31:36 -0400

    Core-9321. File Added for removing deleted from git repository files, including incorrect link.

---
+++ upgrade/v810to811/DeleteObsoleteFiles.cmd
@@ -0,0 +1,3 @@
+del %MTRMP%\config\SqlCore\Install\RecurringCharges\CreateTables\__CREATE_TMP_PAYER_CHANGES__._Info.xml
+del %MTRMP%\config\SqlCore\Install\RecurringCharges\CreateTables\__CREATE_TMP_PAYER_CHANGES__.Oracle.sql
+del "%MTRMP%\Config\UsageServer\Where is usageserver.xml.lnk"

4a0b4c5340cb4fa405640d3121ee92c8302280b5
msukhovarov <msukhovarov@metratech.com>
2015-03-11 16:56:46 -0400

    CORE-9511 CapabilityHook doesn't update capability description and localized names and descriptions
    Implemented Update functionality for Capability Hook

---
+++ SqlCore/Queries/Auth/__UPDATE_ACT_ONLY_DESCRIPTION__.Common.sql
@@ -0,0 +1,7 @@
+
+				UPDATE 
+				/* Query Tag: __UPDATE_ACT_ONLY_DESCRIPTION__ */
+				t_atomic_capability_type
+				SET tx_desc=N'%%DESC%%'
+				WHERE id_cap_type = %%ID%%
+    	
---
+++ SqlCore/Queries/Auth/__UPDATE_CCT_ONLY_DESCRIPTION__.Common.sql
@@ -0,0 +1,7 @@
+
+				UPDATE 
+				/* Query Tag: __UPDATE_CCT_ONLY_DESCRIPTION__ */
+				t_composite_capability_type
+				SET tx_desc=N'%%DESC%%'
+				WHERE id_cap_type = %%ID%%
+    	

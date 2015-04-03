1840371b0bac489f45df20a711b8489b05060c34
dgaz <dgaz@metratech.com>
2015-04-03 11:07:35 -0400

    ESR-7995 Credit Notes - Backport feature to 8.1.0-S002

---
+++ SqlCore/Install/CreditNote/CreateTables/__CREATE_T_CREDIT_NOTE_CURRENT_ID_TABLE__.Oracle.sql
@@ -0,0 +1,7 @@
+
+          CREATE TABLE t_credit_note_current_id
+          (
+          nm_current nvarchar2(20) NOT NULL,
+          id_current number(10) NOT NULL,
+          CONSTRAINT PK_t_credit_note_current_id PRIMARY KEY (nm_current)
+          )
---
+++ SqlCore/Install/CreditNote/CreateTables/__CREATE_T_CREDIT_NOTE_CURRENT_ID_TABLE__.SqlServer.sql
@@ -0,0 +1,6 @@
+create table t_credit_note_current_id
+(
+nm_current nvarchar(20) NOT NULL,
+id_current int NOT NULL,
+CONSTRAINT PK_t_credit_note_current_id PRIMARY KEY CLUSTERED (nm_current)
+)

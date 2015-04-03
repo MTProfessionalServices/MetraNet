baecc4ef0db34dcbbecc61655df69197c910e552
dgaz <dgaz@metratech.com>
2015-04-03 11:07:56 -0400

    ESR-7995 Credit Notes - Backport feature to 8.1.0-S002

---
+++ SqlCore/Queries/CreditNotes/__GET_CREDIT_NOTE_PDF_STATUSES__.Oracle.sql
@@ -0,0 +1,7 @@
+SELECT 
+	NVL(td.tx_desc, REPLACE(ed.nm_enum_data, 'MetraTech.MetraNet.CreditNotes/CreditNotePDFStatus/', '')) AS localized_pdf_status
+FROM t_enum_data ed 
+LEFT JOIN t_description td ON ed.id_enum_data = td.id_desc AND td.id_lang_code = :LangID
+WHERE ed.nm_enum_data LIKE 'MetraTech.MetraNet.CreditNotes/CreditNotePDFStatus/%'
+ORDER BY localized_pdf_status ASC
+

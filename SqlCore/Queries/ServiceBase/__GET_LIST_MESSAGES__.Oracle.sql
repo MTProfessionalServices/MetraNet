d19e62474b856c80d58bc04c78cad32ad93e4f8e
Dmitri Shakhov <dshahov@metratech.com>
2015-04-10 07:27:25 -0400

    ESR-7824 CLONE - PipelineMeteringHelperCache in Synchronous mode not waiting until complete pipeline processing

--- SqlCore/Queries/ServiceBase/__GET_LIST_MESSAGES__.Oracle.sql
+++ SqlCore/Queries/ServiceBase/__GET_LIST_MESSAGES__.Oracle.sql
@@ -1,9 +1,9 @@
 	
 	select id_origin_message as id 
-	     from t_message_mapping 
-	     where id_origin_message in (%%ID_MESSAGES%%)
+	     from t_message_mapping %%LOCK%%
+	     where id_origin_message in (%%ID_MESSAGES%%) 
 	     union all
 	      select id_message as id
-		from t_message_mapping  
+		from t_message_mapping %%LOCK%% 
 	START WITH id_origin_message in (%%ID_MESSAGES%%)
 	CONNECT BY PRIOR  id_message =id_origin_message

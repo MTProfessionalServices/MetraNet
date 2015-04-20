3898d7d9e51adc462d796d3eeed9d00ebb5463fa
dgaz <dgaz@metratech.com>
2015-04-20 17:35:58 -0400

    ESR-8023 Credit Notes - - Backport of FEAT-5933 and credit note bug fixes to 8.1.0-S002

--- SqlCore/Queries/Adjustments/__LOAD_SELECT_ACCOUNT_CREDITS__.Common.sql
+++ SqlCore/Queries/Adjustments/__LOAD_SELECT_ACCOUNT_CREDITS__.Common.sql
@@ -1,6 +1,10 @@
 
             select 
-              *
+              pv.*,
+              mapper.nm_login as cn_issuer
             from t_pv_AccountCredit pv
-            where pv.c_RequestID in (%%SESSION_IDS%%) and c_IssueCreditNote = 1
+            inner join t_acc_usage au on au.id_sess = pv.c_RequestID
+            inner join t_svc_AccountCredit svc on svc.id_source_sess = au.tx_UID          
+            inner join t_account_mapper mapper on mapper.id_acc = svc.c_Issuer
+            where pv.c_RequestID in (%%SESSION_IDS%%) and pv.c_IssueCreditNote = 1
 			

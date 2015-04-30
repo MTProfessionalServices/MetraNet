0d61dc6c617af0069d13479d3a9aaf54d0666691
Evgeniy Shvecov <yshvetsov@metratech.com>
2015-04-30 09:49:46 -0400

    ESR-7932  CLONE - ICAL - GBL - Backout/Resubmit Performance

--- SqlCore/Queries/billingrerun/__COPY_INTO_REAL_SESSION_TABLES__.Oracle.sql
+++ SqlCore/Queries/billingrerun/__COPY_INTO_REAL_SESSION_TABLES__.Oracle.sql
@@ -1,17 +1,20 @@
-declare
-  myClobVar clob := to_clob('%%CONTEXT%%');
-begin
-  insert into t_session (id_ss, id_source_sess)
-              select id_ss, id_source_sess from tmp_session
-              order by id_ss, id_source_sess;
-              
-  insert into t_session_set (id_ss, id_message, id_svc, session_count, b_root)
-              select id_ss, id_message, id_svc, session_count, b_root from tmp_session_set
-              order by id_ss, id_message;
-
-  insert into t_message (id_message, id_route, dt_crt, dt_assigned, id_listener, id_pipeline, dt_completed, id_feedback,                       dt_metered, tx_sc_serialized, tx_ip_address) 
-              select  id_message, null, %%METRADATE%%, null, null, null, 
-                      null, null, %%METRADATE%%, myClobVar, '127.0.0.1'
-              from tmp_message
-              order by id_message;
-end;
+	   
+		      declare
+		        myClobVar clob := to_clob('%%CONTEXT%%');
+		      begin
+		      insert into t_session (id_ss, id_source_sess)
+		      select id_ss, id_source_sess from tmp_session
+          order by id_ss, id_source_sess;
+		      
+		      insert into t_session_set (id_ss, id_message, id_svc, session_count, b_root)
+		      select id_ss, id_message, id_svc, session_count, b_root from tmp_session_set
+          order by id_ss, id_message;
+		      
+		      insert into t_message(id_message, id_route, dt_crt, dt_assigned, 
+			    id_listener, id_pipeline, dt_completed, id_feedback, dt_metered, tx_sc_serialized, tx_ip_address) 
+			    select  id_message, null, %%METRADATE%%, null, null, null, 
+			    null, null, %%METRADATE%%, myClobVar, '127.0.0.1'
+			    from tmp_message
+          order by id_message;
+			    end;
+	  
--- SqlCore/Queries/billingrerun/__CREATE_LARGE_MESSAGES_AND_SESSIONSETS__.Oracle.sql
+++ SqlCore/Queries/billingrerun/__CREATE_LARGE_MESSAGES_AND_SESSIONSETS__.Oracle.sql
@@ -1,36 +1,39 @@
-declare 
+declare
   /* For performance reasons, break the encapsulation of the TTT package layer. */
-  /* In general, this should not be done. */
-  v_tx_id dba_2pc_pending.global_tran_id%TYPE             DEFAULT NULL;
-begin
-  /* Access the TTT table directly, by storing the transaction id in a local variable, */
-  /* and adding the tx_id column access as required. */
-  v_tx_id := mt_ttt.get_tx_id();
-  
-  insert into ttt_tmp_session (tx_id, id_ss, id_source_sess)
-              select v_tx_id, id_sess + %%ID_SESSIONSET_START%%, id_parent_source_sess
-              from ttt_tmp_aggregate_large
-              where tx_id = v_tx_id;
-                
-  insert into ttt_tmp_session_set (tx_id, id_ss, id_message, id_svc, session_count, b_root)
-              select v_tx_id, id_sess + %%ID_SESSIONSET_START%%, id_sess + %%ID_MESSAGE_START%%, %%ID_SVC%%, 1, '1'
-              from ttt_tmp_aggregate_large
-              where tx_id = v_tx_id;
-      
-  insert into ttt_tmp_message (tx_id, id_message)
-              select v_tx_id, id_message
-              from ttt_tmp_session_set
-              where id_message > %%ID_MESSAGE_START%% and tx_id = v_tx_id;
-      
-  insert into ttt_tmp_session (tx_id, id_ss, id_source_sess)
-              select v_tx_id, css.id_sess + %%NUM_LARGE_MESSAGES%% + %%ID_SESSIONSET_START%%, rr.id_source_sess
-              from %%RERUN_TABLE_NAME%% rr
-              inner join ttt_tmp_aggregate_large prnt on prnt.id_parent_source_sess = rr.id_parent_source_sess and prnt.tx_id = v_tx_id
-              inner join ttt_tmp_child_session_sets css on css.id_parent_sess = prnt.id_sess and css.id_svc = rr.id_svc and css.tx_id = v_tx_id;
-                    
-  insert into ttt_tmp_session_set (tx_id, id_message, id_svc, session_count, b_root)
-              select v_tx_id, id_sess + %%NUM_LARGE_MESSAGES%% + %%ID_SESSIONSET_START%%, id_parent_sess + %%ID_MESSAGE_START%%, id_svc, cnt, '0'
-              from ttt_tmp_child_session_sets
-              where tx_id = v_tx_id;
- 
- end;
+  /* In general, this should not be done.                                       */
+  v_tx_id dba_2pc_pending.global_tran_id%TYPE   DEFAULT NULL;
+ begin
+    /* Access the TTT table directly, by storing the transaction id in a          */
+    /* local variable, and adding the tx_id column access as required.            */
+    v_tx_id := mt_ttt.get_tx_id();
+    	
+    insert into ttt_tmp_session (tx_id, id_ss, id_source_sess) 
+		              select v_tx_id, id_sess + %%ID_SESSIONSET_START%%, id_parent_source_sess 
+			            from ttt_tmp_aggregate_large
+                  where tx_id = v_tx_id;
+
+		insert into ttt_tmp_session_set (tx_id, id_ss, id_message, id_svc, session_count, b_root) 
+		              select v_tx_id, id_sess + %%ID_SESSIONSET_START%%, id_sess + %%ID_MESSAGE_START%%, %%ID_SVC%%, 1, '1'
+			            from ttt_tmp_aggregate_large
+                  where tx_id = v_tx_id;
+    			         
+		insert into ttt_tmp_message(tx_id, id_message) 
+			  select v_tx_id, id_message
+			    from ttt_tmp_session_set
+			    where id_message > %%ID_MESSAGE_START%%
+            and tx_id = v_tx_id;
+			              
+		insert into ttt_tmp_session (tx_id, id_ss, id_source_sess) 
+				select v_tx_id, css.id_sess + %%NUM_LARGE_MESSAGES%% + %%ID_SESSIONSET_START%%, rr.id_source_sess
+				from  %%RERUN_TABLE_NAME%% rr
+				inner join ttt_tmp_aggregate_large prnt on prnt.id_parent_source_sess=rr.id_parent_source_sess and prnt.tx_id = v_tx_id
+				inner join ttt_tmp_child_session_sets css on css.id_parent_sess=prnt.id_sess and css.id_svc=rr.id_svc and css.tx_id = v_tx_id;
+			                 
+     	insert into ttt_tmp_session_set (tx_id, id_ss, id_message, id_svc, session_count, b_root) 
+				select v_tx_id, id_sess + %%NUM_LARGE_MESSAGES%% + %%ID_SESSIONSET_START%%, id_parent_sess + %%ID_MESSAGE_START%%, id_svc, cnt, '0'
+				from ttt_tmp_child_session_sets
+        where tx_id = v_tx_id;
+	
+     
+  end;	   
+	  
